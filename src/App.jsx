8ea3d49955f3bbd5e5a64f41d99070a96d99195e
admin_dho <davidhorph@hotmail.com>
2025-05-05 23:30:50 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <header>
+        <h1>FUTURE HOME OF...</h1>
+        <h3>BEAT THE IDOLS!!!</h3>
+      </header>
+    </div>
+  )
+}
+
+export default App
