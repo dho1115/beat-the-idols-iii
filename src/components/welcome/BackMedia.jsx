77e4f9739ffde5c4033043cf89baa9971797f372
admin_dho <davidhorph@hotmail.com>
2025-05-07 21:00:48 -0500

    Media changes.

--- src/components/welcome/BackMedia.jsx
+++ src/components/welcome/BackMedia.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
 
 import backVideo from '../../media/videos/RockBand4.mp4';
 
 const BackMedia = () => {
   return (
-    <video muted width="100%" height="100%" className='back-video'>
+    <video muted autoPlay loop height="100%" className='back-video'>
       <source src={backVideo} type='video/mp4' />
       <source src={backVideo} type='video/ogg' />
       <h5>Sorry... your browser does not support this video.</h5>
-    </video>
+    </video>     
   )
 }
 
--- src/components/welcome/FrontMedia.jsx
+++ src/components/welcome/FrontMedia.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import FrontImage from '../../media/images/RockConcert_NoBG.png';
 
+import "./WelcomeComponent.styles.css";
+
 const FrontMedia = () => {
-  return <img src={FrontImage} className='front-media-img' />
+  return (
+    <div className='front-media-div'>
+      <img src={FrontImage} className='front-media-img' />
+    </div>
+  )
 }
 
 export default FrontMedia
