311259b588682afb2bf42793ec0f420e3700772e
admin_dho <davidhorph@hotmail.com>
2025-05-09 16:26:43 -0500

    Update BackMedia.jsx
    This is the video that goes behind the image of the audience.

--- src/components/welcome/BackMedia.jsx
+++ src/components/welcome/BackMedia.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
+import React, { useRef, useEffect } from 'react'
+
+import { Parallax } from 'react-parallax';
 
 import backVideo from '../../media/videos/RockBand4.mp4';
 
-const BackMedia = () => {
+import './WelcomeComponent.styles.css'
+
+const BackMedia = () => {  
   return (
-    <video muted autoPlay loop height="100%" className='back-video'>
+    <video muted autoPlay loop playsInline className='back-video'>
       <source src={backVideo} type='video/mp4' />
       <source src={backVideo} type='video/ogg' />
       <h5>Sorry... your browser does not support this video.</h5>
-    </video>     
+    </video>
   )
 }
 
