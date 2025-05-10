a506867c335c0807b06aa859641de7d53981beef
admin_dho <davidhorph@hotmail.com>
2025-05-10 15:32:29 -0500

    Used the use hook to create route founction.
    WHY DID I USE THE use Hook?
    
    Because inside that use hook, I plan to use the useLocation hook to filter out routes that are NOT my current page.
    
    So, if I am currently on the Home page, my route path would be "/home'.
    
    I want to use location to find all routes inside either generalRoutes or loggedInRoutes function to return routes that are NOT '/home'.
    
    WHY???
    
    I plan to use those inside the navigation bars (found inside navigationbars).
    
    How do I plan to  use them inside the navigation bars?
    
    I plan to create links to pages that are NOT the pages I am on.
    
    Again, if I am on the home page, my general navigation bar would have the following links:
    
    /about
    /contact
    /any other link that is NOT home.
    
    I mean, it doesn't make sense to have a link that leads to a parge you are currently on.

--- src/routes/GeneralRoutes.jsx
+++ src/routes/GeneralRoutes.jsx
@@ -1,3 +1,10 @@
-export default GeneralRoutes = [
+import { useLocation, useState } from "react-router-dom"
+
+const useGeneralRoutes = (arrayOfRoutes=[]) => {
+   const location = useLocation();
+   const currentLocation = location.pathname;
+
+   const [generalRoutes, setGeneralRoutes] = useState({ allRoutes: [], nonCurrentRoutes: generalRoutes.allRoutes.length ? generalRoutes.allRoutes.filter(value => value.pathame != currentLocation) : [] });
    
-]
+   return [generalRoutes, setGeneralRoutes];
+}
--- src/routes/LoggedInRoutes.jsx
+++ src/routes/LoggedInRoutes.jsx
@@ -1,3 +1,10 @@
-export default LoggedInRoutes = [
+import { useLocation, useState } from "react-router-dom"
+
+const useLoggedInRoutes = (arrayOfRoutes=[]) => {
+   const location = useLocation();
+   const currentLocation = location.pathname;
+
+   const [loggedInRoutes, setLoggedInRoutes] = useState({ allRoutes: [], nonCurrentRoutes: loggedInRoutes.allRoutes.length ? loggedInRoutes.allRoutes.filter(value => value.pathame != currentLocation) : [] })
    
-]
+   return [loggedInRoutes, setLoggedInRoutes];
+}
