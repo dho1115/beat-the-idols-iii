76033d0aab7ebea3b22153cec27edf0d4dc034be
admin_dho <davidhorph@hotmail.com>
2025-05-10 15:02:21 -0500

    Styling to WelcomeComponent/page.

--- src/pages/welcome/WelcomePage.jsx
+++ src/pages/welcome/WelcomePage.jsx
@@ -4,7 +4,9 @@ import React from 'react';
 import { Parallax } from 'react-scroll-parallax';
 
 //Components
+//Components.
 import Header from '../../components/welcome/Header';
+import WelcomeNavbar from '../../components/navigationbars/welcome/WelcomeNavbar';
 
 import "./WelcomePage.styles.css";
 
@@ -12,8 +14,9 @@ const WelcomePage = () => {
 
   return (    
     <div className='welcome-page-wrapper p-0'>
+      <WelcomeNavbar />
       <Header />
-      <section style={{backgroundColor: 'lavender'}}>
+      <section style={{backgroundColor: 'lavender', minHeight: '100vh'}}>
         <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
 
         <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
