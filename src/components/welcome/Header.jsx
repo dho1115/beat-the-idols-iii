275a87b3603456cb639bf56e4b242aabd84dad37
admin_dho <davidhorph@hotmail.com>
2025-05-10 11:57:35 -0500

    SUCCESS!!! Parallax works.
    Parallax works.
    
    Had to create a .wrapper:
    
    .welcome-page-wrapper{
       height: 100vh;
       overflow-x: hidden;
       overflow-y: auto;
       perspective: 10px;
    }
    
    Inside this .wrapper is where I will put the components (inner components) that will be involved in the parallax scroll.
    
    The css logic will be inside each inner component and would involve using the translateZ( ) to move the image in a 3D perspective relative to the perspective in wrapper and then scaling with scale( ) css prop..
    
    Also note the preserve 3d:
    
    transform-style: preserve-3d;
    
    Inside the <Header> which contains the back video, front image and logo.

--- src/components/welcome/Header.jsx
+++ src/components/welcome/Header.jsx
@@ -4,11 +4,13 @@ import BackMedia from './BackMedia';
 import FrontMedia from './FrontMedia';
 import Logo from '../../media/images/BeatTheIdols_NoBackground.png';
 
+import "./WelcomeComponent.styles.css";
+
 const Header = () => {
   return (
     <div className='header'>
       <BackMedia />
-      <img src={Logo} />
+      <img src={Logo} className='logo' />
       <FrontMedia />
     </div>
   )
--- src/pages/welcome/WelcomePage.jsx
+++ src/pages/welcome/WelcomePage.jsx
@@ -11,9 +11,9 @@ import "./WelcomePage.styles.css";
 const WelcomePage = () => {
 
   return (    
-    <div className='welcome-page-wrapper'>
+    <div className='welcome-page-wrapper p-0'>
       <Header />
-      <section>
+      <section style={{backgroundColor: 'lavender'}}>
         <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
 
         <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
