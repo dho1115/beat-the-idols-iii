fe1b7e622efc131b06ff869aa89ba75801d0a12d
admin_dho <davidhorph@hotmail.com>
2025-05-10 15:01:00 -0500

    New Components and pages.

---
+++ src/pages/about/AboutUsPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AboutUs from '../../components/about/AboutUs';
+
+import './AboutUsPage.styles.css';
+
+const AboutUsPage = () => {
+  return (
+    <div>
+      <AboutUs />
+    </div>
+  )
+}
+
+export default AboutUsPage
---
+++ src/pages/home/Homepage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const Homepage = () => {
+  return (
+    <div>
+      <header>
+         <h1>WELCOME TO THE HOME PAGE!!!</h1>
+      </header>
+      <div>
+         <strong>
+            lorem ipsul dolor here...
+         </strong>
+      </div>
+    </div>
+  )
+}
+
+export default Homepage
