b3967f28accf1c3f37136f7f55589d2858d5b070
admin_dho <davidhorph@hotmail.com>
2025-05-10 16:17:38 -0500

    WelcomeNavbar
    the WelcomeNavbar now is position, absolute, so there is now only 1 Y scroll.

--- src/components/navigationbars/welcome/WelcomeNavbar.jsx
+++ src/components/navigationbars/welcome/WelcomeNavbar.jsx
@@ -2,18 +2,19 @@ import React from 'react'
 
 //Dependencies.
 import { Link } from 'react-router-dom';
+import logo from "../../../media/images/BeatTheIdols_NoBackground.png";
 
 import "./WelcomeNavbar.styles.css";
 
-import logo from "../../../media/images/BeatTheIdols_NoBackground.png";
-
 const WelcomeNavbar = () => {
   return (
-    <nav className='welcome-navbar p-1 sticky-top'>
-      <a class="navbar-brand bg bg-dark" href="/" style={{backgroundColor: "#333"}}><img src={logo} className='img-thumbnail welcome-nav-img' /></a>
-      <Link to="/">LINK 1</Link>
-      <Link to="/">LINK 2</Link>
-      <Link to="/">LINK 3</Link>
+    <nav className='welcome-navbar p-1 sticky-top' style={{position: 'absolute', top: '0%', left: '0%', right: '0%'}}>
+      <a class="navbar-brand" href="/" style={{ width: '3vw' }}><img src={logo} className='img-thumbnail welcome-nav-img' /></a>
+      <div className='welcome-link-container p-0'>
+        <Link to="/"><strong style={{color: 'whitesmoke'}}>LINK 1</strong></Link>
+        <Link to="/"><strong style={{color: 'whitesmoke'}}>LINK 2</strong></Link>
+        <Link to="/"><strong style={{color: 'whitesmoke'}}>LINK 3</strong></Link>
+      </div>      
     </nav>
   )
 }
