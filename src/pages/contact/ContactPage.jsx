536151f21bd01e8cb4bc8ec9f6931f76b443ab51
admin_dho <davidhorph@hotmail.com>
2025-05-11 15:14:45 -0500

    form aesthetics.

--- src/pages/contact/ContactPage.jsx
+++ src/pages/contact/ContactPage.jsx
@@ -5,15 +5,18 @@ import "./ContactPage.styles.css";
 
 const ContactPage = () => {
   return (
-    <div className='contact-page-div'>
+    <div className='contact-page-div my-5'>
       <header>FEEL FREE TO CONTACT US!!!</header>
       <Container>
          <Form onSubmit={(e) => {
             e.preventDefault()
             console.log("Thanks for the submission!!!")
-         }}>
+         }} style={{borderRadius: '5px', border: '1.5px solid black', backgroundColor: 'lightsalmon'}} className='p-3'>
             <FormGroup>
                <Input type='text' placeholder='Your name' />
+            </FormGroup>
+            <FormGroup>
+               <Input type='textarea' placeholder='Whaddya wanna say to us?' maxLength={573} />
             </FormGroup>
             <FormGroup>
                <Button size='lg' color='danger' type='submit'>SUBMIT</Button>
