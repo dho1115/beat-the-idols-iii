74d4c85ef347f994712500b63b4941ea501d8053
admin_dho <davidhorph@hotmail.com>
2025-05-11 15:06:56 -0500

    SUCCESS!!! Dynamic Route name placement.
    SUCCESS!!!
    
    I was able to make the welcome navbar do...
    
    DYNAMIC ROUTE PLACEMENT.
    
    What I did:
    
    1. Create a CUSTOM HOOK called useGeneralRoutes (before you say anything, read to the end!!!).
    
    2. I then INITIALIZED my hook to the following:
    [{name: 'home', path: '/'}, {name: 'about', path: '/about'}, {name: 'contact us!!!', path: '/contact'}]
    
    3. IMPORTANT!!! I then imported a hook called useLocation and then did this:   const location = useLocation();. This is important when I do .filter( ).map( ) later on in the return( ).
    
    4. Inside the return, I did this:
    {
              generalRoutes
                .filter(val => val.path != location.pathname)
                .map(route => <Link to={route.path}><strong style={{color: 'whiteSmoke'}}>{route.name}</strong></Link>)
            }
    
    HAVING SAID ALL THAT...
    
    I realized I did NOT have to use the custom Hook at all!!!
    
    Hell, I could have used useState( ) and initialized it with the general routes ("general", because you don't need to log in to see these routes. These routes lead to public pages).
    
    I plan to change it to useState( ) later.

--- src/components/navigationbars/welcome/WelcomeNavbar.jsx
+++ src/components/navigationbars/welcome/WelcomeNavbar.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
+import {useEffect} from 'react'
 
 //Dependencies.
 import { Link } from 'react-router-dom';
 import logo from "../../../media/images/BeatTheIdols_NoBackground.png";
+import { useGeneralRoutes } from '../../../useRoutes';
+import { useLocation } from 'react-router-dom';
 
 import "./WelcomeNavbar.styles.css";
 
 const WelcomeNavbar = () => {
+  const location = useLocation();
+
+  const [generalRoutes, setGeneralRoutes] = useGeneralRoutes([{name: 'home', path: '/'}, {name: 'about', path: '/about'}, {name: 'contact us!!!', path: '/contact'}])
+
+  console.log({ generalRoutes });
+  console.log({ locationPathname: location.pathname });
+
   return (
     <nav className='welcome-navbar p-1 sticky-top' style={{position: 'absolute', top: '0%', left: '0%', right: '0%'}}>
       <a class="navbar-brand" href="/" style={{ width: '3vw' }}><img src={logo} className='img-thumbnail welcome-nav-img' /></a>
       <div className='welcome-link-container p-0'>
-        <Link to="/"><strong style={{color: 'whitesmoke'}}>LINK 1</strong></Link>
-        <Link to="/"><strong style={{color: 'whitesmoke'}}>LINK 2</strong></Link>
-        <Link to="/"><strong style={{color: 'whitesmoke'}}>LINK 3</strong></Link>
+        {
+          generalRoutes
+            .filter(val => val.path != location.pathname)
+            .map(route => <Link to={route.path}><strong style={{color: 'whiteSmoke'}}>{route.name}</strong></Link>)
+        }
       </div>      
     </nav>
   )
