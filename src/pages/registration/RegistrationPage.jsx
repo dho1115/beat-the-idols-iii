4beb8a41feabb25ec62d2b0b229f01b9b4742b8a
admin_dho <davidhorph@hotmail.com>
2025-05-12 18:26:39 -0500

    RegistrationPage component.
    This is the page that will hold the Registration component (which I imported. See import Registration... ).

---
+++ src/pages/registration/RegistrationPage.jsx
@@ -0,0 +1,19 @@
+import React, {Suspense} from 'react'
+
+//Components.
+import Registration from '../../components/registration/Registration';
+import SuspenseFallback from '../../components/suspense_fallback/SuspenseFallback';
+
+import "./RegistrationPage.styles.css";
+
+const RegistrationPage = () => {
+  return (
+   <div className='registration-page-div'>
+      <Suspense fallback={<SuspenseFallback />}>
+         <h1>LOGIN AND SIGNUP GOES HERE.</h1>
+      </Suspense>
+   </div>
+  )
+}
+
+export default RegistrationPage
