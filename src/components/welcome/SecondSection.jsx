9b30b503eaa431ebb59c8159fbeb47bb8005da56
admin_dho <davidhorph@hotmail.com>
2025-05-13 09:26:15 -0500

    Create SecondSection.jsx

---
+++ src/components/welcome/SecondSection.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import "./WelcomeComponent.styles.css"
+
+const SecondSection = () => {
+  return (
+   <div className='second-section'>
+      <header>
+         <h1>You are the captain!!!</h1>
+         <h3>Build your own performance team</h3>
+      </header>
+      <div>
+         <strong>CAROUSEL GOES HERE</strong>
+      </div>
+   </div>
+  )
+}
+
+export default SecondSection
