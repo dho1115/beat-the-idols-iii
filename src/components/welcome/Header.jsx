38cc77a84ba5b6793f00d2eee6eabb8e748f6011
admin_dho <davidhorph@hotmail.com>
2025-05-13 09:26:09 -0500

    Note to address the header wrapper.
    See green highlight.

--- src/components/welcome/Header.jsx
+++ src/components/welcome/Header.jsx
@@ -8,7 +8,7 @@ import "./WelcomeComponent.styles.css";
 
 const Header = () => {
   return (
-    <div className='header'>
+    <div className='header'> {/* I May need to make the HEADER the wrapper */}
       <BackMedia />
       <img src={Logo} className='logo' />
       <FrontMedia />
