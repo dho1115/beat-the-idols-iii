8e6331561787a74f92ca9e391965d2c149654758
admin_dho <davidhorph@hotmail.com>
2025-05-14 08:28:14 -0500

    Create SecondSectionCarousel.jsx

---
+++ src/components/welcome/SecondSectionCarousel.jsx
@@ -0,0 +1,14 @@
+import { UncontrolledCarousel } from 'reactstrap';
+
+import './WelcomeComponent.styles.css';
+
+const SecondSectionCarousel = ({ images }) => {
+  return (
+   <div>
+      <h1>Carousel would go here</h1>
+      <h5>https://reactstrap.github.io/?path=/docs/components-carousel--carousel</h5>
+   </div>
+  )
+}
+
+export default SecondSectionCarousel
