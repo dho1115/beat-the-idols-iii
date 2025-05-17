f7d86bf68a619655e7fc6b61c20ce85599e332e1
admin_dho <davidhorph@hotmail.com>
2025-05-17 13:40:25 -0500

    ErrorBoundary implementation.
    1. Implemented <ErrorBoundary></ErrorBoundary>.
    2. Imported and implemented useLocation hook.

--- src/components/welcome/SecondSection.jsx
+++ src/components/welcome/SecondSection.jsx
@@ -1,8 +1,13 @@
+//Dependencies.
+import { useLocation } from "react-router-dom"
 import SecondSectionCarousel from "./SecondSectionCarousel"
+import ErrorBoundary from "../ErrorBoundary"
 
 import "./WelcomeComponent.styles.css"
 
 const SecondSection = () => {
+   const location = useLocation();
+
    const images = [
       { src: "https://images.pexels.com/photos/2531728/pexels-photo-2531728.jpeg?auto=compress&cs=tinysrgb&w=600", altText: 'first image', caption: 'image 1', key: 1 },
       { src: "https://images.pexels.com/photos/8198134/pexels-photo-8198134.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2", altText: 'second image', caption: 'image 2', key: 2 },
@@ -11,13 +16,15 @@ const SecondSection = () => {
 
    return (
       <div className='second-section my-1'>
-         <header>
+         <ErrorBoundary fallback={<h1>Crap. Something went wrong inside {location.pathname}.</h1>}>
+            <header>
             <h1>You are the captain!!!</h1>
             <h3>Build your own performance team</h3>
-         </header>
-         <div>
-            
-         </div>
+            </header>
+            <div>
+               
+            </div>
+         </ErrorBoundary>         
       </div>
    )
 }
