5164c8ea167ccf9f4b947cff2394b4f8be12419d
admin_dho <davidhorph@hotmail.com>
2025-05-17 13:46:30 -0500

    Implemented ErrorBoundary in main.jsx.
    SUCCESSFULLY wrapped <ErrorBoundary></ErrorBoundary> around <app />.

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import ErrorBoundary from './components/ErrorBoundary.jsx';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -6,5 +6,7 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
-  <App />
+  <ErrorBoundary fallback={<div style={{ height: '100vh', backgroundColor: 'lightgreen'}}><h1>Something went wrong inside main.jsx file.</h1></div>}>
+    <App />
+  </ErrorBoundary>  
 )
