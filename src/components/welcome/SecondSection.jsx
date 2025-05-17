97c2b8d23682082da63a2ec3ba9cda04feda193a
admin_dho <davidhorph@hotmail.com>
2025-05-17 13:06:53 -0500

    Updated test images.
    Added "alt" property.

--- src/components/welcome/SecondSection.jsx
+++ src/components/welcome/SecondSection.jsx
@@ -4,9 +4,9 @@ import "./WelcomeComponent.styles.css"
 
 const SecondSection = () => {
    const images = [
-      { src: "https://images.pexels.com/photos/2531728/pexels-photo-2531728.jpeg?auto=compress&cs=tinysrgb&w=600", alt: 'first image' },
-      { src: "https://images.pexels.com/photos/8198134/pexels-photo-8198134.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2", alt: 'second image' },
-      { src: "https://images.pexels.com/photos/30989671/pexels-photo-30989671/free-photo-of-elegant-woman-in-blue-dress-on-theater-stage.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2", alt: 'third image' }
+      { src: "https://images.pexels.com/photos/2531728/pexels-photo-2531728.jpeg?auto=compress&cs=tinysrgb&w=600", altText: 'first image', caption: 'image 1', key: 1 },
+      { src: "https://images.pexels.com/photos/8198134/pexels-photo-8198134.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2", altText: 'second image', caption: 'image 2', key: 2 },
+      { src: "https://images.pexels.com/photos/30989671/pexels-photo-30989671/free-photo-of-elegant-woman-in-blue-dress-on-theater-stage.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2", altText: 'third image', caption: 'image 3', key: 3 }
    ]
 
    return (
@@ -16,7 +16,7 @@ const SecondSection = () => {
             <h3>Build your own performance team</h3>
          </header>
          <div>
-            <SecondSectionCarousel images={images} />
+            
          </div>
       </div>
    )
