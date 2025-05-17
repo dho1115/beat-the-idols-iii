951bfb33d5293dc8e9aecff0fcff3cb737a062f1
admin_dho <davidhorph@hotmail.com>
2025-05-17 13:13:37 -0500

    Update SecondSectionCarousel.jsx
    Implemented Carousel code from Reactstrap: https://reactstrap.github.io/?path=/docs/components-carousel--carousel

--- src/components/welcome/SecondSectionCarousel.jsx
+++ src/components/welcome/SecondSectionCarousel.jsx
@@ -1,12 +1,43 @@
-import { UncontrolledCarousel } from 'reactstrap';
+import { useState } from 'react';
+import { Carousel, CarouselItem, CarouselControl, CarouselIndicators, CarouselCaption } from 'reactstrap';
 
 import './WelcomeComponent.styles.css';
 
 const SecondSectionCarousel = ({ images }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => { 
+    const nextIndex = activeIndex === images.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const previous = () => { 
+    const nextIndex = activeIndex === 0 ? images.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex)
+  };
+
+  const goToIndex = newIndex => {
+    if (animating) return;
+    setActiveIndex(newIndex)
+  }
+
+  const slides = images.map(val => (
+    <CarouselItem
+      onExiting={() => setAnimating(true)}
+      onExited={() => setAnimating(false)}
+      key={val.src}>
+        <img src={val.src} alt={val.altText} />
+      <CarouselCaption
+        captionText={val.caption}
+        captionHeader={val.caption}
+      />
+    </CarouselItem>
+  ))
+
   return (
    <div>
-      <h1>Carousel would go here</h1>
-      <h5>https://reactstrap.github.io/?path=/docs/components-carousel--carousel</h5>
+      <h1>Carousel would go here</h1>      
    </div>
   )
 }
