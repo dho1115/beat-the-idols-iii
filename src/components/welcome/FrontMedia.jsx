3a38eb1d3c0e820193c31939d11de3b6280d1f6b
admin_dho <davidhorph@hotmail.com>
2025-05-25 11:20:45 -0500

    Uploaded and imported new image.
    This image will be the new front image for <Header />

--- src/components/welcome/FrontMedia.jsx
+++ src/components/welcome/FrontMedia.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import FrontImage from '../../media/images/RockConcert_NoBG.png';
-
+import FrontImage2 from '../../media/images/audience-nobg.png'
 import "./WelcomeComponent.styles.css";
 
 const FrontMedia = () => {
-  return (
-    <img src={FrontImage} className='front-img' />
-  )
+  return <img src={FrontImage2} className='front-img' />
 }
 
 export default FrontMedia
