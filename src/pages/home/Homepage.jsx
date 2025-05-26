7220602c8e80ba88cb0b1a6c154daed3f9048b5f
admin_dho <davidhorph@hotmail.com>
2025-05-26 10:55:10 -0500

    Added ErrorBoundary, Suspense.

--- src/pages/home/Homepage.jsx
+++ src/pages/home/Homepage.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
+import React, { Suspense, useEffect } from 'react'
 import { Outlet, useLocation } from 'react-router-dom'
 import { Container } from 'reactstrap';
 
+import SuspenseFallback from '../../components/suspense_fallback/SuspenseFallback';
+import ErrorBoundary from '../../components/ErrorBoundary';
+
 import "./Homepage.styles.css";
+import CurrentChallenges from '../../components/home/CurrentChallenges';
 
 const Homepage = () => {
   const location = useLocation();
 
   return (
-    <div className='my-5'>
-      <header className='homepage-header'>
-        <h1>Welcome To The Home Page!!!</h1>
-      </header>
-      <Container>
-        <Outlet />
-      </Container>
-    </div>
+    <ErrorBoundary fallback={<h3>Something went wrong in {location.pathname}!!!</h3>}>
+      <Suspense fallback={<SuspenseFallback />}>
+        <div className='my-5'>
+          <header className='homepage-header'>
+            <h1>Welcome To The Home Page!!!</h1>
+          </header>
+          <Container>
+            <Outlet />
+          </Container>
+        </div>
+      </Suspense>
+    </ErrorBoundary>
   )
 }
 
