5fa4b448ca203e8f37478e0dc7934d4432c7c857
admin_dho <davidhorph@hotmail.com>
2025-05-26 10:05:43 -0500

    Update SecondSectionCarousel.jsx
    Added <Caption></Caption>.

--- src/components/welcome/SecondSectionCarousel.jsx
+++ src/components/welcome/SecondSectionCarousel.jsx
@@ -4,9 +4,6 @@ import ErrorBoundary from '../ErrorBoundary';
 import './WelcomeComponent.styles.css';
 
 const SecondSectionCarousel = ({ images }) => {
-  console.log(images);
-  debugger;
-
   const { Item, Caption } = Carousel;
 
   return (
@@ -14,10 +11,13 @@ const SecondSectionCarousel = ({ images }) => {
       <ErrorBoundary fallback={<h3>Error inside SecondSectionCarousel.jsx. Location = {location.pathname}.</h3>}>
         <Carousel className='react-bootstrap-carousel'>
           {
-            images.map(({src, caption, altText, key}, idx) => (
+            images.map(({src, caption, altText}, idx) => (
               <Item key={idx}>
                 <div className='img-div'>
-                  <img src={src} altText={altText}/>
+                  <img src={src} altText={altText} />
+                  <Caption>
+                    <h1 className='carousel-caption'>{caption}</h1>
+                  </Caption>
                 </div>                
               </Item>
             ))
