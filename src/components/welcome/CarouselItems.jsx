58bc355bda6b40f01e9f6f20858e0c0f0b91046b
admin_dho <davidhorph@hotmail.com>
2025-05-26 10:04:30 -0500

    Create CarouselItems.jsx
    A JSX page to hold my items for React Bootstrap carousel in Welcome page.

---
+++ src/components/welcome/CarouselItems.jsx
@@ -0,0 +1,24 @@
+import band from "../../media/images/musicians.jpg";
+import singer from "../../media/images/singers4.jpg";
+import trophy from "../../media/images/winner2.jpg";
+
+export const images = [
+   {
+      src: band,
+      caption: 'CREATE YOUR OWN TEAM...',
+      altText: 'band image.',
+      key: 1
+   },
+   {
+      src: singer,
+      caption: 'COMPETE AGAINST OTHERS...',
+      altText: 'singer image.',
+      key: 2
+   },
+   {
+      src: trophy,
+      caption: `WIN \n ${`(hopefully)`}!!!`,
+      altText: 'trophy.',
+      key: 3
+   }
+]
