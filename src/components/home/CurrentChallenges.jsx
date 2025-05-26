80d7de8d6bd21631e90886d5960d238eb3c4d408
admin_dho <davidhorph@hotmail.com>
2025-05-26 10:03:11 -0500

    Added CurrentChallenges.jsx.
    Purpose: To .map( ) out all the current challenges pulled in from fetch( ) request.

---
+++ src/components/home/CurrentChallenges.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { useFetch } from '../../functions/fetchapi';
+
+const CurrentChallenges = () => {
+  return (
+    <div>
+      <h1>CURRENT CHALLENGES.</h1>
+    </div>
+  )
+}
+
+export default CurrentChallenges
