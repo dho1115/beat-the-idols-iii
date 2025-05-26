90752d17ec4b296145b3a15b98b5e0ce729bef97
admin_dho <davidhorph@hotmail.com>
2025-05-26 10:55:25 -0500

    Added Suspense.

--- src/components/home/CurrentChallenges.jsx
+++ src/components/home/CurrentChallenges.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 
 import { useFetch } from '../../functions/fetchapi';
+import SuspenseFallback from '../suspense_fallback/SuspenseFallback';
 
 const CurrentChallenges = () => {
+  const [data, setData] = useFetch("http://localhost:3003/currentChallenges", []);
+  console.log("DATA IS: ", data);
+
   return (
     <div>
-      <h1>CURRENT CHALLENGES.</h1>
+      <Suspense fallback={<SuspenseFallback />}>
+        <h1>CURRENT CHALLENGES.</h1>
+        <h3>{JSON.stringify(data)}</h3>
+      </Suspense>      
     </div>
   )
 }
