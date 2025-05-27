cbdd4e24d91670a34a2a273b1e9d445a38c40eda
admin_dho <davidhorph@hotmail.com>
2025-05-27 18:59:42 -0500

    Update CurrentChallenges.jsx
    Added a conditional where if there is no data, a message appears.

--- src/components/home/CurrentChallenges.jsx
+++ src/components/home/CurrentChallenges.jsx
@@ -2,10 +2,21 @@ import React, { Suspense } from 'react';
 
 import { useFetch } from '../../functions/fetchapi';
 import SuspenseFallback from '../suspense_fallback/SuspenseFallback';
+import { Container } from 'reactstrap';
 
 const CurrentChallenges = () => {
   const [data, setData] = useFetch("http://localhost:3003/currentChallenges", []);
   console.log("DATA IS: ", data);
+
+  if (!data.length) {
+    return (
+      <div>
+        <Container className='my-5' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', border: '3px solid black', backgroundColor: 'peachpuff' }}>
+          <h1>SORRY... THERE ARE NO CURRENT CHALLENGES.</h1>
+        </Container>
+      </div>
+    )
+  }
 
   return (
     <div>
