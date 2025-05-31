1bb330e991a2c443e97086fbba206477283733df
admin_dho <davidhorph@hotmail.com>
2025-05-31 18:57:29 -0500

    Created Signup Modal and integrated...
    Successfully created the Signup form (<Signup ... />) and integrated it into RegistrationPage.jsx along with the toggle function and registrationModal.

---
+++ src/components/forms/registration/Signup.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+//Dependencies.
+import { Form, FormGroup, Input, Label, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
+
+import "./registrationforms.css"
+
+const Signup = ({text, registrationModal, toggle}) => {
+  return (
+    <Modal
+      isOpen={registrationModal.signUp}
+      toggle={toggle}
+      backdrop="static"
+    >
+      <ModalHeader style={{ backgroundColor: 'khaki' }} toggle={toggle}>
+        <h1 style={{textAlign: 'center', width: '100%'}}>{text}</h1>     
+      </ModalHeader>
+      <ModalBody style={{backgroundColor: 'lightgrey'}}>
+         <Form onSubmit={() => console.log("Submitted information!!!")} className='signupform'>
+          <FormGroup>
+            <Label for='username'>USERNAME</Label>
+            <Input type='text' id='username' placeholder='username' required />
+          </FormGroup>
+          <FormGroup>
+            <Label for='password'>PASSWORD</Label>
+            <Input type='password' id='password' placeholder='password' required />
+          </FormGroup>
+          <FormGroup>
+            <Label for='email'>E-MAIL</Label>
+            <Input type='email' id='email' placeholder='email' required />
+          </FormGroup>
+          <FormGroup>
+            <button type="submit" className="btn btn-danger btn-lg btn-block" style={{width: '100%'}}>SUBMIT</button>
+          </FormGroup>
+         </Form>
+      </ModalBody>
+    </Modal>
+  )
+}
+
+export default Signup
--- src/pages/registration/RegistrationPage.jsx
+++ src/pages/registration/RegistrationPage.jsx
@@ -1,7 +1,8 @@
-import React, {Suspense} from 'react'
+import React, {Suspense, useState} from 'react'
 
-//Components.
+//Dependencies.
 import Registration from '../../components/registration/Registration';
+import Signup from '../../components/forms/registration/Signup';
 import SuspenseFallback from '../../components/suspense_fallback/SuspenseFallback';
 
 //media.
@@ -11,16 +12,21 @@ import musicians from "../../media/videos/musicians.mp4"
 import "./RegistrationPage.styles.css";
 
 const RegistrationPage = () => {
-  return (
-   <div className='registration-page-div py-5'>
-      <Suspense fallback={<SuspenseFallback />}>
-         <div className='registration-buttons-div'>
-            <Registration title="REGISTER" link={singers} mediaFormat="uploadedVideo" />
-            <Registration title="LOGIN" link={musicians} mediaFormat="uploadedVideo" />
-         </div>
-      </Suspense>
-   </div>
-  )
+   const [registrationModal, setRegistrationModal] = useState({ signUp: false, login: false });
+   const toggleSignupModal = () => setRegistrationModal(prv => ({ ...prv, signUp: !registrationModal.signUp }));
+   const toggleLoginModal = () => setRegistrationModal(prv => ({ ...prv, login: !registrationModal.login }));
+
+   return (
+      <div className='registration-page-div py-5'>
+         <Suspense fallback={<SuspenseFallback />}>
+            <Signup text="SIGN UP!!!" registrationModal={registrationModal} toggle={toggleSignupModal} />
+            <div className='registration-buttons-div'>
+               <Registration title="REGISTER" link={singers} mediaFormat="uploadedVideo" toggle={toggleSignupModal} />
+               <Registration title="LOGIN" link={musicians} mediaFormat="uploadedVideo" toggle={toggleLoginModal} />
+            </div>
+         </Suspense>
+      </div>
+   )
 }
 
 export default RegistrationPage
