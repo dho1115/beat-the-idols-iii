11ff7911fff23292727986feed7db9f334381503
admin_dho <davidhorph@hotmail.com>
2025-05-31 17:11:15 -0500

    Imported videos and then made CSS changes.
    CSS changes to center videos.

--- src/pages/registration/RegistrationPage.jsx
+++ src/pages/registration/RegistrationPage.jsx
@@ -4,13 +4,20 @@ import React, {Suspense} from 'react'
 import Registration from '../../components/registration/Registration';
 import SuspenseFallback from '../../components/suspense_fallback/SuspenseFallback';
 
+//media.
+import singers from "../../media/videos/singingGroup.mp4";
+import musicians from "../../media/videos/musicians.mp4"
+
 import "./RegistrationPage.styles.css";
 
 const RegistrationPage = () => {
   return (
    <div className='registration-page-div py-5'>
       <Suspense fallback={<SuspenseFallback />}>
-         <h1>LOGIN AND SIGNUP GOES HERE.</h1>
+         <div className='registration-buttons-div'>
+            <Registration title="REGISTER" link={singers} mediaFormat="uploadedVideo" />
+            <Registration title="LOGIN" link={musicians} mediaFormat="uploadedVideo" />
+         </div>
       </Suspense>
    </div>
   )
