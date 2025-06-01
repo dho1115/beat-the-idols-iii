327146e32460fea023699645c31a4dbe639d9a9d
admin_dho <davidhorph@hotmail.com>
2025-06-01 01:11:14 -0500

    Update Signup.jsx
    1. Added useNavigate( ) to navigate to current user's homepage.
    
    2. Added userEffect( ) to ensure navigate will only work if currentUser has a _userID.

--- src/components/forms/registration/Signup.jsx
+++ src/components/forms/registration/Signup.jsx
@@ -1,6 +1,7 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 
 //Dependencies.
+import { useNavigate } from 'react-router-dom';
 import { Form, FormGroup, Input, Label, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 import { v4 } from 'uuid';
 import { dataContext } from '../../../App';
@@ -8,13 +9,24 @@ import { dataContext } from '../../../App';
 import "./registrationforms.css"
 
 const Signup = ({ text, registrationModal, toggle }) => {
+  const navigate = useNavigate();
   const { currentUser, setCurrentUser } = useContext(dataContext);
 
   const handleSignup = e => {
+    e.preventDefault();
     const _userID = v4();
-    console.log(`Submitted information for ${_userID}.`)
+    console.log(`Submitted information for ${_userID}.`);
     setCurrentUser(prv => ({ ...prv, _userID }));
   }
+
+  useEffect(() => {
+    if (currentUser._userID) navigate(`/currentUser/${currentUser._userID}/`)
+  
+    return () => {
+      
+    }
+  }, [currentUser._userID]) //Navigate to user's homepage only if there is a _userID.
+  
   
   return (
     <Modal
