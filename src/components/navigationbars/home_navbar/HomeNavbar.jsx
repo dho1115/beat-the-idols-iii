d5b440818a2478bfe20791c86f70a93709dbcccc
admin_dho <davidhorph@hotmail.com>
2025-06-01 09:50:31 -0500

    Changed the folder name to home_navbar

---
+++ src/components/navigationbars/home_navbar/HomeNavbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Card } from 'reactstrap'
+
+import "./HomeNavbar.styles.css";
+
+const HomeNavbar = ({ arrayOfLinks /* from Homepage.jsx */ }) => {
+   const navigate = useNavigate();
+
+   if (arrayOfLinks.length) {
+      return (
+         <nav className='homenavbar p-3'>
+            {
+               arrayOfLinks.map(({src, text}, idx) => {
+                  return (
+                     <Card className='homenavbar-card' key={idx} onClick={() => navigate("/home/current-challenges")}>
+                        <img src={src} alt={text} className='homenavbar-img' />
+                        <div className='text-div'>
+                           <h3>{text}</h3>
+                        </div>                        
+                     </Card>
+                  )
+               })
+            }
+         </nav>
+      )
+   } else {
+      return (
+         <nav>
+            <strong>NO LINKS YET.</strong>
+         </nav>
+      )
+   }
+}
+
+export default HomeNavbar
