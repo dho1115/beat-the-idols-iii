099045df0fcbceb4329dfac59b748c09454693a4
admin_dho <davidhorph@hotmail.com>
2025-06-04 17:20:02 -0500

    Update App.jsx
    Added new routes and createContext.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { createContext, useState } from 'react';
-import { lazy } from 'react';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-
-//Components.
-import WelcomeNavbar from './components/navigationbars/welcome/WelcomeNavbar';
 
 //Components - Lazy loaded.
-const CurrentChallenges = lazy(() => import("./components/home/CurrentChallenges"));
+import WelcomeNavbar from './components/navigationbars/welcome/WelcomeNavbar';
+
+//Dependencies.
+import { lazy } from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+// import { dataContext } from './contextprovider';
 
 //Pages - Lazy loaded.
 const AboutUsPage = lazy(() => import('./pages/about/AboutUsPage'));
@@ -15,11 +15,12 @@ const Homepage = lazy(() => import('./pages/home/Homepage'));
 const RegistrationPage = lazy(() => import('./pages/registration/RegistrationPage'));
 const WelcomePage = lazy(() => import('./pages/welcome/WelcomePage'));
 const CurrentUserHomepage = lazy(() => import('./pages/private_pages/userhomepage/CurrentUserHomepage'));
+const CurrentChallenges = lazy(() => import("./components/home/CurrentChallenges"));
 
 import './App.css';
 
-export const dataContext = createContext()
-
+export const dataContext = createContext();
+  
 function App() {
   const [currentUser, setCurrentUser] = useState({});
 
@@ -36,15 +37,9 @@ function App() {
           <Route path='/contact' element={<ContactPage />} />
           <Route path='/register' element={<RegistrationPage />} />
           {/* === PRIVATE ROUTES === */}
-          {
-            currentUser._userID
-            &
-            <>
-              <Route path='/currentUser/:currentUser._userID/*' element={<CurrentUserHomepage />}>
-                <Route path='' element={<h3>If you are seeing this, then you don't have child pages.</h3>} />
-              </Route>
-            </>
-          }
+          <Route path='/currentUser/:user/*' element={<CurrentUserHomepage />}>
+            <Route path='' element={<h3>If you are seeing this, then you don't have child pages.</h3>} />
+          </Route>
           {/* ====================== */}
           <Route path='*' element={<Navigate to="/" replace />} />
         </Routes>
