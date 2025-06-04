eaade6a8868519f801e52d75d274e12cb82d5cf7
admin_dho <davidhorph@hotmail.com>
2025-06-04 17:20:54 -0500

    Update WelcomeNavbar.jsx
    New routes.

--- src/components/navigationbars/welcome/WelcomeNavbar.jsx
+++ src/components/navigationbars/welcome/WelcomeNavbar.jsx
@@ -1,8 +1,9 @@
-import {useState} from 'react'
+import {useState, useEffect, useContext} from 'react'
 
 //Dependencies.
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
+import { dataContext } from '../../../App';
 
 //Media.
 import logo from "../../../media/images/BeatTheIdols_NoBackground.png";
@@ -11,8 +12,15 @@ import "./WelcomeNavbar.styles.css";
 
 const WelcomeNavbar = () => {
   const location = useLocation();
+  const { currentUser } = useContext(dataContext);
 
-  const [generalRoutes, setGeneralRoutes] = useState([{name: 'welcome', path: '/'}, {name: 'home', path: '/home'}, {name: 'about', path: '/about'}, {name: 'contact', path: '/contact'}, {name: 'register', path: '/register'}])
+  const [generalRoutes, setGeneralRoutes] = useState([{ name: 'welcome', path: '/' }, { name: 'home', path: '/home' }, { name: 'about', path: '/about' }, { name: 'contact', path: '/contact' }, { name: 'register', path: '/register' }])
+  
+  useEffect(() => {
+    if (currentUser._userID && currentUser.username) {
+      setGeneralRoutes(prv => ([...prv, {name: `Welcome, ${currentUser.username}!!!`, path: '/'}]))
+    }
+  }, [Object.values(currentUser).length]);
 
   return (
     <nav className='welcome-navbar p-1 mb-0 sticky-top' style={{position: 'absolute', top: '0%', left: '0%', right: '0%'}}>
