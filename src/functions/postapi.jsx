3bcc4154fe051cd57563a4e2671efa765d5ac365
admin_dho <davidhorph@hotmail.com>
2025-06-15 20:32:09 -0500

    Create postapi.jsx
    Created a usePost function.

---
+++ src/functions/postapi.jsx
@@ -0,0 +1,17 @@
+export const usePost = async (url, data) => {
+   try {
+      const jsonData = JSON.stringify(data);
+      const postData = await fetch(url, {
+         method: 'POST',
+         headers: { 'Content-Type': 'application/json' },
+         body: jsonData
+      });
+      console.log({ message: 'usePostSuccess!!!', jsonData, postData });
+
+      return { message: 'usePost success!!!', jsonData, postData };
+   } catch (error) {
+      console.error({ message: 'Error in usepost function', error, errorCode: error.code, status: error.status, errMessage: error.message });
+
+      return { error, errorCode: error.code, errorMessage: error.message };
+   }
+}
