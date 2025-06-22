efed3b34336face1321785ae3a5f894c15e92027
admin_dho <davidhorph@hotmail.com>
2025-06-22 11:41:56 -0500

    miscellaneous (personal for me. notes).

--- src/pages/welcome/WelcomePage.jsx
+++ src/pages/welcome/WelcomePage.jsx
@@ -10,11 +10,10 @@ import { dataContext } from '../../App';
 import "./WelcomePage.styles.css";
 
 const WelcomePage = () => {
-  // alert("From WelcomePage.jsx (route = '/'): PLEASE CHECK YOUR todo.txt!!!")
+  alert("From WelcomePage.jsx (route = '/'): PLEASE CHECK YOUR todo.txt!!!")
   const contextProvider = useContext(dataContext);
   const { currentUser } = contextProvider;
   const { id, username } = currentUser;
-  console.log({ contextProvider, currentUser, id, username });
 
   return (    
     <div className='welcome-page-wrapper p-0'>
