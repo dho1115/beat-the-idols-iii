5c2b4a7bbe2b277624732a5c39aa24351cce19fc
admin_dho <davidhorph@hotmail.com>
2025-06-23 09:27:54 -0500

    Update CurrentUserHomepage.jsx
    Created a logoutpromise, a function that returns a Promise.all[ logoutcurrentUserPromise, PostDataAPI ] to be used in the logout onclick.

--- src/pages/private_pages/userhomepage/CurrentUserHomepage.jsx
+++ src/pages/private_pages/userhomepage/CurrentUserHomepage.jsx
@@ -4,7 +4,7 @@ import React, { Suspense, useContext, useEffect } from 'react'
 import { dataContext } from '../../../App';
 import { Outlet } from 'react-router-dom';
 import { Container } from 'reactstrap';
-import { usePost } from '../../../functions/postapi';
+import { PostDataAPI } from '../../../functions/postapi';
 
 //Components.
 import SuspenseFallback from '../../../components/suspense_fallback/SuspenseFallback';
@@ -12,10 +12,20 @@ import SuspenseFallback from '../../../components/suspense_fallback/SuspenseFall
 const CurrentUserHomepage = () => {
    const { currentUser, setCurrentUser, mainRoutes, setMainRoutes } = useContext(dataContext);
    const { username, email } = currentUser;
-   
+
+   const logoutCurrentUserPromise = () => new Promise((res, rej) => {
+      setCurrentUser({ username: "", password: "", id: "", email: "" });
+      if (!currentUser.username && !currentUser.id) res({ message: "SUCCESSFULLY LOGGED OUT CURRENT USER FROM STATE!!!", currentUser });
+      else rej({ message: "ERROR TRYING TO LOG OUT CURRENT USER FROM STATE!!!", currentUser });
+   })
+
+   const logoutpromise = () => Promise.all([logoutCurrentUserPromise, PostDataAPI("http://localhost:3003/currentUser", { username: "", password: "", id: "", email: "" })])
+      .then(result => console.log(result))
+      .catch(error => console.error({ message: "logoutpromise ERROR!!!", error, errorMessage: error.message, errorCode: error.code }));
+
    useEffect(() => {
-      !mainRoutes.filter(({name}) => name == 'LOG OUT!!!').length && setMainRoutes(prv => ([...prv, { name: 'LOG OUT!!!', path: '/', onClick: () => console.log('LOG OUT!!!') }]))
-   }, [])
+      !mainRoutes.filter(({name}) => name == 'LOG OUT!!!').length && setMainRoutes(prv => ([...prv, { name: 'LOG OUT!!!', path: '/', onClick: logoutpromise }]))
+   }, []) //This prevents duplication of LOG OUT link. If there is no logout (!mainroutes.filter), it will add a LOG OUT link.
 
    try {
       return (
