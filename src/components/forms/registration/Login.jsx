c913e0636a21cee41df586ac8ca408bf190459e6
admin_dho <davidhorph@hotmail.com>
2025-06-24 14:51:03 -0500

    Update Login.jsx
    created logic for onHandleSubmit.
    
    Also, created useEffect( fn, [currentUser.id, currentUser.username) that will navigate the currentUser (if there is one) to the user's homepage (see logic inside that useEffect hook).

--- src/components/forms/registration/Login.jsx
+++ src/components/forms/registration/Login.jsx
@@ -1,28 +1,50 @@
 import React, {useEffect, useContext, useState} from 'react'
+import { useNavigate } from 'react-router-dom';
 
 //Dependencies.
-import { Form, Modal, ModalHeader, ModalBody, FormGroup, Input, Label } from 'reactstrap'
+import { Form, Modal, ModalHeader, ModalBody, FormGroup, Input, Label, Alert } from 'reactstrap'
 import { dataContext } from '../../../App'
-import { fetchDataAPI } from '../../../functions/fetchapi'
 
 import "./registrationforms.css"
 
 const Login = ({ text, registrationModal, toggle }) => {
-  const { currentUser, setCurrentUser } = useContext(dataContext);
+  const { currentUser, setCurrentUser, allUsers } = useContext(dataContext);
   const [currentUserNotFound, setCurrentUserNotFound] = useState(false);
+  const [dataEntered, setDataEntered] = useState({ username: "", password: "" });
+  const navigate = useNavigate()
 
   const onHandleSubmit = e => {
     e.preventDefault();
+    const loggedInUserExists = allUsers.find(({ username, password }) => (username == dataEntered.username && password == dataEntered.password));    
     try {
-      
+      if (!(loggedInUserExists.username && loggedInUserExists.password)) throw new Error(`There is no such user with username and password of ${JSON.stringify(dataEntered)}!!!`);
+      else {
+        setCurrentUserNotFound(false);
+        console.log("FOUND THIS USER: ", loggedInUserExists);
+        setCurrentUser(prv => ({ ...prv, ...loggedInUserExists }));
+      }
     } catch (error) {
-      
+      console.error({ message: "error in onHandleSubmit function in Login.jsx!!!", error, errorMessage: error.message })
+      setCurrentUserNotFound(true);
     }
   }
 
   useEffect(() => {
-    return () => setCurrentUserNotFound(false)
-  }, [])
+    if (currentUser.id && currentUser.username) navigate(`/currentUser/${currentUser.id}`);
+    toggle();
+    
+    return () => {
+      setCurrentUserNotFound(false);
+      setDataEntered({ username: "", password: "" });
+    };
+  }, [currentUser.id && currentUser.username])
+
+  useEffect(() => {
+    return () => {
+      setCurrentUserNotFound(false);
+      setDataEntered({ username: "", password: "" });
+    }
+  }, []);
 
   return (
     <Modal
@@ -37,11 +59,11 @@ const Login = ({ text, registrationModal, toggle }) => {
         <Form onSubmit={onHandleSubmit}>
           <FormGroup>
             <Label for='username'>USERNAME</Label>
-            <Input type='text' placeholder='username' id='username' required />
+            <Input type='text' placeholder='username' value={dataEntered.username} id='username' required onChange={e => setDataEntered(prv => ({...prv, username: e.target.value }))}/>
           </FormGroup>
           <FormGroup>
             <Label for='password'>PASSWORD</Label>
-            <Input type='password' placeholder='password' id='password' required />
+            <Input type='password' placeholder='password' value={dataEntered.password} id='password' required onChange={e => setDataEntered(prv => ({...prv, password: e.target.value }))} />
           </FormGroup>
           <FormGroup>
             <button type="submit" class="btn btn-danger btn-lg btn-block">LOGIN</button>
