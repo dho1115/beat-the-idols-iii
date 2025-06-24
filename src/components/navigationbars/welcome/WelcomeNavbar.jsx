24fdc02e41831bbacef1518de51f017b541cca6a
admin_dho <davidhorph@hotmail.com>
2025-06-24 14:24:33 -0500

    Update WelcomeNavbar.jsx
    Managed to create dynamic main routes where different links show, depending on whether there is an id or username (id && username):
    
        const updatedMainRoutes =
          (id && username) ?
            [...mainRoutes, { name: `Welcome, ${username}!!!`, path: `/currentUser/${username}` }]
            :
            [...mainRoutes, { name: "LOGIN/SIGN UP!!!", path: '/register' }]
          .map(val => {
            if (filterUniqueName.has(val.name)) false;
            else {
              filterUniqueName.add(val.name);
              return val
            }
          })

--- src/components/navigationbars/welcome/WelcomeNavbar.jsx
+++ src/components/navigationbars/welcome/WelcomeNavbar.jsx
@@ -9,26 +9,29 @@ import { dataContext } from '../../../App';
 import logo from "../../../media/images/BeatTheIdols_NoBackground.png";
 
 import "./WelcomeNavbar.styles.css";
-import { stringify } from 'uuid';
 
 const WelcomeNavbar = () => {
   const location = useLocation();
-  const { currentUser, setCurrentUser, mainRoutes, setMainRoutes } = useContext(dataContext);
+  const { currentUser, allUsers, currentChallenges, mainRoutes, setMainRoutes } = useContext(dataContext);
   const { id, username } = currentUser;
 
   useEffect(() => {
     const filterUniqueName = new Set()
-    if (id && username) {
-      const updatedMainRoutes = [...mainRoutes, { name: `Welcome, ${username}!!!`, path: `/currentUser/${username}` }].map(val => {
+    const updatedMainRoutes =
+      (id && username) ?
+        [...mainRoutes, { name: `Welcome, ${username}!!!`, path: `/currentUser/${username}` }]
+        :
+        [...mainRoutes, { name: "LOGIN/SIGN UP!!!", path: '/register' }]
+      .map(val => {
         if (filterUniqueName.has(val.name)) false;
         else {
           filterUniqueName.add(val.name);
           return val
         }
       })
-      setMainRoutes(updatedMainRoutes);
-    }
-  }, [id, username]); //Checks to see if there is a current user.
+    setMainRoutes(updatedMainRoutes)
+    
+  }, []); //Checks to see if there is a current user.
 
   return (
     <nav className='welcome-navbar p-1 mb-0 sticky-top' style={{position: 'absolute', top: '0%', left: '0%', right: '0%'}}>
