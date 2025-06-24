d2dd36c7653e51410d50b4ecf5e7d4cf15b309d3
admin_dho <davidhorph@hotmail.com>
2025-06-24 14:26:25 -0500

    started onHandleSubmit for the Login.jsx.

--- src/components/forms/registration/Login.jsx
+++ src/components/forms/registration/Login.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, {useEffect, useContext, useState} from 'react'
 
+//Dependencies.
 import { Form, Modal, ModalHeader, ModalBody, FormGroup, Input, Label } from 'reactstrap'
+import { dataContext } from '../../../App'
+import { fetchDataAPI } from '../../../functions/fetchapi'
 
 import "./registrationforms.css"
 
-const Login = ({text, registrationModal, toggle}) => {
+const Login = ({ text, registrationModal, toggle }) => {
+  const { currentUser, setCurrentUser } = useContext(dataContext);
+  const [currentUserNotFound, setCurrentUserNotFound] = useState(false);
+
+  const onHandleSubmit = e => {
+    e.preventDefault();
+    try {
+      
+    } catch (error) {
+      
+    }
+  }
+
+  useEffect(() => {
+    return () => setCurrentUserNotFound(false)
+  }, [])
+
   return (
     <Modal
       isOpen={registrationModal.login}
@@ -15,7 +34,7 @@ const Login = ({text, registrationModal, toggle}) => {
         {text}
       </ModalHeader>
       <ModalBody style={{backgroundColor: 'lightgrey'}}>
-        <Form onSubmit={e => console.log("submitted form")}>
+        <Form onSubmit={onHandleSubmit}>
           <FormGroup>
             <Label for='username'>USERNAME</Label>
             <Input type='text' placeholder='username' id='username' required />
