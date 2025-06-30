e7fa66b2fc8ab5dfe52590626771d3c4ed39c647
admin_dho <davidhorph@hotmail.com>
2025-06-30 12:19:24 -0500

    Create welcome_navbar_links.jsx

---
+++ src/components/navigationbars/welcome/welcome_navbar_links.jsx
@@ -0,0 +1,5 @@
+export const welcomeNavbarLinks = [
+   { name: 'welcome', path: '/' },
+   { name: 'home', path: '/home' },
+   { name: 'about', path: '/about' }, { name: 'contact', path: '/contact' }
+]
