4e4fa524e332475cf40847d7292499b4bb6a228d
admin_dho <davidhorph@hotmail.com>
2025-07-04 12:43:31 -0500

    Fixed the automatic pop up for login modal.
    Solved the problem where the login modal automatically pups up by moving the toggle inside of the if (currentUser.id && currentUser.username):
    
        if (currentUser.id && currentUser.username) {
          navigate(`/currentUser/${currentUser.id}`);
          toggle();
        }

--- src/components/forms/registration/Login.jsx
+++ src/components/forms/registration/Login.jsx
@@ -9,15 +9,14 @@ import { PostDataAPI } from '../../../functions/postapi';
 import "./registrationforms.css"
 
 const Login = ({ text, registrationModal, toggle }) => {
-  const { currentUser, setCurrentUser, allUsers, welcomeLinks, setWelcomeLinks } = useContext(dataContext);
+  const { currentUser, allUsers, welcomeLinks, setWelcomeLinks } = useContext(dataContext);
   const [currentUserNotFound, setCurrentUserNotFound] = useState(false);
   const [dataEntered, setDataEntered] = useState({ username: "", password: "" });
-  console.log(dataEntered)
   const navigate = useNavigate()
 
   const onHandleSubmit = e => {
     e.preventDefault();
-    const loggedInUserExists = allUsers.find((val) => ((val.username == dataEntered.username) && (val.password == dataEntered.password)));
+    const loggedInUserExists = allUsers.find((val) => ((val.username == dataEntered.username) && (val.password == dataEntered.password)))
     
     try {
       if (!loggedInUserExists) throw new Error(`There is no such user with username and password of ${JSON.stringify(dataEntered)}!!!`);
@@ -44,15 +43,18 @@ const Login = ({ text, registrationModal, toggle }) => {
           .then(() => navigate(`/currentUser/${currentUser.id}`))
           .catch(error => console.error({ from: "Login.jsx", message: "Error inside onHandleSubmit!!!", error, errorMessage: error.message }));
       }
-    } catch (error) {
+    }
+    catch (error) {
       console.error({ message: "error in onHandleSubmit function in Login.jsx!!!", error, errorMessage: error.message })
       setCurrentUserNotFound(true);
     }
   }
 
   useEffect(() => {
-    if (currentUser.id && currentUser.username) navigate(`/currentUser/${currentUser.id}`);
-    toggle();
+    if (currentUser.id && currentUser.username) {
+      navigate(`/currentUser/${currentUser.id}`);
+      toggle();
+    }
 
     return () => {
       setCurrentUserNotFound(false);
@@ -77,15 +79,15 @@ const Login = ({ text, registrationModal, toggle }) => {
         {text}
         {currentUserNotFound && <Alert color='danger'>There is no user that matches your entry of {JSON.stringify(dataEntered)}!!!</Alert>}
       </ModalHeader>
-      <ModalBody style={{backgroundColor: 'lightgrey'}}>
+      <ModalBody style={{ backgroundColor: 'lightgrey' }}>
         <Form onSubmit={onHandleSubmit}>
           <FormGroup>
             <Label for='username'>USERNAME</Label>
-            <Input type='text' placeholder='username' value={dataEntered.username} id='username' required onChange={e => setDataEntered(prv => ({...prv, username: e.target.value }))}/>
+            <Input type='text' placeholder='username' value={dataEntered.username} id='username' required onChange={e => setDataEntered(prv => ({ ...prv, username: e.target.value }))} />
           </FormGroup>
           <FormGroup>
             <Label for='password'>PASSWORD</Label>
-            <Input type='password' placeholder='password' value={dataEntered.password} id='password' required onChange={e => setDataEntered(prv => ({...prv, password: e.target.value }))} />
+            <Input type='password' placeholder='password' value={dataEntered.password} id='password' required onChange={e => setDataEntered(prv => ({ ...prv, password: e.target.value }))} />
           </FormGroup>
           <FormGroup>
             <button type="submit" class="btn btn-danger btn-lg btn-block">LOGIN</button>
