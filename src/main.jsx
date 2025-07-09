8016d32e8568fa6638fb466d600d848785717f4e
admin_dho <davidhorph@hotmail.com>
2025-07-09 12:14:44 -0500

    Moved BrowserRouter.
    Moved BrowserRouter to see if the link for the User's Homepage goes away (now, it only shows once, when it navigates to the homepage after login).

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import ErrorBoundary from './components/ErrorBoundary.jsx';
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
-  <ErrorBoundary fallback={<div style={{ height: '100vh', backgroundColor: 'lightgreen'}}><h1>Something went wrong inside main.jsx file.</h1></div>}>
-    <App />
-  </ErrorBoundary>  
+  <BrowserRouter>
+    <ErrorBoundary fallback={<div style={{ height: '100vh', backgroundColor: 'lightgreen' }}><h1>Something went wrong inside main.jsx file.</h1></div>}>
+      <App />
+    </ErrorBoundary>
+  </BrowserRouter>  
 )
