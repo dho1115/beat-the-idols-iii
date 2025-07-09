6ce7ce8589f35b75002c491f8062888d8c3e78a3
admin_dho <davidhorph@hotmail.com>
2025-07-09 09:14:45 -0500

    Update welcome_navbar_links.jsx
    I changed welcomeNavbarLinks to be a function that takes in username and id as an arguement.
    
    Depending on whenter it receives a username or id, the loggedInRoutes could return different links and thus, the links returned could be different (dynamic, depending on logged in user status).

--- src/components/navigationbars/welcome/welcome_navbar_links.jsx
+++ src/components/navigationbars/welcome/welcome_navbar_links.jsx
@@ -1,5 +1,21 @@
-export const welcomeNavbarLinks = [
-   { name: 'welcome', path: '/' },
-   { name: 'home', path: '/home' },
-   { name: 'about', path: '/about' }, { name: 'contact', path: '/contact' }
-]
+export const welcomeNavbarLinks = (username = null, id = null, ...args) => {
+   let options = args;
+   if (args.length) options.reduce((accumulator, current) => {
+      accumulator = { ...accumulator, ...current };
+      return accumulator;
+   }, {});
+
+   const baseRoutes= [
+      { name: 'welcome', path: '/' },
+      { name: 'home', path: '/home' },
+      { name: 'about', path: '/about' },
+      { name: 'contact', path: '/contact' }
+   ];
+   const loggedInRoutes = (id && username)
+      ? 
+      [{ name: `${username}'s homepage`, path: `/currentUser/${username}` }, {name: 'LOGOUT', path: "/", onClick: () => console.log("LOGOUT LOGIC HERE!!!")}]
+      :
+      [{ name: 'LOGIN/SIGN UP!!!', path: '/register' }]
+   
+   return [...baseRoutes, ...loggedInRoutes];
+}
