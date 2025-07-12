f4efa6676ae96052e7e6403fecd415c0aba9a3e8
admin_dho <davidhorph@hotmail.com>
2025-07-12 13:29:33 -0500

    Removed the logout logic
    Removed logoutCurrentUserPromose because that logic is now inside App.jsx and that function is passed when I setWelcomeLinks inside useEffect( ) of App.jsx.

--- src/pages/private_pages/userhomepage/CurrentUserHomepage.jsx
+++ src/pages/private_pages/userhomepage/CurrentUserHomepage.jsx
@@ -1,31 +1,16 @@
-import React, { Suspense, useContext, useEffect } from 'react'
+import React, { Suspense, useContext } from 'react'
 
 //Dependencies.
 import { dataContext } from '../../../App';
 import { Outlet } from 'react-router-dom';
 import { Container } from 'reactstrap';
-import { PostDataAPI } from '../../../functions/postapi';
 
 //Components.
 import SuspenseFallback from '../../../components/suspense_fallback/SuspenseFallback';
 
 const CurrentUserHomepage = () => {
-   const { currentUser, setCurrentUser, welcomeLinks, setWelcomeLinks } = useContext(dataContext);
+   const { currentUser } = useContext(dataContext);
    const { username, email } = currentUser;
-
-   const logoutCurrentUserPromise = () => new Promise((res, rej) => {
-      setCurrentUser({});
-      if (!currentUser.username && !currentUser.id) res({ message: "SUCCESSFULLY LOGGED OUT CURRENT USER FROM STATE!!!", currentUser });
-      else rej({ message: "ERROR TRYING TO LOG OUT CURRENT USER FROM STATE!!!", currentUser });
-   })
-
-   const logoutpromise = () => Promise.all([logoutCurrentUserPromise(), PostDataAPI("http://localhost:3003/currentUser", { })])
-      .then(result => {
-         console.log({ result });
-         const updatedWelcomeLinks = [...welcomeLinks].filter(({ name }) => name.endsWith("'s homepage"));
-         setWelcomeLinks([...updatedWelcomeLinks, { name: 'LOGIN/SIGN UP!!!', path: '/register' }]);
-      })
-      .catch(error => console.error({ message: "logoutpromise ERROR!!!", error, errorMessage: error.message, errorCode: error.code }));
 
    try {
       return (
