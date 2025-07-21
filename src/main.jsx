1c6de2769c97a7f7272308c98b3a679241a1278c
admin_dho <davidhorph@hotmail.com>
2025-07-21 09:53:30 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ErrorBoundary from './components/ErrorBoundary.jsx';
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
