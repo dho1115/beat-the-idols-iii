c966c2114c86b9cb794752d06c7fe94d636a5ec7
admin_dho <davidhorph@hotmail.com>
2025-07-21 10:02:30 -0500

    Commented out <img />.
    Commented out image. Will replace with SlideOne that I just imported.

--- src/components/forms/challenge/ChallengeForm.jsx
+++ src/components/forms/challenge/ChallengeForm.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
+
+//Dependencies and Components.
 import { Container } from 'reactstrap';
 import { Carousel } from 'react-bootstrap';
 import { images } from './testImages';
+import SlideOne from './components/SlideOne';
 
 import './ChallengeForm.styles.css';
 
@@ -12,12 +15,17 @@ const ChallengeForm = () => {
 
   return (
     <Container>
-      <Carousel activeIndex={index} interval={null} onSelect={handleSelect}>
+      <Carousel
+        activeIndex={index}
+        indicators={false}
+        interval={null}
+        onSelect={handleSelect}
+      >
         {
           images.map(({ src, alt }, idx) => {
             return (
               <Item key={idx} style={{ border: '1.5px solid black', backgroundColor: 'yellow', overflow: 'hidden', height: '75vh'}}>
-                <img src={src} alt={alt} height='100%' width='100%' style={{objectFit: 'cover'}} />
+                {/* <img src={src} alt={alt} height='100%' width='100%' style={{objectFit: 'cover'}} /> */} {/* commented out image. About to replace with SlideOne */} 
               </Item>
             )
           })
