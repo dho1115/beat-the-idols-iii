e95c2f4ce81bb797c69ea3af7bc825b906ec5193
admin_dho <davidhorph@hotmail.com>
2025-07-21 18:48:31 -0500

    ChallengeForm.jsx. Context, submit logic.
    1. Imported useContext and context.
    2. Used <Item /> (formerly in SlideOne).
    3. Submit logic: handleSubmit.

--- src/components/forms/challenge/ChallengeForm.jsx
+++ src/components/forms/challenge/ChallengeForm.jsx
@@ -1,36 +1,46 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 
 //Dependencies and Components.
-import { Container } from 'reactstrap';
+import { Container, Form } from 'reactstrap';
 import { Carousel } from 'react-bootstrap';
-import { images } from './testImages';
+import { dataContext } from '../../../App';
 import SlideOne from './components/SlideOne';
 
 import './ChallengeForm.styles.css';
 
 const ChallengeForm = () => {
+  const { challengeAnnouncements, setChallengeAnnouncements, currentChallenges, setCurrentChallenges, currentUser } = useContext(dataContext);
   const [index, setIndex] = useState(0);
   const handleSelect = selectedIndex => setIndex(selectedIndex);
   const { Item } = Carousel;
 
+  const slides = [<SlideOne />, <SlideOne />, <SlideOne />]
+  
+  const handleSubmit = e => {
+    e.preventDefault;
+  }
+
   return (
-    <Container>
-      <Carousel
-        activeIndex={index}
-        indicators={false}
-        interval={null}
-        onSelect={handleSelect}
-      >
-        {
-          images.map(({ src, alt }, idx) => {
-            return (
-              <Item key={idx} style={{ border: '1.5px solid black', backgroundColor: 'yellow', overflow: 'hidden', height: '75vh'}}>
-                {/* <img src={src} alt={alt} height='100%' width='100%' style={{objectFit: 'cover'}} /> */} {/* commented out image. About to replace with SlideOne */} 
-              </Item>
-            )
-          })
-        }
-      </Carousel>
+    <Container className='challenge-form-container p-0'>
+      <Form>
+        <Carousel
+          activeIndex={index}
+          indicators={false}
+          interval={null}
+          onSelect={handleSelect}
+          variant='dark'
+        >
+          {
+            slides.map((val, idx) => {
+              return (
+                <Item className="carousel-item"  key={idx}>
+                  {val}
+                </Item>
+              )
+            })
+          }
+        </Carousel>
+      </Form>      
     </Container>        
   )
 }
