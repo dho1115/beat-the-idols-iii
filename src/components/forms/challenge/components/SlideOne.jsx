a522ad2e832ad5ffb4f3a490bc9624b5505ebd6c
admin_dho <davidhorph@hotmail.com>
2025-07-21 09:51:46 -0500

    Update SlideOne.jsx
    Changed <></> to <Item></Item.

--- src/components/forms/challenge/components/SlideOne.jsx
+++ src/components/forms/challenge/components/SlideOne.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import { FormGroup, Input, Label } from 'reactstrap'
+import { Carousel } from 'react-bootstrap';
 
 const SlideOne = () => {
-  return (
-   <>
-      <FormGroup>
-         <Label for='title'>TITLE OF CHALLENGE</Label>
-         <Input type='text' id='title' placeholder='title' required />
-      </FormGroup>
-      <FormGroup>
-         <Label for='description'>DESCRIPTION</Label>
-         <Input type='textarea' id='description' placeholder='Brief description of challenge' maxLength={109} />
-      </FormGroup>
-   </>
-  )
+   const { Item } = Carousel;
+   return (
+      <Item>
+         <FormGroup>
+            <Label for='title'>TITLE OF CHALLENGE</Label>
+            <Input type='text' id='title' placeholder='title' required />
+         </FormGroup>
+         <FormGroup>
+            <Label for='description'>DESCRIPTION</Label>
+            <Input type='textarea' id='description' placeholder='Brief description of challenge' maxLength={109} />
+         </FormGroup>
+      </Item>
+   )
 }
 
 export default SlideOne
