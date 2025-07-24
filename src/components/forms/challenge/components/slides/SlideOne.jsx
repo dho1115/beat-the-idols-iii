4ce16dfce8d990c6d0ef820326d58466610a232f
admin_dho <davidhorph@hotmail.com>
2025-07-24 16:32:49 -0500

    Update SlideOne.jsx
    Removed dataContext. The data for the Challenge form will be temporarily be stored in ChallengeFormContext, then transferred into dataContext.

--- src/components/forms/challenge/components/slides/SlideOne.jsx
+++ src/components/forms/challenge/components/slides/SlideOne.jsx
@@ -2,22 +2,25 @@ import React, { useContext } from 'react';
 
 //Dependencies.
 import { FormGroup, Label, Input } from 'reactstrap';
-import { dataContext } from '../../../../../App';
+import { ChallengeFormContext } from '../../ChallengeForm';
 
 import "./Slide.styles.css";
 
 const SlideOne = () => {
-   
+   const { challengeFormDetails, setChallengeFormDetails } = useContext(ChallengeFormContext);
+
    return (
       <div className='slide-div'>
-         <FormGroup>
-            <Label for='title'>TITLE OF CHALLENGE</Label>
-            <Input type='text' id='title' placeholder='title' required />
-         </FormGroup>
-         <FormGroup>
-            <Label for='description'>DESCRIPTION</Label>
-            <Input type='textarea' id='description' placeholder='Brief description of challenge' maxLength={109} />
-         </FormGroup>
+         <div style={{width: '100%'}}>
+            <FormGroup>
+               <Label for='title'>TITLE OF CHALLENGE</Label>
+               <Input type='text' id='title' placeholder='title' required onChange={e => setChallengeFormDetails({...challengeFormDetails, title: e.target.value})} />
+            </FormGroup>
+            <FormGroup>
+               <Label for='description'>DESCRIPTION</Label>
+               <Input type='textarea' id='description' placeholder='Brief description of challenge' maxLength={109} onChange={e => setChallengeFormDetails({...challengeFormDetails, description: e.target.value})} />
+            </FormGroup>
+         </div>
       </div>
    )
 }
