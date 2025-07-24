a16b9a92134314ff4935c746bafb1e11d54827c6
admin_dho <davidhorph@hotmail.com>
2025-07-24 16:34:29 -0500

    Update ChallengeForm.jsx
    Modified handleSubmit. Changed e.preventDefault to e.prevnetDefault( ) (called the function).
    
    The details on the carousel works. Data shows.
    
    Imported SlideThree component which is (for now), just a button.

--- src/components/forms/challenge/ChallengeForm.jsx
+++ src/components/forms/challenge/ChallengeForm.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 
 //Dependencies and Components.
 import { Container, Form } from 'reactstrap';
@@ -7,42 +7,56 @@ import { dataContext } from '../../../App';
 import ItemDiv from './components/ItemDiv';
 import SlideOne from './components/slides/SlideOne';
 import SlideTwo from './components/slides/SlideTwo';
+import SlideThree from './components/slides/SlideThree';
+
+export const ChallengeFormContext = createContext();
 
 import './ChallengeForm.styles.css';
 
 const ChallengeForm = () => {
   const { challengeAnnouncements, setChallengeAnnouncements, currentChallenges, setCurrentChallenges, currentUser } = useContext(dataContext);
+  const [challengeFormDetails, setChallengeFormDetails] = useState({});
   const [index, setIndex] = useState(0);
   const handleSelect = selectedIndex => setIndex(selectedIndex);
   const { Item } = Carousel;
 
-  const slides = [<SlideOne />, <SlideTwo />]
+  const slides = [<SlideOne />, <SlideTwo />, <SlideThree />]
   
+  useEffect(() => {
+    console.log({ challengeFormDetails });
+    return () => setChallengeFormDetails({});
+  }, [])
+
   const handleSubmit = e => {
-    e.preventDefault;
+    e.preventDefault();
+    console.log("======= SUBMITTED THE FOLLOWING: =======");
+    console.log(challengeFormDetails);
+    console.log("========================================");
   }
 
   return (
     <Container className='challenge-form-container p-0'>
-      <Form>
-        <Carousel
-          activeIndex={index}
-          indicators={false}
-          interval={null}
-          onSelect={handleSelect}
-          variant='dark'
-        >
-          {
-            slides.map((val, idx) => {
-              return (
-                <Item className="carousel-item"  key={idx}>
-                  <ItemDiv slide={val} />
-                </Item>
-              )
-            })
-          }
-        </Carousel>
-      </Form>      
+      <ChallengeFormContext.Provider value = {{handleSubmit, challengeFormDetails, setChallengeFormDetails}}>
+        <Form onSubmit={handleSubmit}>
+          <Carousel
+            activeIndex={index}
+            indicators={false}
+            interval={null}
+            onSelect={handleSelect}
+            variant='dark'
+          >
+            {
+              slides.map((val, idx) => {
+                return (
+                  <Item className="carousel-item"  key={idx}>
+                    <ItemDiv slide={val} />
+                  </Item>
+                )
+              })
+            }
+          </Carousel>
+        </Form>
+      </ChallengeFormContext.Provider>            
     </Container>        
   )
 }
