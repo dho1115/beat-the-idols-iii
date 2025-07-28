46bde860b51325d827c5caa9269eb69cc6528104
admin_dho <davidhorph@hotmail.com>
2025-07-28 14:00:59 -0500

    Update YouTubeUpload.jsx
    Change in the onChange logic.

--- src/components/forms/add_video/video_type_formgroup/type_youtube/YouTubeUpload.jsx
+++ src/components/forms/add_video/video_type_formgroup/type_youtube/YouTubeUpload.jsx
@@ -4,7 +4,7 @@ const YouTubeUpload = ({currentState, setStateFunction}) => {
   return (
     <FormGroup>
       <Label for='you-tube'>ADD YOU-TUBE URL.</Label>
-      <Input type='text' value={currentState} placeholder='You-Tube url' onChange={setStateFunction} required/>
+      <Input type='text' value={currentState} placeholder='You-Tube url' onChange={e => setStateFunction(prv => ({...prv, urlOrFile: e.target.value}))} required/>
     </FormGroup>
   )
 }
