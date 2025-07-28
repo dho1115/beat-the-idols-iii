79e2edef723aeb5f1b1ca6e07b6358dcd0e65dcf
admin_dho <davidhorph@hotmail.com>
2025-07-28 11:43:14 -0500

    Created VideoWrapper.
    <VideoWrapper /> component, which is a <Card></Card> component to contain the video.

---
+++ src/components/templates/video_wrapper/VideoWrapper.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+//Dependencies & components.
+import { Card, CardText, CardTitle } from 'reactstrap';
+
+const VideoWrapper = ({video, title, description, button_text, clickLogic}) => {
+  return (
+    <Card style={{border: '1.5px solid black', borderRadius: '15px'}}>
+      <div className='w-100 p-0 card-video-div'>
+         {video}
+      </div>
+      <CardTitle>
+         <strong>{title}</strong>
+      </CardTitle>
+      <CardText>
+         {description}
+      </CardText>
+      <div className='w-100'>
+         <button type="button" className="btn btn-danger btn-lg btn-block" onClick={clickLogic || null}>{button_text}</button>
+      </div>
+    </Card>
+  )
+}
+
+export default VideoWrapper
