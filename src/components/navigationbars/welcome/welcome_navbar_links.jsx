9fd7e07222d2fa100b4e4c8ceed68e11b5620669
admin_dho <davidhorph@hotmail.com>
2025-07-29 22:18:42 -0500

    New Link added.
    {name: 'challenge videos', path: `/currentUser/${id}//view/challengeVideos/${options.all}

--- src/components/navigationbars/welcome/welcome_navbar_links.jsx
+++ src/components/navigationbars/welcome/welcome_navbar_links.jsx
@@ -1,5 +1,9 @@
 export const welcomeNavbarLinks = (username = null, id = null, ...args) => {
-   let options = args;
+   let options = Array.from(args => args).reduce((acc, value) => {
+      acc[value] = value;
+      return acc;
+   }, {});
+
    if (args.length) {
       options = options.reduce((accumulator, current) => {
          accumulator = { ...accumulator, ...current };
@@ -14,7 +18,7 @@ export const welcomeNavbarLinks = (username = null, id = null, ...args) => {
    ];
    const loggedInRoutes = (id && username)
       ?
-      [{ name: `${username}'s homepage`, path: `/currentUser/${id}` }, {name: 'LOGOUT', path: "/", onClick: options.logoutLogic}]
+      [{ name: `${username}'s homepage`, path: `/currentUser/${id}` }, {name: 'LOGOUT', path: "/", onClick: options.logoutLogic}, {name: 'challenge videos', path: `/currentUser/${id}//view/challengeVideos/${options.all}`}]
       :
       [{ name: 'LOGIN/SIGN UP!!!', path: '/register' }]
    
