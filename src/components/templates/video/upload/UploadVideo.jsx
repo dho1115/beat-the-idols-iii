fb4540317a3c7360ca54aa23ec5d84784ce211b2
admin_dho <davidhorph@hotmail.com>
2025-08-02 21:45:44 -0500

    Create UploadVideo.jsx
    Component designed to for users who want to upload a video as opposed to using YouTube. To be used in conjunction with VideoWrapper.jsx.

---
+++ src/components/templates/video/upload/UploadVideo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import "./UploadVideo.styles.css";
+
+const UploadVideo = ({link}) => {
+  return (
+    <video muted loop autoPlay className='upload-video-wrapper'>
+      <source src={link} type='video/mp4' />
+      <source src={link} type='video/ogg' />
+      <strong>Your browser does not support the video tag.</strong>
+    </video>
+  )
+}
+
+export default UploadVideo
