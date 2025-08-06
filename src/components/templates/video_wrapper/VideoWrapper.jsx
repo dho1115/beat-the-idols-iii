2586be73199992542c0126ed12096dd5ffe4e7c4
admin_dho <davidhorph@hotmail.com>
2025-08-06 13:17:10 -0500

    Update VideoWrapper.jsx
    Destructured usrename from args to use it.

--- src/components/templates/video_wrapper/VideoWrapper.jsx
+++ src/components/templates/video_wrapper/VideoWrapper.jsx
@@ -3,23 +3,31 @@ import React from 'react'
 //Dependencies & components.
 import { Card, CardText, CardTitle } from 'reactstrap';
 
-const VideoWrapper = ({video_component, title, description, button_text, clickLogic}) => {
-  return (
-    <Card style={{border: '1.5px solid black', borderRadius: '15px'}}>
-      <div className='w-100 p-0 card-video-div'>
-         {video_component}
-      </div>
-      <CardTitle>
-         <strong>{title}</strong>
-      </CardTitle>
-      <CardText>
-         {description}
-      </CardText>
-      <div className='w-100'>
-         <button type="button" className="btn btn-danger btn-lg btn-block" onClick={clickLogic || null}>{button_text}</button>
-      </div>
-    </Card>
-  )
+import "./VideoWrapper.styles.css";
+
+const VideoWrapper = ({ video_component, title, description, button_text, clickLogic, ...args }) => {
+   const { username } = args;
+   return (
+      <Card style={{border: '3.5px solid lightseagreen', borderRadius: '15px'}}>
+         <div className='w-100 p-0 card-video-div'>
+            {video_component}
+         </div>
+         <CardTitle className='p-1'>
+            <strong>{title}</strong>
+         </CardTitle>
+         <CardText className='px-1'><strong>Posted By: {username}</strong></CardText>
+         <CardText className='px-1' style={{paddingBottom: '5px'}}>
+            {description}
+         </CardText>
+         {
+            (button_text && clickLogic)
+            &&
+            <div className='w-100'>
+               <button type="button" className="btn btn-danger btn-lg btn-block" onClick={clickLogic || null}>{button_text}</button>
+            </div>
+         }
+      </Card>
+   )
 }
 
 export default VideoWrapper
