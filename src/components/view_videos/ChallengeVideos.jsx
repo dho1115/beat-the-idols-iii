1c3b1b71e560fd014cbedc9bb0bac4b9d134c610
admin_dho <davidhorph@hotmail.com>
2025-08-06 13:18:16 -0500

    Replaced div ... div with <VideoWrapper />
    Replaced the <div></div> with a <VideoWrapper />.

--- src/components/view_videos/ChallengeVideos.jsx
+++ src/components/view_videos/ChallengeVideos.jsx
@@ -9,7 +9,6 @@ import { dataContext } from '../../App'
 
 import './ChallengeVideos.styles.css';
 
-
 const ChallengeVideos = () => {
    const { videos } = useContext(dataContext);
    const {user, filter} = useParams(); //Allows the user to either see [user] videos only or "all" videos (see filterQuery).
@@ -20,21 +19,14 @@ const ChallengeVideos = () => {
    }
 
    return (
-      <Container className='challenge-videos-container'>
+      <Container className='challenge-videos-container p-3'>
          {
             filterQuery[filter].length ?
                filterQuery[filter].map(({ id, _userID, username, title, description, urlOrFile, videoType }, idx) => {
-                  return (
-                     <div className='p-1 m-1' style={{ border: '1.5px solid firebrick', backgroundColor: idx%2 == 1 ? 'white' : 'lightgreen' }}>
-                        {/* <h5>video id: {id}</h5>
-                        <h5>_userID: {_userID}</h5>
-                        <h5>username: {username}</h5>
-                        <h3>title: {title}</h3>
-                        <h5>file or url: {urlOrFile}</h5>
-                        <h5>videoType: {videoType}</h5>
-                        <h5>description: {description}</h5> */}
-                     </div>
-                  )
+                  if (videoType == "you-tube") {
+                     return <VideoWrapper key={idx} video_component={<YouTubeVideo url={urlOrFile} title={title} />} title={title} description={description} id={id} _userID={_userID} username={username}/>
+                  }
+                  
                })
                :
                <h1 className='text-danger'><i>Sorry... there are no challenges for you!!!</i></h1>
