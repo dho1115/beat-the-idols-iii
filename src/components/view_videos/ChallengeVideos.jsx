e8a4dbb4d8d60cad6f532988a5fcad4089582500
admin_dho <davidhorph@hotmail.com>
2025-08-06 12:19:13 -0500

    About to replace mapped video details with card.
    About to replace the video details (title, urlOrFile, videoType, etc...) with the actual Card.

--- src/components/view_videos/ChallengeVideos.jsx
+++ src/components/view_videos/ChallengeVideos.jsx
@@ -8,7 +8,6 @@ import YouTubeVideo from '../templates/video/you_tube/YouTubeVideo';
 import { dataContext } from '../../App'
 
 import './ChallengeVideos.styles.css';
-import { desc } from 'motion/react-client';
 
 
 const ChallengeVideos = () => {
@@ -27,13 +26,13 @@ const ChallengeVideos = () => {
                filterQuery[filter].map(({ id, _userID, username, title, description, urlOrFile, videoType }, idx) => {
                   return (
                      <div className='p-1 m-1' style={{ border: '1.5px solid firebrick', backgroundColor: idx%2 == 1 ? 'white' : 'lightgreen' }}>
-                        <h5>video id: {id}</h5>
+                        {/* <h5>video id: {id}</h5>
                         <h5>_userID: {_userID}</h5>
                         <h5>username: {username}</h5>
                         <h3>title: {title}</h3>
                         <h5>file or url: {urlOrFile}</h5>
                         <h5>videoType: {videoType}</h5>
-                        <h5>description: {description}</h5>
+                        <h5>description: {description}</h5> */}
                      </div>
                   )
                })
