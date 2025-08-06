b22e9532ca6f951363ea57e09810218360948a7a
admin_dho <davidhorph@hotmail.com>
2025-08-06 11:42:23 -0500

    Update ChallengeVideos.jsx
    Refactoring.

--- src/components/view_videos/ChallengeVideos.jsx
+++ src/components/view_videos/ChallengeVideos.jsx
@@ -3,9 +3,13 @@ import React, { useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import { Container } from 'reactstrap'
 import VideoWrapper from '../templates/video_wrapper/VideoWrapper';
+import UploadVideo from '../templates/video/upload/UploadVideo';
+import YouTubeVideo from '../templates/video/you_tube/YouTubeVideo';
 import { dataContext } from '../../App'
 
 import './ChallengeVideos.styles.css';
+import { desc } from 'motion/react-client';
+
 
 const ChallengeVideos = () => {
    const { videos } = useContext(dataContext);
@@ -20,11 +24,19 @@ const ChallengeVideos = () => {
       <Container className='challenge-videos-container'>
          {
             filterQuery[filter].length ?
-               filterQuery[filter].map(({ id, ...rest }) => (
-                  <div key={id}>
-                     <strong>{JSON.stringify({id, ...rest})}</strong>
-                  </div>
-               ))
+               filterQuery[filter].map(({ id, _userID, username, title, description, urlOrFile, videoType }, idx) => {
+                  return (
+                     <div className='p-1 m-1' style={{ border: '1.5px solid firebrick', backgroundColor: idx%2 == 1 ? 'white' : 'lightgreen' }}>
+                        <h5>video id: {id}</h5>
+                        <h5>_userID: {_userID}</h5>
+                        <h5>username: {username}</h5>
+                        <h3>title: {title}</h3>
+                        <h5>file or url: {urlOrFile}</h5>
+                        <h5>videoType: {videoType}</h5>
+                        <h5>description: {description}</h5>
+                     </div>
+                  )
+               })
                :
                <h1 className='text-danger'><i>Sorry... there are no challenges for you!!!</i></h1>
          }
