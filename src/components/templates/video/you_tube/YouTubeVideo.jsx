b4bb46658f975752d23e267c01d115ceaf9726ac
admin_dho <davidhorph@hotmail.com>
2025-08-06 13:18:56 -0500

    embedded target video.
    Used getVideoID to get the video id of the target youtube video.

--- src/components/templates/video/you_tube/YouTubeVideo.jsx
+++ src/components/templates/video/you_tube/YouTubeVideo.jsx
@@ -1,8 +1,11 @@
+import getVideoId from "get-video-id";
 import "./YouTubeVideo.styles.css"
 
 const YouTubeVideo = ({ url, title }) => {
+  const { id: _youtubeID } = getVideoId(url)
+  
   return (
-    <iframe width="100%" height="100" src={url} title={title} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <iframe width="100%" height="100%" src={`https://www.youtube.com/embed/${_youtubeID}?si=0MNCojt0FOjgI5bA`} title={title} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen style={{borderTopLeftRadius: '15px', borderTopRightRadius: '15px'}}></iframe>
   )
 }
 
