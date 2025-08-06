35c680f9254dbe177b3ae718c2acd76ed76ae2cc
admin_dho <davidhorph@hotmail.com>
2025-08-06 12:18:09 -0500

    Update YouTubeVideo.jsx
    Added title prop.

--- src/components/templates/video/you_tube/YouTubeVideo.jsx
+++ src/components/templates/video/you_tube/YouTubeVideo.jsx
@@ -1,11 +1,8 @@
-import React from 'react'
-
 import "./YouTubeVideo.styles.css"
 
-const YouTubeVideo = ({ url }) => {
-   
+const YouTubeVideo = ({ url, title }) => {
   return (
-    <iframe width="100%" height="100" src={url} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <iframe width="100%" height="100" src={url} title={title} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
   )
 }
 
