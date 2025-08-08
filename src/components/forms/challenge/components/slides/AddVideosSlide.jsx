77e9057541cc87fd316744e05d7a28bca7052be6
admin_dho <davidhorph@hotmail.com>
2025-08-08 13:38:50 -0500

    Added <Suspense> and <ErrorBoundary>

--- src/components/forms/challenge/components/slides/AddVideosSlide.jsx
+++ src/components/forms/challenge/components/slides/AddVideosSlide.jsx
@@ -1,6 +1,8 @@
-import React, { useContext } from 'react'
+import React, { Suspense, useContext } from 'react'
 import { Container } from 'reactstrap';
 import { dataContext } from '../../../../../App'
+import SuspenseFallback from '../../../../suspense_fallback/SuspenseFallback';
+import ErrorBoundary from '../../../../ErrorBoundary';
 
 import "./Slide.styles.css";
 
@@ -9,8 +11,12 @@ const AddVideosSlide = () => {
 
    return (
       <div className='slide-div'>
-         <Container className='m-0' style={{ width: '100%', height: '100%', backgroundColor: 'whitesmoke'}}>
-            <h1>THE VIDEOS GO HERE ALONG WITH A <button className='btn btn-danger'>SELECT THIS VIDEO!!!</button>.</h1>
+         <Container className='m-0' style={{ width: '100%', height: '100%', backgroundColor: 'whitesmoke' }}>
+            <ErrorBoundary fallback={<h3 className='text-danger'>Problem inside AddVideosSlide.jsx</h3>}>
+               <Suspense fallback={<SuspenseFallback />}>
+                  <h1>THE VIDEOS GO HERE ALONG WITH A <button className='btn btn-danger'>SELECT THIS VIDEO!!!</button>.</h1>
+               </Suspense>
+            </ErrorBoundary>
          </Container>
       </div>
    )
