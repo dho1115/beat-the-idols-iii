14575ed17c252ac1cb5038fcf8325172c2c3ca20
admin_dho <davidhorph@hotmail.com>
2025-08-09 11:52:43 -0500

    Update ChallengeVideos.jsx
    Started adding logic to ChallengeDetails, incluing setChallengeDetails, which is a CreateContext value from ChallengeForm.js.

--- src/components/forms/challenge/challenge_videos/ChallengeVideos.jsx
+++ src/components/forms/challenge/challenge_videos/ChallengeVideos.jsx
@@ -1,15 +1,23 @@
 import React, { useContext } from 'react'
-import { Container, FormGroup } from 'reactstrap'
+import { Container, FormGroup } from 'reactstrap';
+import { ChallengeDetailsContext } from '../ChallengeForm';
+import { dataContext } from '../../../../App';
+import ErrorBoundary from '../../../ErrorBoundary';
 import UploadVideo from '../../../templates/video/upload/UploadVideo';
 import VideoWrapper from '../../../templates/video_wrapper/VideoWrapper';
 import YouTubeVideo from '../../../templates/video/you_tube/YouTubeVideo';
-import { dataContext } from '../../../../App';
 
 import "./ChallengeVideos.styles.css";
 
 const ChallengeVideos = () => {
    const { videos } = useContext(dataContext);
+   const { setChallengeDetails } = useContext(ChallengeDetailsContext);
+
    console.log({ videos });
+
+   const onHandleSubmitChallengeVideos = e => {
+      e.preventDefault();
+   }
 
    return (
       <FormGroup>
