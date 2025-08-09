d99e9f703c50e1df5310b00f701d53b9ca866430
admin_dho <davidhorph@hotmail.com>
2025-08-09 10:49:24 -0500

    Modifications to <VideoWrapper />.
    Made some of the props dynamic.

--- src/components/templates/video_wrapper/VideoWrapper.jsx
+++ src/components/templates/video_wrapper/VideoWrapper.jsx
@@ -1,30 +1,37 @@
 import React from 'react'
 
 //Dependencies & components.
-import { Card, CardText, CardTitle } from 'reactstrap';
+import { Card, CardBody, CardFooter, CardText, CardTitle } from 'reactstrap';
 
 import "./VideoWrapper.styles.css";
 
 const VideoWrapper = ({ video_component, title, description, button_text, clickLogic, ...args }) => {
    const { username, idx } = args;
    return (
-      <Card style={{border: idx%2==1 ? '3.5px solid lightseagreen' : '3.5px solid yellow', borderRadius: '15px'}}>
+      <Card className='m-1' style={{ border: idx % 2 == 1 ? '3.5px solid lightseagreen' : '3.5px solid red', height: '100%', borderRadius: '15.3px'}}>
          <div className='w-100 p-0 card-video-div'>
             {video_component}
          </div>
          <CardTitle className='p-1'>
-            <strong>{title}</strong>
+            <strong>
+               {title.length > 15 ? title.substring(0, 15) + "..." : title}
+            </strong>
          </CardTitle>
-         <CardText className='px-1'><strong>Posted By: {username}</strong></CardText>
-         <CardText className='px-1' style={{paddingBottom: '5px'}}>
-            {description}
-         </CardText>
+         <CardBody className='px-1'>
+            <strong>Posted By: {username}</strong>
+            {
+               description &&
+               <CardText className='px-1' style={{paddingBottom: '5px', height: '100%'}}>
+                  {description}
+               </CardText>
+            }
+         </CardBody>            
          {
             (button_text && clickLogic)
             &&
-            <div className='w-100'>
-               <button type="button" className="btn btn-danger btn-lg btn-block" onClick={clickLogic || null}>{button_text}</button>
-            </div>
+            <CardFooter className='w-100' style={{backgroundColor: 'black'}}>
+               <button type="button" className="btn btn-danger btn-block w-100" onClick={clickLogic || null}>{button_text}</button>
+            </CardFooter>
          }
       </Card>
    )
