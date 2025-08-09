2910fe570a984cf8d7c063105964215dbe41fc7b
admin_dho <davidhorph@hotmail.com>
2025-08-09 11:53:56 -0500

    Added ErrorBoundary, createContext, uuid.
    Added the following:
    1. v4 from uuid to generate random id.
    2. Added DateTime from luxon so I can add a posted date property.
    3. ErrorBoundary to handle errors.

--- src/components/forms/challenge/ChallengeForm.jsx
+++ src/components/forms/challenge/ChallengeForm.jsx
@@ -1,41 +1,63 @@
-import React, { useState } from 'react';
-import { Col, Form, FormGroup, Input, Label } from 'reactstrap';
+import React, { useState, createContext } from 'react';
+import ErrorBoundary from '../../ErrorBoundary';
+import { v4 } from 'uuid';
+import { DateTime } from 'luxon';
+import { Button, Col, Form, FormGroup, Input, Label } from 'reactstrap';
 import Deadline from './input_invite_others/Deadline';
+
+export const ChallengeDetailsContext = createContext();
 
 import './ChallengeForm.styles.css';
 
+
 const ChallengeForm = () => {
-  const [challengeDetails, setChallengeDetails] = useState({ id: null, posted: null, title: '', inviteOthers: '', deadline: "0000-00-00", challengeExpires: ''});
+  const [challengeDetails, setChallengeDetails] = useState({ id: null, posted: null, title: '', inviteOthers: '', deadline: "0000-00-00", challengeExpires: '', challengeVideos: []});
   
   const handleSubmit = (e) => {
     e.preventDefault();
+    const posted = DateTime.local().toFormat('yyyy-MM-dd');
+    const _videoID = v4();
+    let submitChallengeDetails = { posted, id: _videoID, ...challengeDetails, _videoID };
+    if (!challengeDetails.inviteOthers) delete submitChallengeDetails.deadline;
+    console.log("Successfully submitted the following:", challengeDetails);
   }
 
   return (
-    <Form className='challenge-form p-3 m-1' onSubmit={handleSubmit}>
-      <FormGroup>
-        <Label for='title'>TITLE OF CHALLENGE</Label>
-        <Input type='text' id='title' value={challengeDetails.title} placeholder='TITLE OF CHALLENGE' onChange={e => setChallengeDetails(prv => ({...prv, title: e.target.value}))} required />
-      </FormGroup>
-      <FormGroup row tag="fieldset">
-        <legend className="col-form-label col-sm-5">INVITE OTHERS TO CHALLENGE?</legend>
-        <Col sm={7}>
-          <FormGroup check>
-            <Input type='radio' name='inviteOthers' id='YES' value={challengeDetails.inviteOthers} onChange={() => setChallengeDetails(prv => ({...prv, inviteOthers: true}))} required />{' '}<Label for='YES' check>YES</Label>
+    <ErrorBoundary fallback={
+      <div style={{height: '100vh', backgroundColor: 'lightgoldenrodyellow', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <h1 className='text-danger'>SOMETHING WENT WRONG INSIDE CHALLENGEFORM.JSX!!!</h1>
+      </div>}
+    >
+      <ChallengeDetailsContext.Provider value={{challengeDetails, setChallengeDetails}}>
+        <Form className='challenge-form p-3 m-1' onSubmit={handleSubmit}>
+          <FormGroup>
+            <Label for='title'><strong>TITLE OF CHALLENGE</strong></Label>
+            <Input type='text' id='title' value={challengeDetails.title} placeholder='TITLE OF CHALLENGE' onChange={e => setChallengeDetails(prv => ({...prv, title: e.target.value}))} required />
           </FormGroup>
-          <FormGroup check>
-            <Input type='radio' name='inviteOthers' id='NO' value={challengeDetails.inviteOthers} onChange={() => setChallengeDetails(prv => ({...prv, inviteOthers: false}))} />{' '}<Label for='NO' check>NO</Label>
+          <FormGroup row tag="fieldset">
+            <legend className="col-form-label col-sm-5"><strong>INVITE OTHERS TO CHALLENGE?</strong></legend>
+            <Col sm={7}>
+              <FormGroup check>
+                <Input type='radio' name='inviteOthers' id='YES' value={challengeDetails.inviteOthers} onChange={() => setChallengeDetails(prv => ({...prv, inviteOthers: true}))} required />{' '}<Label for='YES' check>YES</Label>
+              </FormGroup>
+              <FormGroup check>
+                <Input type='radio' name='inviteOthers' id='NO' value={challengeDetails.inviteOthers} onChange={() => setChallengeDetails(prv => ({...prv, inviteOthers: false}))} />{' '}<Label for='NO' check>NO</Label>
+              </FormGroup>
+            </Col>
           </FormGroup>
-        </Col>
-      </FormGroup>
-      {
-        challengeDetails.inviteOthers && <Deadline challengeDetails={challengeDetails} setChallengeDetails={setChallengeDetails} />
-      }
-      <FormGroup>
-        <Label for='description'>DESCRIPTION</Label>
-        <Input type='textarea' id='description' placeholder='Briefly describe your challenge here.' maxLength={107} />
-      </FormGroup>
-    </Form>
+          {
+            challengeDetails.inviteOthers && <Deadline challengeDetails={challengeDetails} setChallengeDetails={setChallengeDetails} />
+          }
+          <FormGroup>
+            <Label for='description'><strong>DESCRIPTION</strong></Label>
+            <Input type='textarea' id='description' placeholder='Briefly describe your challenge here.' maxLength={107} />
+          </FormGroup>
+          <FormGroup className='w-100'>
+            <Button type='submit' className='w-100' color='danger' size='lg'>NEXT</Button>
+          </FormGroup>
+        </Form>
+      </ChallengeDetailsContext.Provider>
+    </ErrorBoundary>
   )
 }
 
