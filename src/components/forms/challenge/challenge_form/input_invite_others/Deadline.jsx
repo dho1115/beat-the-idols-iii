26969056c53a8263b195ee1d5a62460331beaab0
admin_dho <davidhorph@hotmail.com>
2025-08-10 22:51:19 -0500

    Created <Deadline /> component.
    A component where the user can specify when the deadline is for users to sign up.

---
+++ src/components/forms/challenge/challenge_form/input_invite_others/Deadline.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { FormGroup, Input, Label } from 'reactstrap';
+import { DateTime } from 'luxon';
+const Deadline = ({ challengeDetails, setChallengeDetails, ...args }) => {
+   const currentDate = DateTime.local().toFormat('yyyy-MM-dd');
+   const add5months = DateTime.local().plus({ months: 5 }).toFormat('yyyy-MM-dd');
+   
+   return (
+      <FormGroup>
+         <Label for='deadline'>SELECT A DEADLINE FOR PEOPLE TO APPLY.</Label>
+         <Input type='date' id='deadline' value={challengeDetails.Deadline} min={currentDate} max={add5months} onChange={e => setChallengeDetails(prv => ({...prv, deadline: e.target.value}))} required/>
+      </FormGroup>
+   )
+}
+
+export default Deadline
