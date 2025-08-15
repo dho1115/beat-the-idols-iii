3a36047ae5a35ef63c9088dd7f116df707b50a50
admin_dho <davidhorph@hotmail.com>
2025-08-15 12:42:28 -0500

    Added useRef (videosContainerRef) for DOM.
    added a videosContainerRef = useRef( ) and embedded it inside Container.challenge-videos-form-container to manipulate the DOM (gridTemplateColumns).

--- src/components/forms/challenge/challenge_videos/AddChallengeVideos.jsx
+++ src/components/forms/challenge/challenge_videos/AddChallengeVideos.jsx
@@ -1,5 +1,5 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { Form, Label, Button, Container, FormGroup } from 'reactstrap';
+import React, { useContext, useEffect, useState, useRef } from 'react'
+import { Form, Label, Button, Container, FormGroup, Alert } from 'reactstrap';
 import { ChallengeDetailsContext } from '../ChallengeFormComponent';
 import { dataContext } from '../../../../App';
 import ErrorBoundary from '../../../ErrorBoundary';
@@ -10,25 +10,39 @@ import YouTubeVideo from '../../../templates/video/you_tube/YouTubeVideo';
 import "./ChallengeVideos.styles.css";
 
 const AddChallengeVideos = () => {
+   const videosContainerRef = useRef();
    const [selectedVideos, setSelectedVideos] = useState([])
+   const [addVideoAlert, setAddVideoAlert] = useState(false)
    const { videos } = useContext(dataContext);
    const { challengeDetails, setChallengeDetails } = useContext(ChallengeDetailsContext);
 
    const onHandleSubmitChallengeVideos = e => {
       e.preventDefault();
       console.log("About to submit:", selectedVideos);
+      if (!selectedVideos.length) {
+         setAddVideoAlert(true)
+      }
       setChallengeDetails(prv => ({ ...prv, challengeVideos: [...challengeDetails.challengeVideos, ...selectedVideos] }));
    }
 
    useEffect(() => {
-      return () => setSelectedVideos([])
+      const setGridTemplateColumns = videos.length >= 5 ? "auto auto auto auto auto" : videos.map(val => "auto").join(" ");
+
+      videosContainerRef.current.style.gridTemplateColumns = setGridTemplateColumns
+      return () => {
+         setSelectedVideos([])
+         setAddVideoAlert(false)
+      }
    }, [])
 
    return (
-      <Container className="challenge-videos-form p-3 m-3" onSubmit={onHandleSubmitChallengeVideos}>
+      <div className="challenge-videos-form p-3 m-3">
+         {
+            addVideoAlert && <Alert color='danger'><h3>PLEASE SELECT AT LEAST 1 VIDEO (you currently have {selectedVideos.length} videos)!!!</h3></Alert>
+         }
          <FormGroup>
             <Label><strong>SELECT CHALLENGE VIDEOS!!!</strong></Label>
-            <Container className='challenge-videos-form-container'>
+            <Container className='challenge-videos-form-container' ref={videosContainerRef}>
                {
                   videos.map((val, idx) => (
                      <div key={idx} className='p-1 m-1' style={{border: `5px solid ${idx%2==1 ? "#333" : "red"}`, backgroundColor: idx%2==1 ? 'beige' : 'floralwhite', overflow: "scroll", textWrap: 'pretty'}}>
@@ -42,7 +56,7 @@ const AddChallengeVideos = () => {
          <FormGroup>
             <Button type='button' color='danger' className='w-100' onClick={onHandleSubmitChallengeVideos}>FINISHED SELECTING VIDEOS.</Button>
          </FormGroup>
-      </Container>      
+      </div>      
    )
 }
 
