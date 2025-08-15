baf1d398cd08c51634f84a3c55d687145a57830d
admin_dho <davidhorph@hotmail.com>
2025-08-15 10:44:24 -0500

    Added Suspense component.
    Added a Suspense

--- src/components/templates/video/you_tube/YouTubeVideo.jsx
+++ src/components/templates/video/you_tube/YouTubeVideo.jsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import getVideoId from "get-video-id";
 import "./YouTubeVideo.styles.css"
 
@@ -5,7 +6,10 @@ const YouTubeVideo = ({ url, title }) => {
   const { id: _youtubeID } = getVideoId(url)
   
   return (
-    <iframe width="100%" height="100%" src={`https://www.youtube.com/embed/${_youtubeID}?si=0MNCojt0FOjgI5bA`} title={title} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen style={{borderTopLeftRadius: '15px', borderTopRightRadius: '15px'}}></iframe>
+    <Suspense fallback={<h5 className="text-danger"><i>Loading... please wait!!!</i></h5>}>
+      <iframe width="100%" height="100%" src={`https://www.youtube.com/embed/${_youtubeID}?si=0MNCojt0FOjgI5bA`} title={title} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen style={{borderTopLeftRadius: '15px', borderTopRightRadius: '15px'}}></iframe>
+    </Suspense>
+    
   )
 }
 
--- src/components/templates/video_wrapper/VideoWrapper.jsx
+++ src/components/templates/video_wrapper/VideoWrapper.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-
+import React, { Suspense } from 'react'
+import SuspenseFallback from '../../suspense_fallback/SuspenseFallback';
 //Dependencies & components.
 import { Card, CardBody, CardFooter, CardText, CardTitle } from 'reactstrap';
 
@@ -8,31 +8,33 @@ import "./VideoWrapper.styles.css";
 const VideoWrapper = ({ video_component, title, description, button_text, clickLogic, ...args }) => {
    const { username, idx } = args;
    return (
-      <Card className='m-1' style={{ border: idx % 2 == 1 ? '3.5px solid lightseagreen' : '3.5px solid red', height: '100%', borderRadius: '15.3px'}}>
-         <div className='w-100 p-0 card-video-div'>
-            {video_component}
-         </div>
-         <CardTitle className='p-1'>
-            <strong>
-               {title.length > 15 ? title.substring(0, 15) + "..." : title}
-            </strong>
-         </CardTitle>
-         <CardBody className='px-1'>
-            <strong>Posted By: {username}</strong>
+      <Card className='m-1' style={{ border: idx % 2 == 1 ? '3.5px solid lightseagreen' : '3.5px solid red', height: '100%', borderRadius: '15.3px' }}>
+         <Suspense fallback={<SuspenseFallback />}>
+            <div className='w-100 p-0 card-video-div'>
+               {video_component}
+            </div>
+            <CardTitle className='p-1'>
+               <strong>
+                  {title.length > 15 ? title.substring(0, 15) + "..." : title}
+               </strong>
+            </CardTitle>
+            <CardBody className='px-1'>
+               <strong>Posted By: {username}</strong>
+               {
+                  description &&
+                  <CardText className='px-1' style={{paddingBottom: '5px', height: '100%'}}>
+                     {description}
+                  </CardText>
+               }
+            </CardBody>            
             {
-               description &&
-               <CardText className='px-1' style={{paddingBottom: '5px', height: '100%'}}>
-                  {description}
-               </CardText>
+               (button_text && clickLogic)
+               &&
+               <CardFooter className='w-100' style={{backgroundColor: 'black'}}>
+                  <button type="button" className="btn btn-danger btn-block w-100" onClick={clickLogic || null}>{button_text}</button>
+               </CardFooter>
             }
-         </CardBody>            
-         {
-            (button_text && clickLogic)
-            &&
-            <CardFooter className='w-100' style={{backgroundColor: 'black'}}>
-               <button type="button" className="btn btn-danger btn-block w-100" onClick={clickLogic || null}>{button_text}</button>
-            </CardFooter>
-         }
+         </Suspense>
       </Card>
    )
 }
