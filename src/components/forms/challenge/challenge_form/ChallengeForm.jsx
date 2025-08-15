b19f7e8608f6d9849180f12c9ae37f07e849e796
admin_dho <davidhorph@hotmail.com>
2025-08-15 10:41:29 -0500

    Added challengeExpiration function.
    A dynamic function that generates a min and max for Input type='date' for the challenge expiration and is dependent on whether the user wants to invite others.

--- src/components/forms/challenge/challenge_form/ChallengeForm.jsx
+++ src/components/forms/challenge/challenge_form/ChallengeForm.jsx
@@ -1,8 +1,7 @@
 import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { v4 } from 'uuid';
-import { DateTime } from 'luxon';
 import { Button, Col, Form, FormGroup, Input, Label } from 'reactstrap';
+import { DateTime } from 'luxon';
 import Deadline from './input_invite_others/Deadline';
 import { dataContext } from '../../../../App';
 import { ChallengeDetailsContext } from '../ChallengeFormComponent';
@@ -14,50 +13,55 @@ const ChallengeForm = () => {
   const { challengeDetails, setChallengeDetails } = useContext(ChallengeDetailsContext);
   const { currentUser } = useContext(dataContext);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const posted = DateTime.local().toFormat('yyyy-MM-dd');
-    const _videoID = v4();
-    let submitChallengeDetails = { posted, id: _videoID, ...challengeDetails, _videoID };
-    if (!challengeDetails.inviteOthers) delete submitChallengeDetails.deadline;
-    console.log("Successfully submitted the following:", challengeDetails);
-    navigate(`/currentUser/${currentUser.id}/challenge-form/add-video`)
+  const challengeExpiration = () => {
+    if (challengeDetails.inviteOthers) {
+      const deadlineString = challengeDetails.deadline;
+      const ISOmin = DateTime.fromISO(deadlineString)
+      const min = ISOmin.plus({ days: 3 }).toFormat('yyyy-MM-dd');
+      const max = ISOmin.plus({ months: 5 }).toFormat('yyyy-MM-dd');
+      return { min, max };
+    }
+
+    const min = DateTime.local().plus({ days: 3 }).toFormat("yyyy-MM-dd");
+    const max = DateTime.local().plus({ months: 5 }).toFormat("yyyy-MM-dd");
+
+    return { min, max };
   }
 
+  const onButtonClick = () => navigate(`/currentUser/${currentUser.id}/challenge-form/add-video`)
+
   return (
-      <ChallengeDetailsContext.Provider value={{challengeDetails, setChallengeDetails}}>
-        <Form className='challenge-form p-3 m-1' onSubmit={handleSubmit}>
-          <FormGroup>
-            <Label for='title'><strong>TITLE OF CHALLENGE</strong></Label>
-            <Input type='text' id='title' value={challengeDetails.title} placeholder='TITLE OF CHALLENGE' onChange={e => setChallengeDetails(prv => ({...prv, title: e.target.value}))} required />
+    <>
+      <FormGroup>
+        <Label for='title'><strong>TITLE OF CHALLENGE</strong></Label>
+        <Input type='text' id='title' value={challengeDetails.title} placeholder='TITLE OF CHALLENGE' onChange={e => setChallengeDetails(prv => ({...prv, title: e.target.value}))} required />
+      </FormGroup>
+      <FormGroup row tag="fieldset">
+        <legend className="col-form-label col-sm-5"><strong>INVITE OTHERS TO CHALLENGE?</strong></legend>
+        <Col sm={7}>
+          <FormGroup check>
+            <Input type='radio' name='inviteOthers' id='YES' value={challengeDetails.inviteOthers} onChange={() => setChallengeDetails(prv => ({...prv, inviteOthers: true}))} required />{' '}<Label for='YES' check>YES</Label>
           </FormGroup>
-          <FormGroup row tag="fieldset">
-            <legend className="col-form-label col-sm-5"><strong>INVITE OTHERS TO CHALLENGE?</strong></legend>
-            <Col sm={7}>
-              <FormGroup check>
-                <Input type='radio' name='inviteOthers' id='YES' value={challengeDetails.inviteOthers} onChange={() => setChallengeDetails(prv => ({...prv, inviteOthers: true}))} required />{' '}<Label for='YES' check>YES</Label>
-              </FormGroup>
-              <FormGroup check>
-                <Input type='radio' name='inviteOthers' id='NO' value={challengeDetails.inviteOthers} onChange={() => setChallengeDetails(prv => ({...prv, inviteOthers: false}))} />{' '}<Label for='NO' check>NO</Label>
-              </FormGroup>
-            </Col>
+          <FormGroup check>
+            <Input type='radio' name='inviteOthers' id='NO' value={challengeDetails.inviteOthers} onChange={() => setChallengeDetails(prv => ({...prv, inviteOthers: false}))} />{' '}<Label for='NO' check>NO</Label>
           </FormGroup>
-          {
-            challengeDetails.inviteOthers && <Deadline challengeDetails={challengeDetails} setChallengeDetails={setChallengeDetails} />
-          }
-          <FormGroup>
-            <Label for='description'><strong>DESCRIPTION</strong></Label>
-            <Input type='textarea' id='description' placeholder='Briefly describe your challenge here.' maxLength={107} />
-          </FormGroup>
-          <FormGroup>
-            <Label for='challengeEnds'><strong>CHALLENGE END DATE</strong></Label>
-            <Input type='date' id='challengeEnds' placeholder='Select a date for when your challenge ends' />
-          </FormGroup>
-          <FormGroup className='w-100'>
-            <Button type='submit' className='w-100' color='danger' size='lg'>NEXT</Button>
-          </FormGroup>
-        </Form>
-      </ChallengeDetailsContext.Provider>
+        </Col>
+      </FormGroup>
+      {
+        challengeDetails.inviteOthers && <Deadline challengeDetails={challengeDetails} setChallengeDetails={setChallengeDetails} />
+      }
+      <FormGroup>
+        <Label for='description'><strong>DESCRIPTION</strong></Label>
+        <Input type='textarea' id='description' placeholder='Briefly describe your challenge here.' maxLength={107} />
+      </FormGroup>
+      <FormGroup>
+        <Label for='challengeEnds'><strong>CHALLENGE END DATE</strong></Label>
+        <Input type='date' id='challengeEnds' min={challengeExpiration().min} max={challengeExpiration().max} placeholder='Select a date for when your challenge ends' />
+      </FormGroup>
+      <FormGroup className='w-100'>
+        <Button type='button' className='w-100' color='danger' size='lg' onClick={onButtonClick}>NEXT</Button>
+      </FormGroup>
+    </>
   )
 }
 
