de8debd4559dbd6507244456a5d57cfe81c46d1c
admin_dho <davidhorph@hotmail.com>
2025-08-24 18:49:50 -0500

    Update HomeNavbarLinks.jsx
    Added new property: link which I plan to destructure later.

--- src/components/home/HomeNavbarLinks.jsx
+++ src/components/home/HomeNavbarLinks.jsx
@@ -1,8 +1,15 @@
-import linkImg1 from "../../media/images/musicians.jpg";
+import linkImg1 from "../../media/images/starsAndGalaxy.jpg";
+import linkImg2 from "../../media/images/singers_performers.jpg";
 
 export const HomeNavbarLinks = [
    {
       src: linkImg1,
-      text: "VIEW CURRENT CHALLENGES!!!"
+      text: "ACTIVE CHALLENGES!!!",
+      link: "/home/current-challenges"
+   },
+   {
+      src: linkImg2,
+      text: "CHALLENGE ANNOUNCEMENTS!!!",
+      link: "/home/challenge-announcements"
    }
 ]
