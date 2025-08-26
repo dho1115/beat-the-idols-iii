719aa8d3a1f5c9ae2daa1c7c8238101fb1b23526
admin_dho <davidhorph@hotmail.com>
2025-08-26 17:30:23 -0500

    Update ExpirationDateOption.jsx
    Form for if the user wants the challenge to end via Expiration date.

--- src/components/forms/challenge/challenge_ends_option/ExpirationDateOption.jsx
+++ src/components/forms/challenge/challenge_ends_option/ExpirationDateOption.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { ChallengeDetailsContext } from '../ChallengeFormComponent';
+import { DateTime } from 'luxon';
+import { FormGroup, Label, Input } from 'reactstrap';
 
 const ExpirationDateOption = () => {
+  const { challengeDetails, setChallengeDetails } = useContext(ChallengeDetailsContext)
+  
+  const challengeExpiration = () => {
+    if (challengeDetails.inviteOthers) {
+      const deadlineString = challengeDetails.deadline; //DEADLINE FOR PEOPLE TO APPLY.
+      const ISOmin = DateTime.fromISO(deadlineString)
+      const min = ISOmin.plus({ days: 3 }).toFormat('yyyy-MM-dd');
+      const max = ISOmin.plus({ months: 5 }).toFormat('yyyy-MM-dd');
+      return { min, max };
+    }
+
+    const min = DateTime.local().plus({ days: 3 }).toFormat("yyyy-MM-dd");
+    const max = DateTime.local().plus({ months: 5 }).toFormat("yyyy-MM-dd");
+
+    return { min, max };
+  }
+
   return (
-    <div>ExpirationDateOption</div>
+    <FormGroup>
+        <Label for='endbydate'><strong>CHALLENGE END DATE</strong></Label>
+        <Input type='date' id='endbydate' min={challengeExpiration().min} max={challengeExpiration().max} placeholder='Select a date for when your challenge ends' onChange={e => setChallengeDetails(prv => ({...prv, challengeExpires: e.target.value}))} required />
+      </FormGroup>
   )
 }
 
