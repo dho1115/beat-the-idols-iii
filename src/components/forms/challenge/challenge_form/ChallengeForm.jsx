302ff40360328bb5e0fda6f691ef2e8e7e741256
admin_dho <davidhorph@hotmail.com>
2025-08-26 17:31:26 -0500

    Update ChallengeForm.jsx
    Moved many of the code (ex. challengeExiration) to the module that handles the situation where the user wants the challenge to end by a date.

--- src/components/forms/challenge/challenge_form/ChallengeForm.jsx
+++ src/components/forms/challenge/challenge_form/ChallengeForm.jsx
@@ -1,10 +1,12 @@
 import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button, Col, Form, FormGroup, Input, Label } from 'reactstrap';
-import { DateTime } from 'luxon';
-import Deadline from './input_invite_others/Deadline';
-import { dataContext } from '../../../../App';
 import { ChallengeDetailsContext } from '../ChallengeFormComponent';
+import { dataContext } from '../../../../App';
+//Modules-Components.
+import Deadline from './input_invite_others/Deadline';
+import ExpirationDateOption from '../challenge_ends_option/ExpirationDateOption';
+import WinningVotesOption from '../challenge_ends_option/WinningVotesOption';
 
 import './ChallengeForm.styles.css';
 
@@ -12,21 +14,6 @@ const ChallengeForm = () => {
   const navigate = useNavigate();
   const { challengeDetails, setChallengeDetails } = useContext(ChallengeDetailsContext);
   const { currentUser } = useContext(dataContext);
-
-  const challengeExpiration = () => {
-    if (challengeDetails.inviteOthers) {
-      const deadlineString = challengeDetails.deadline;
-      const ISOmin = DateTime.fromISO(deadlineString)
-      const min = ISOmin.plus({ days: 3 }).toFormat('yyyy-MM-dd');
-      const max = ISOmin.plus({ months: 5 }).toFormat('yyyy-MM-dd');
-      return { min, max };
-    }
-
-    const min = DateTime.local().plus({ days: 3 }).toFormat("yyyy-MM-dd");
-    const max = DateTime.local().plus({ months: 5 }).toFormat("yyyy-MM-dd");
-
-    return { min, max };
-  }
 
   const onButtonClick = () => navigate(`/currentUser/${currentUser.id}/challenge-form/add-video`, { state: { from: "ChallengeForm.jsx" } });
 
@@ -40,10 +27,10 @@ const ChallengeForm = () => {
         <legend className="col-form-label col-sm-5"><strong>INVITE OTHERS TO CHALLENGE?</strong></legend>
         <Col sm={7}>
           <FormGroup check>
-            <Input type='radio' name='inviteOthers' id='YES' value={challengeDetails.inviteOthers} onChange={() => setChallengeDetails(prv => ({...prv, inviteOthers: true}))} required />{' '}<Label for='YES' check>YES</Label>
+            <Input type='radio' name='inviteOthers' id='YES' value={challengeDetails.inviteOthers} onChange={() => setChallengeDetails(prv => ({...prv, inviteOthers: true}))} required />{' '}<Label for='YES' check><strong>YES</strong></Label>
           </FormGroup>
           <FormGroup check>
-            <Input type='radio' name='inviteOthers' id='NO' value={challengeDetails.inviteOthers} onChange={() => setChallengeDetails(prv => ({...prv, inviteOthers: false}))} />{' '}<Label for='NO' check>NO</Label>
+            <Input type='radio' name='inviteOthers' id='NO' value={challengeDetails.inviteOthers} onChange={() => setChallengeDetails(prv => ({...prv, inviteOthers: false}))} />{' '}<Label for='NO' check><strong>NO</strong></Label>
           </FormGroup>
         </Col>
       </FormGroup>
@@ -57,16 +44,15 @@ const ChallengeForm = () => {
       <FormGroup tag="fieldset">
         <legend>HOW DO YOU WANT YOUR CHALLENGE TO END?</legend>
         <FormGroup check>
-          <Input type='radio' name='end-conditions' value={challengeDetails.challengeEndsCondition} onChange={() => setChallengeDetails(prv => ({...prv, challengeEndsCondition: 'date'}))}/>
+          <Label for='date' check><strong>DATE</strong></Label>{' '}<Input type='radio' id='date' name='end-conditions' value={challengeDetails.challengeEndsCondition} onChange={() => setChallengeDetails(prv => ({...prv, challengeEndsCondition: 'date'}))}/>
         </FormGroup>
         <FormGroup check>
-          <Input type='radio' name='end-conditions' value={challengeDetails.challengeEndsCondition} onChange={() => setChallengeDetails(prv => ({...prv, challengeEndsCondition: 'votes'}))}/>
+          <Label for='votes' check><strong>VOTES</strong></Label>{' '}<Input type='radio' name='end-conditions' value={challengeDetails.challengeEndsCondition} onChange={() => setChallengeDetails(prv => ({...prv, challengeEndsCondition: 'votes'}))}/>
         </FormGroup>
       </FormGroup>
-      <FormGroup>
-        <Label for='endbydate'><strong>CHALLENGE END DATE</strong></Label>
-        <Input type='date' id='endbydate' min={challengeExpiration().min} max={challengeExpiration().max} placeholder='Select a date for when your challenge ends' onChange={e => setChallengeDetails(prv => ({...prv, challengeExpires: e.target.value}))} required />
-      </FormGroup>
+      {
+        challengeDetails.challengeEndsCondition == 'date' && <ExpirationDateOption />
+      }
       <FormGroup className='w-100'>
         <Button type='button' className='w-100' color='danger' size='lg' onClick={onButtonClick}>NEXT</Button>
       </FormGroup>
