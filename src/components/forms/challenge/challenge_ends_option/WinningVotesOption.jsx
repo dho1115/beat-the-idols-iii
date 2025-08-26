fd56e33485ddc834b7958c08f1264785974511f1
admin_dho <davidhorph@hotmail.com>
2025-08-26 18:24:58 -0500

    Logic for winningVotesOption.

--- src/components/forms/challenge/challenge_ends_option/WinningVotesOption.jsx
+++ src/components/forms/challenge/challenge_ends_option/WinningVotesOption.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
 import { FormGroup, Label, Input } from 'reactstrap';
+import { ChallengeDetailsContext } from '../ChallengeFormComponent';
 
 const WinningVotesOption = () => {
+  const { challengeDetails, setChallengeDetails } = useContext(ChallengeDetailsContext);
+  
   return (
     <FormGroup>
-      <Label for='votes'>WINNING VOTES</Label>
+      <Label for='votes'>THE CHALLENGE ENDS WHEN THE WINNER GETS HOW MANY VOTES?</Label>
+      <Input type='number' value={challengeDetails.winningVotes} placeholder='First To Reach This Number of Votes Wins!!!' min={5} max={105} onChange={e => setChallengeDetails(prv => ({...prv, winningVotes: e.target.value}))} />
     </FormGroup>
   )
 }
