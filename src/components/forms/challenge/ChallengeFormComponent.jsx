7ddef91515d50843bdd1b582a0ed51bca57594d8
admin_dho <davidhorph@hotmail.com>
2025-08-26 18:24:02 -0500

    Added a DEFAULT expiration Date.
    defaultExpirationDate is a function that assigns a DEFAULT date if the user wants the challenge to end via vote: howChallengeEnds = "votes" because we do not want the date to stay 0000-00-00.

--- src/components/forms/challenge/ChallengeFormComponent.jsx
+++ src/components/forms/challenge/ChallengeFormComponent.jsx
@@ -14,21 +14,39 @@ export const ChallengeDetailsContext = createContext();
 import './ChallengeFormComponent.styles.css';
 
 const ChallengeFormComponent = () => {
-   const [challengeDetails, setChallengeDetails] = useState({ id: null, posted: null, title: '', description: '', inviteOthers: '', deadline: "0000-00-00", challengeCoverType: '', challengeCoverImage: '', challengeEndsCondition: "", challengeExpires: "0000-00-00", challengeVideos: [] /* each video should have a record prop */, howChallengeEnds: '' });
+   const [challengeDetails, setChallengeDetails] = useState({ id: null, posted: null, title: '', description: '', inviteOthers: '', deadline: "0000-00-00", challengeCoverType: '', challengeCoverImage: '', challengeExpires: "0000-00-00", winningVotes: 0, challengeVideos: [] /* each video should have a record prop */, howChallengeEnds: '' });
 
    const { videos } = useContext(dataContext);
+
+     const defaultExpirationDate = () => {
+       if (challengeDetails.inviteOthers) {
+         const deadlineString = challengeDetails.deadline; //DEADLINE FOR PEOPLE TO APPLY.
+         const deadline = DateTime.fromISO(deadlineString)
+         const defaultExpiration = deadline.plus({ months: 5 }).toFormat('yyyy-MM-dd');
+          
+          return defaultExpiration;
+       }
+   
+       const defaultExpiration = DateTime.local().plus({ months: 5 }).toFormat("yyyy-MM-dd");
+       
+        return defaultExpiration;
+     }
 
    const handleSubmit = (e) => {
       e.preventDefault();
       const posted = DateTime.local().toFormat('yyyy-MM-dd');
       const _videoID = v4();
-      let submitChallengeDetails = { posted, id: _videoID, ...challengeDetails, _videoID };
+      let submitChallengeDetails = challengeDetails.howChallengeEnds == 'votes' ?
+         { posted, id: _videoID, ...challengeDetails, _videoID, challengeExpires: defaultExpirationDate() /* If user selects 'votes', we do NOT want the challenge to never expire, so this is the default expiration date */ }
+         :
+         { posted, id: _videoID, ...challengeDetails, _videoID }
+
       if (!challengeDetails.inviteOthers) delete submitChallengeDetails.deadline;
       alert(`About to submit the following: ${JSON.stringify({ ...challengeDetails, id: _videoID, posted })}.`)
    }
 
    useEffect(() => {
-      return () => setChallengeDetails({ id: null, posted: null, title: '', inviteOthers: '', deadline: "0000-00-00", description: '', challengeCoverImage: '', challengeCoverType: '', challengeExpires: '', challengeVideos: [] });
+      return () => setChallengeDetails({ id: null, posted: null, title: '', inviteOthers: '', deadline: "0000-00-00", description: '', challengeCoverImage: '', challengeCoverType: '', winningVotes: 0, challengeExpires: '', challengeVideos: [] });
    }, [])
 
    if (videos.length <= 2) return <ChallengeFormError videos={videos} />
