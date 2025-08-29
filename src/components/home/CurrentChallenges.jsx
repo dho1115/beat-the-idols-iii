2d3f768da1fe38dca2ae1c1e3bf13af3f74827f6
admin_dho <davidhorph@hotmail.com>
2025-08-29 17:17:15 -0500

    DOM manipulation with useRef.
    Manipulated the DOM using a combination of:
    1. currentChallengesRef = useRef( ).
    2. Inserting the currentChallengesRef inside the Container: ref = { CurrentChallengesRef }.
    3. Accessing the CSS properties via const setGridTemplateColumns = currentChallenges.length >= 5 ? " auto auto auto auto auto " : currentChallenges.map( ... ).

--- src/components/home/CurrentChallenges.jsx
+++ src/components/home/CurrentChallenges.jsx
@@ -1,31 +1,54 @@
-import React, { Suspense, useContext } from 'react';
+import React, { Suspense, useContext, useRef, useEffect } from 'react';
 
 import SuspenseFallback from '../suspense_fallback/SuspenseFallback';
 import { Container } from 'reactstrap';
 import { dataContext } from '../../App';
 
+import './Challenges.styles.css';
+
 const CurrentChallenges = () => {
   const { currentChallenges } = useContext(dataContext);
+  const currentChallengesRef = useRef();
+  const setGridTemplateColumns = currentChallenges.length >= 5 ? "auto auto auto auto auto" : currentChallenges.map((_, __, arr) => `${(100/(arr.length))-1}%`).join(" ")
+
+  useEffect(() => {
+    if (currentChallengesRef?.current?.style?.setGridTemplateColumns)currentChallengesRef.current.style.setGridTemplateColumns = setGridTemplateColumns
+    return () => {
+      
+    };
+  }, [])
 
   if (!currentChallenges.length) {
     return (
-      <div>
-        <Container className='my-5' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', border: '3px solid black', backgroundColor: 'peachpuff' }}>
-          <h1>SORRY... THERE ARE NO CURRENT CHALLENGES.</h1>
-        </Container>
-      </div>
+      <Container className='my-5' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', border: '3px solid black', backgroundColor: 'peachpuff' }}>
+        <h1>SORRY... THERE ARE NO CURRENT CHALLENGES.</h1>
+      </Container>
     )
   }
 
-  const challenges = currentChallenges.map(val => val);
+  const challenges = currentChallenges.map((val, idx) => (
+    <div key={idx}>
+      {
+        <div className='mx-1 p-1' style={{
+          overflowWrap: 'anywhere',
+          border: `1.5px solid ${idx%2 == 1 ? 'lightseagreen' : 'firebrick'}`,
+          backgroundColor: `${idx % 2 == 1 ? 'bisque' : 'antiquewhite'}`
+        }}>
+          <strong>{JSON.stringify(val)}</strong>
+        </div>
+      }
+    </div>
+  ));
 
   return (
-    <div>
-      <Suspense fallback={<SuspenseFallback />}>
-        <h1>CURRENT CHALLENGES.</h1>
-        <strong>{JSON.stringify(challenges)}</strong>
-      </Suspense>      
-    </div>
+    <Suspense fallback={<SuspenseFallback />}>
+      <h1>CURRENT CHALLENGES.</h1>
+      {
+        <Container className='challengesContainer p-3' ref={currentChallengesRef}>
+          { challenges }
+        </Container>
+      }
+    </Suspense>      
   )
 }
 
