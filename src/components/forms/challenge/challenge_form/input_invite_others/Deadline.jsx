ab8bccc2b0897048750f18ad73e763d97d56f421
admin_dho <davidhorph@hotmail.com>
2025-08-29 17:13:54 -0500

    changed label name.

--- src/components/forms/challenge/challenge_form/input_invite_others/Deadline.jsx
+++ src/components/forms/challenge/challenge_form/input_invite_others/Deadline.jsx
@@ -7,8 +7,8 @@ const Deadline = ({ challengeDetails, setChallengeDetails, ...args }) => {
    
    return (
       <FormGroup>
-         <Label for='deadline'>SELECT A DEADLINE FOR PEOPLE TO APPLY.</Label>
-         <Input type='date' id='deadline' value={challengeDetails.deadline} min={currentDate} max={add5months} onChange={e => setChallengeDetails(prv => ({...prv, deadline: e.target.value}))} required/>
+         <Label for='announcementDeadline'>SELECT A DEADLINE FOR PEOPLE TO APPLY.</Label>
+         <Input type='date' id='announcementDeadline' value={challengeDetails.announcementDeadline} min={currentDate} max={add5months} onChange={e => setChallengeDetails(prv => ({...prv, announcementDeadline: e.target.value}))} required/>
       </FormGroup>
    )
 }
