4b00e019bb3d8f891f802c9387c9402c603f02d2
admin_dho <davidhorph@hotmail.com>
2025-09-01 15:19:36 -0500

    Replace online.jsx, upload.jsx.
    Replaces the individual online.jsx and upload.jsx components.

---
+++ src/components/forms/challenge-announcement/challenge_announcement_form/CoverImgComponents.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react';
+import { ChallengeAnnouncementFormContext } from '../ChallengeAnnouncementFormComponent';
+import { FormGroup, Input, Label } from 'reactstrap';
+
+export const Online = () => {
+  const { challengeAnnouncementDetails, setChallengeAnnouncementDetails } = useContext(ChallengeAnnouncementFormContext);
+   return (
+      <FormGroup>
+         <Label for='online'>COPY IMAGE ADDRESS ONLINE & PASTE IMAGE ADDRESS BELOW.</Label>
+         <Input type='text' value={challengeAnnouncementDetails.coverImageLink} placeholder='Paste (online) image address here.' onChange={e => setChallengeAnnouncementDetails(prv => ({...prv, coverImageLink: e.target.value}))} />
+      </FormGroup>
+   )
+} //Online.
+
+export const Upload = () => {
+   const { challengeAnnouncementDetails, setChallengeAnnouncementDetails } = useContext(ChallengeAnnouncementFormContext);
+
+  return (
+    <FormGroup>
+      <Label for='upload'>UPLOAD YOUR COVER IMAGE.</Label>
+      <Input type='file' value={challengeAnnouncementDetails.coverImageLink} placeholder='Upload Cover Image From Device' onChange={e => setChallengeAnnouncementDetails(prv => ({ ...prv, coverImageLink: e.target.files[0] }))} required />
+    </FormGroup>
+  )
+} //Upload.
