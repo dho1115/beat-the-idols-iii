b3027f1932bae54858055f073147e34399a365a1
admin_dho <davidhorph@hotmail.com>
2025-09-04 12:25:54 -0500

    Deleted unnecessary Components.
    Deleted the Cover Image components and stuff related to it.

--- src/components/forms/challenge-announcement/challenge_announcement_form/ChallengeAnnouncementForm.jsx
+++ src/components/forms/challenge-announcement/challenge_announcement_form/ChallengeAnnouncementForm.jsx
@@ -22,24 +22,6 @@ const ChallengeAnnouncementForm = () => {
             <Input type='textarea' value={challengeAnnouncementDetails.description} placeholder='Description of your challenge' onChange={e => setChallengeAnnouncementDetails(prv => ({...prv, description: e.target.value}))} />
          </FormGroup>
          <Deadline />
-         {/* <FormGroup tag={'fieldset'}>
-            <legend>SOURCE OF YOUR COVER IMAGE?</legend>
-            <FormGroup check>
-               <Input type='radio' name='coverImageSource' value={challengeAnnouncementDetails.coverImageSource} required onChange={() => setChallengeAnnouncementDetails(prv => ({...prv, coverImageSource: 'online'}))} /> {' '} <Label check>ONLINE</Label>
-            </FormGroup>
-            <FormGroup check>
-               <Input type='radio' name='coverImageSource' value={challengeAnnouncementDetails.coverImageSource} required onChange={() => setChallengeAnnouncementDetails(prv => ({...prv, coverImageSource: 'upload'}))} /> {' '} <Label check>UPLOAD</Label>
-            </FormGroup>
-         </FormGroup>
-         {
-            challengeAnnouncementDetails.coverImageSource == "online" && <Online />
-         }
-         {
-            challengeAnnouncementDetails.coverImageSource == "upload" && <Upload />
-         }
-         <FormGroup>
-            <Button type='submit' color='danger' size='lg' className='w-100'>SUBMIT!!!</Button>
-         </FormGroup> */} {/* DELETING THIS FROM CHALLENGE ANNOUNCEMENT. I PLAN TO MOVE CHALLENGE ANNOUNCEMENT INTO THE CHALLENGE SECTION. */}
       </>
    )
 }
