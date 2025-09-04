8ac5bcf4c84c0bded2bb42d8db082ae4dca294d6
admin_dho <davidhorph@hotmail.com>
2025-09-04 12:22:35 -0500

    Moved To ChallengeAnnouncementForm.jsx

--- src/components/forms/challenge/challenge_form/input_invite_others/Deadline.jsx
+++ src/components/forms/challenge/challenge_form/input_invite_others/Deadline.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { FormGroup, Input, Label } from 'reactstrap';
 import { DateTime } from 'luxon';
+
+
 const Deadline = ({ challengeDetails, setChallengeDetails, ...args }) => {
    const currentDate = DateTime.local().toFormat('yyyy-MM-dd');
    const add5months = DateTime.local().plus({ months: 5 }).toFormat('yyyy-MM-dd');
    
    return (
       <FormGroup>
-         <Label for='announcementDeadline'>SELECT A DEADLINE FOR PEOPLE TO APPLY.</Label>
+         <Label for='announcementDeadline'>SELECT A DEADLINE FOR PEOPLE TO SEND THEIR VIDEOS.</Label>
          <Input type='date' id='announcementDeadline' value={challengeDetails.announcementDeadline} min={currentDate} max={add5months} onChange={e => setChallengeDetails(prv => ({...prv, announcementDeadline: e.target.value}))} required/>
       </FormGroup>
    )
