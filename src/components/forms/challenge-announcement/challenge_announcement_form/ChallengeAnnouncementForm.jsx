27104b83128a98de0af00c28e67d845e4030609f
admin_dho <davidhorph@hotmail.com>
2025-09-06 12:36:17 -0500

    Update ChallengeAnnouncementForm.jsx
    Destructured setChallengeAnnnouncement to use in the Input components.

--- src/components/forms/challenge-announcement/challenge_announcement_form/ChallengeAnnouncementForm.jsx
+++ src/components/forms/challenge-announcement/challenge_announcement_form/ChallengeAnnouncementForm.jsx
@@ -1,6 +1,6 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { Form, FormGroup, Button, Input, Label } from 'reactstrap'
-import { ChallengeAnnouncementFormContext } from '../ChallengeAnnouncementFormComponent';
+import { ChallengeDetailsContext } from '../../challenge/ChallengeFormComponent';
 
 //Cover Image Components.
 import { Online, Upload } from './CoverImgComponents';
@@ -9,20 +9,20 @@ import Deadline from '../../challenge/challenge_form/input_invite_others/Deadlin
 import "./ChallengeAnnouncementForm.styles.css";
 
 const ChallengeAnnouncementForm = () => {
-   const { challengeAnnouncementDetails, setChallengeAnnouncementDetails } = useContext(ChallengeAnnouncementFormContext);
+   const { challengeAnnouncement, setChallengeAnnouncement } = useContext(ChallengeDetailsContext);
 
    return (
-      <>
+      <div className='challenge-announcement-details p-3 m-1'>
          <FormGroup>
             <Label for='title'>HEADLINE FOR YOUR ANNOUNCEMENT!!!</Label>
-            <Input type='text' value={challengeAnnouncementDetails.title} placeholder='your headline will be shown in the card' maxLength={51} onChange={e => setChallengeAnnouncementDetails(prv => ({...prv, title: e.target.value}))} required />
+            <Input type='text' value={challengeAnnouncement.headline} placeholder='your headline will be shown in the card' maxLength={51} onChange={e => setChallengeAnnouncement(prv => ({...prv, headline: e.target.value}))} required />
          </FormGroup>
          <FormGroup>
             <Label for='description'>ANYTHING YOU WANT TO SAY ABOUT THIS ANNOUNCEMENT?.</Label>
-            <Input type='textarea' value={challengeAnnouncementDetails.description} placeholder='Description of your challenge' onChange={e => setChallengeAnnouncementDetails(prv => ({...prv, description: e.target.value}))} />
+            <Input type='textarea' value={challengeAnnouncement.description} placeholder='Description of your challenge' onChange={e => setChallengeAnnouncement(prv => ({...prv, description: e.target.value}))} />
          </FormGroup>
-         <Deadline />
-      </>
+         <Deadline challengeAnnouncement={challengeAnnouncement} setChallengeAnnouncement={setChallengeAnnouncement} />
+      </div>
    )
 }
 
