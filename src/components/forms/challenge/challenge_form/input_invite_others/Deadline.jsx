ddbd68a261d6bb4db85f1c57cc89c6bfdfcd1f2d
admin_dho <davidhorph@hotmail.com>
2025-09-06 12:35:00 -0500

    Update Deadline.jsx
    Changed the value prperties for the Input.

--- src/components/forms/challenge/challenge_form/input_invite_others/Deadline.jsx
+++ src/components/forms/challenge/challenge_form/input_invite_others/Deadline.jsx
@@ -3,14 +3,14 @@ import { FormGroup, Input, Label } from 'reactstrap';
 import { DateTime } from 'luxon';
 
 
-const Deadline = ({ challengeDetails, setChallengeDetails, ...args }) => {
+const Deadline = ({ challengeAnnouncement, setChallengeAnnouncement, ...args }) => {
    const currentDate = DateTime.local().toFormat('yyyy-MM-dd');
    const add5months = DateTime.local().plus({ months: 5 }).toFormat('yyyy-MM-dd');
    
    return (
       <FormGroup>
          <Label for='announcementDeadline'>SELECT A DEADLINE FOR PEOPLE TO SEND THEIR VIDEOS.</Label>
-         <Input type='date' id='announcementDeadline' value={challengeDetails.announcementDeadline} min={currentDate} max={add5months} onChange={e => setChallengeDetails(prv => ({...prv, announcementDeadline: e.target.value}))} required/>
+         <Input type='date' id='announcementDeadline' value={challengeAnnouncement.announcementEndsOn} min={currentDate} max={add5months} onChange={e => setChallengeAnnouncement(prv => ({...prv, announcementEndsOn: e.target.value}))} required/>
       </FormGroup>
    )
 }
