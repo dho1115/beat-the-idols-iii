a4fec2c9a4ec0107c6c5ce2053a022d86a5f22d6
admin_dho <davidhorph@hotmail.com>
2025-09-07 10:42:20 -0500

    Created "cells" (div) for each Challenge.
    These cells successfully displays the challenges. All information shows. Only tested for the "votes" and not for the "challenge ends" expiration date.

--- src/components/home/ChallengeAnnouncements.jsx
+++ src/components/home/ChallengeAnnouncements.jsx
@@ -1,22 +1,34 @@
-import React, { useContext } from 'react';
-
+import React, { useContext, useRef, useEffect } from 'react';
 import { dataContext } from '../../App';
 import { Container } from 'reactstrap';
 
+import './Challenges.styles.css';
+
 const ChallengeAnnouncements = () => {
    const { challengeAnnouncements } = useContext(dataContext);
+   const challengeAnnouncementsRef = useRef();
+
+   useEffect(() => {
+      if (challengeAnnouncements.length > 1) {
+         challengeAnnouncementsRef.current.style.gridTemplateColumns = "auto auto auto"
+      }
+   }, [challengeAnnouncements.length])
 
    if (challengeAnnouncements.length) {
+      
       return (
-         <div>
+         <Container className='challenge-announcements-container' ref={challengeAnnouncementsRef}>
             {
-               ChallengeAnnouncements.map((val, idx) => (
-                  <div className='m-1 p-1' key={idx}>
-                     <strong>{JSON.stringify({...val})}</strong>
+               challengeAnnouncements.map((val, idx) => (
+                  <div className='m-1 p-1' key={idx} style={{overflowWrap: 'anywhere', overflow: 'hidden', border: idx%2==1 ? '1.5px solid green' : '1.5px solid firebrick', backgroundColor: idx%2==1 ? 'lightpink' : 'lightyellow'}}>
+                     <strong>{JSON.stringify({ ...val })}</strong>
+                     <div className='w-100'>
+                        <button className='w-100 btn btn-success' onClick={() => alert(JSON.stringify({...val}))}>DETAILS!!!</button>
+                     </div>
                   </div>
                ))
             }
-         </div>
+         </Container>
       )
    }
    return (
