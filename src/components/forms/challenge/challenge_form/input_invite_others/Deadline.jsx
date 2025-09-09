7dd5c2ae17387a519a806d57c09649d169e11e4a
admin_dho <davidhorph@hotmail.com>
2025-09-09 15:43:11 -0500

    setChallengeDetails.
    resets challengeEndsOn to 0000-00-00 if the user changes the announcement deadline.

--- src/components/forms/challenge/challenge_form/input_invite_others/Deadline.jsx
+++ src/components/forms/challenge/challenge_form/input_invite_others/Deadline.jsx
@@ -4,13 +4,17 @@ import { DateTime } from 'luxon';
 
 
 const Deadline = ({ challengeAnnouncement, setChallengeAnnouncement, ...args }) => {
+   const { setChallengeDetails } = args;
    const currentDate = DateTime.local().toFormat('yyyy-MM-dd');
    const add5months = DateTime.local().plus({ months: 5 }).toFormat('yyyy-MM-dd');
    
    return (
       <FormGroup>
          <Label for='announcementDeadline'>SELECT A DEADLINE FOR PEOPLE TO SEND THEIR VIDEOS.</Label>
-         <Input type='date' id='announcementDeadline' value={challengeAnnouncement.announcementEndsOn} min={currentDate} max={add5months} onChange={e => setChallengeAnnouncement(prv => ({...prv, announcementEndsOn: e.target.value}))} required/>
+         <Input type='date' id='announcementDeadline' value={challengeAnnouncement.announcementEndsOn} min={currentDate} max={add5months} onChange={e => {
+            setChallengeDetails(prv => ({...prv, challengeEndsOn: "0000-00-00"})) //resetting challenge details IF user changes deadline.
+            setChallengeAnnouncement(prv => ({...prv, announcementEndsOn: e.target.value}))
+         }} required/>
       </FormGroup>
    )
 }
