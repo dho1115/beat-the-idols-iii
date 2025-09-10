e5e4d0400078f93e05a7d5e36baabc621499827f
admin_dho <davidhorph@hotmail.com>
2025-09-10 15:26:41 -0500

    Added validation inside Input.announcementDeadline.

--- src/components/forms/challenge/challenge_form/input_invite_others/Deadline.jsx
+++ src/components/forms/challenge/challenge_form/input_invite_others/Deadline.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import { FormGroup, Input, Label } from 'reactstrap';
+import { Alert, FormGroup, Input, Label } from 'reactstrap';
 import { DateTime } from 'luxon';
 
 
 const Deadline = ({ challengeAnnouncement, setChallengeAnnouncement, ...args }) => {
-   const { setChallengeDetails } = args;
+   const { challengeDetails, setChallengeDetails, dateAlert, setDateAlert } = args;
    const currentDate = DateTime.local().toFormat('yyyy-MM-dd');
    const add5months = DateTime.local().plus({ months: 5 }).toFormat('yyyy-MM-dd');
    
    return (
       <FormGroup>
+         {
+            dateAlert && <Alert color='danger'><strong>Your announcement end date of {challengeAnnouncement.announcementEndsOn} <i>CANNOT</i> be greater than your actual challenge expiration date of {challengeDetails.challengeExpires}</strong></Alert>
+         }
          <Label for='announcementDeadline'>SELECT A DEADLINE FOR PEOPLE TO SEND THEIR VIDEOS.</Label>
          <Input type='date' id='announcementDeadline' value={challengeAnnouncement.announcementEndsOn} min={currentDate} max={add5months} onChange={e => {
-            setChallengeDetails(prv => ({...prv, challengeEndsOn: "0000-00-00"})) //resetting challenge details IF user changes deadline.
-            setChallengeAnnouncement(prv => ({...prv, announcementEndsOn: e.target.value}))
+            e.target.value < challengeDetails.challengeExpires && setDateAlert(false);
+            setChallengeAnnouncement(prv => ({ ...prv, announcementEndsOn: e.target.value }))
          }} required/>
       </FormGroup>
    )
