0d6e7a188af8bb294640bc1f64ad32ed2f0ce17d
admin_dho <davidhorph@hotmail.com>
2025-09-13 09:16:32 -0500

    Fixed the description.length error.

--- src/components/templates/challenge_wrapper/ChallengeWrapper.jsx
+++ src/components/templates/challenge_wrapper/ChallengeWrapper.jsx
@@ -13,25 +13,29 @@ const ChallengeWrapper = ({ coverImg, title, description, button_text, clickLogi
    const { _ownerID, idx } = args;
    const { allUsers } = useContext(dataContext);
 
-   const {username, email} = allUsers.find(({ id }) => id == _ownerID);
+   const { username, email } = allUsers.find(({ id }) => id == _ownerID);
 
    return (
-      <Card className='m-1' style={{ border: idx % 2 == 1 ? '3.5px solid lightseagreen' : '3.5px solid red', height: '100%', borderRadius: '15.3px' }}>
+      <Card className='m-1' style={{ border: idx % 2 == 1 ? '3.5px solid lightseagreen' : '3.5px solid red', height: '30%', width: '100%', borderRadius: '15.3px' }}>
          <Suspense fallback={<SuspenseFallback />}>
             <div className='w-100 p-0 cover-img-div'>
-               <img src={coverImg} alt='cover pic for challenge wrapper' className='cover-img img-fluid'/>
+               <img src={coverImg} alt='cover pic for challenge wrapper' className='cover-img'/>
             </div>
             <CardTitle className='p-1'>
                <strong>
-                  {title.length > 15 ? title.substring(0, 15) + "..." : title}
+                  {title.length > 19 ? title.substring(0, 19) + "..." : title}
                </strong>
             </CardTitle>
             <CardBody className='px-1'>
                <strong>Posted By: {username}</strong>
                {
-                  description &&
+                  description && description.length >= 37 ?
                   <CardText className='px-1' style={{paddingBottom: '5px', height: '100%'}}>
-                     {description.length >= 31 ? <p>{description.substring(0, 30)}...</p> : <p>{description}</p>}
+                     {description.substring(0, 37) + "..."}
+                  </CardText>
+                  :
+                  <CardText className='px-1' style={{paddingBottom: '5px', height: '100%'}}>
+                     {description}
                   </CardText>
                }
             </CardBody>            
