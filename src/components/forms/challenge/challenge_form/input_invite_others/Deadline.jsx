cd11aad756f17af79a41678dcf4908934c16600a
admin_dho <davidhorph@hotmail.com>
2025-09-13 09:15:34 -0500

    Update Deadline.jsx
    Moved the logic from inside the onChange of Input, created a separate ChallengeAnnouncementDeadlineLogic and moved that code/logic inside here.

--- src/components/forms/challenge/challenge_form/input_invite_others/Deadline.jsx
+++ src/components/forms/challenge/challenge_form/input_invite_others/Deadline.jsx
@@ -6,6 +6,12 @@ const Deadline = ({ challengeAnnouncement, setChallengeAnnouncement, ...args }) 
    const { challengeDetails, setChallengeDetails, dateAlert, setDateAlert } = args;
    const currentDate = DateTime.local().toFormat('yyyy-MM-dd');
    const add5months = DateTime.local().plus({ months: 5 }).toFormat('yyyy-MM-dd');
+
+   const challengeAnnouncementDeadlineLogic = e => {
+      ((e.target.value > challengeDetails.challengeExpires) && (challengeDetails.challengeExpires != '0000-00-00')) ? setDateAlert(true) : setDateAlert(false);
+
+      setChallengeAnnouncement(prv => ({ ...prv, announcementEndsOn: e.target.value }))
+   }
    
    return (
       <FormGroup>
@@ -13,10 +19,7 @@ const Deadline = ({ challengeAnnouncement, setChallengeAnnouncement, ...args }) 
             dateAlert && <Alert color='danger'><strong>Your announcement end date of {challengeAnnouncement.announcementEndsOn} <i>CANNOT</i> be greater than your actual challenge expiration date of {challengeDetails.challengeExpires}</strong></Alert>
          }
          <Label for='announcementDeadline'>SELECT A DEADLINE FOR PEOPLE TO SEND THEIR VIDEOS.</Label>
-         <Input type='date' id='announcementDeadline' value={challengeAnnouncement.announcementEndsOn} min={currentDate} max={add5months} onChange={e => {
-            e.target.value < challengeDetails.challengeExpires && setDateAlert(false);
-            setChallengeAnnouncement(prv => ({ ...prv, announcementEndsOn: e.target.value }))
-         }} required/>
+         <Input type='date' id='announcementDeadline' value={challengeAnnouncement.announcementEndsOn} min={currentDate} max={add5months} onChange={challengeAnnouncementDeadlineLogic} required/>
       </FormGroup>
    )
 }
