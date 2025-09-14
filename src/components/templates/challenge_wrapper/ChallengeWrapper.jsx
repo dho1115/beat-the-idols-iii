911ad97d564703ece7d891cd956a4c76718904f4
admin_dho <davidhorph@hotmail.com>
2025-09-14 08:18:42 -0500

    Removed the description from ChallengeWrapper.
    Removed the Challenge description. I plan to put it inside the Challenge Details.
    
    Also, modified width of <Card />

--- src/components/templates/challenge_wrapper/ChallengeWrapper.jsx
+++ src/components/templates/challenge_wrapper/ChallengeWrapper.jsx
@@ -16,7 +16,7 @@ const ChallengeWrapper = ({ coverImg, title, description, button_text, clickLogi
    const { username, email } = allUsers.find(({ id }) => id == _ownerID);
 
    return (
-      <Card className='m-1' style={{ border: idx % 2 == 1 ? '3.5px solid lightseagreen' : '3.5px solid red', height: '30%', width: '100%', borderRadius: '15.3px' }}>
+      <Card className='m-1' style={{ border: idx % 2 == 1 ? '3.5px solid lightseagreen' : '3.5px solid red', height: '30%', width: '19%', borderRadius: '15.3px' }}>
          <Suspense fallback={<SuspenseFallback />}>
             <div className='w-100 p-0 cover-img-div'>
                <img src={coverImg} alt='cover pic for challenge wrapper' className='cover-img'/>
@@ -25,20 +25,7 @@ const ChallengeWrapper = ({ coverImg, title, description, button_text, clickLogi
                <strong>
                   {title.length > 19 ? title.substring(0, 19) + "..." : title}
                </strong>
-            </CardTitle>
-            <CardBody className='px-1'>
-               <strong>Posted By: {username}</strong>
-               {
-                  description && description.length >= 37 ?
-                  <CardText className='px-1' style={{paddingBottom: '5px', height: '100%'}}>
-                     {description.substring(0, 37) + "..."}
-                  </CardText>
-                  :
-                  <CardText className='px-1' style={{paddingBottom: '5px', height: '100%'}}>
-                     {description}
-                  </CardText>
-               }
-            </CardBody>            
+            </CardTitle>    
             {
                (button_text && clickLogic)
                &&
