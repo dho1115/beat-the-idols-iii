573e48770853153f9db67329bb82a3afb1937c3d
admin_dho <davidhorph@hotmail.com>
2025-09-15 13:04:36 -0500

    Update AnnouncementDetailsComponent.jsx
    Added logic to show eligible videos to add.

--- src/components/home/challenge-announcements/AnnouncementDetailsComponent.jsx
+++ src/components/home/challenge-announcements/AnnouncementDetailsComponent.jsx
@@ -11,6 +11,7 @@ import YouTubeVideo from '../../templates/video/you_tube/YouTubeVideo';
 import { dataContext } from '../../../App';
 
 import "../Challenges.styles.css";
+import { video } from 'motion/react-client';
 
 const AnnouncementDetailsComponent = () => {
    const { user, id } = useParams();
@@ -19,24 +20,36 @@ const AnnouncementDetailsComponent = () => {
    const [announcement, setAnnouncement] = useState({})
    const [actualChallenge, setActualChallenge] = useState({})
    const [videosInChallenge, setVideosInChallenge] = useState([]);
-
+   const [videosEligibleForChallenge, setvideosEligibleForChallenge] = useState({ show: false, videos: [] });
    const announcementDetails = challengeAnnouncements.find(({ announcement: { _challengeAnnouncementID } }) => _challengeAnnouncementID == id)
 
    const announcementOwner = allUsers.find(val => val.id == user);
+
+   const showEligibleVideos = () => {
+      const eligibleVideos = videos.filter(({ _userID, id: videoID }) => (_userID == user) && (!videosInChallenge.includes(videoID)))
+
+      setvideosEligibleForChallenge({ show: !videosEligibleForChallenge.show, videos: [...eligibleVideos] });
+      return eligibleVideos;
+   }
 
    useEffect(() => {
       setAnnouncement(prv => ({ ...prv, ...announcementDetails.announcement, owner: announcementOwner.username }));
       setActualChallenge(prv => ({ ...prv, ...announcementDetails.challenge }));
       setVideosInChallenge(prv => ([...prv, ...announcementDetails.challenge.challengeVideos]));
+
+      return (() => {
+         setvideosEligibleForChallenge({ show: false, videos: [] });
+         setVideosInChallenge([]);
+         setActualChallenge({});
+         setAnnouncement({});
+      })
    }, [])
    
    return (
       <div className='m-1 p-1'>
-         <header>
-            <h3>CHALLENGE #{id} OWNED BY {announcement.owner}.</h3>
-         </header>
          <div className='announcementDetailsProfile w-100'>
             <div className='m-1'>
+               <h3>Announcement ID: {id}</h3>
                <h3>Owner: <strong className='text-danger'>{announcement.owner}</strong>.</h3>
                <hr />
                <h5>Description: <strong className='text-danger'>{announcement.description}</strong>.</h5>
@@ -71,7 +84,27 @@ const AnnouncementDetailsComponent = () => {
                   ))
                }
             </Container>
-            <Button color='danger' size='lg'><strong>JOIN THIS CHALLENGE!!!</strong></Button>
+            <Button color='danger' size='lg' onClick={showEligibleVideos}><strong>JOIN THIS CHALLENGE!!!</strong></Button>
+         </Container>
+         <Container>
+            <header><h1>VIDEOS ELIGIBLE FOR CHALLENGE:</h1></header>
+            {
+               (videosEligibleForChallenge.show && !videosEligibleForChallenge.videos.length)
+               &&
+               <h1>You have no eligible videos for challenge (probably because they are already in the challenge).</h1>
+            }
+            {
+               (videosEligibleForChallenge.show && videosEligibleForChallenge.length)
+               &&
+               <div>
+                  <strong>{JSON.stringify(videosEligibleForChallenge.videos)}</strong>
+               </div>
+            }
+            {
+               videosEligibleForChallenge.show
+               &&
+               <Button color='success' size='lg' onClick={() => setvideosEligibleForChallenge(prv => ({ ...prv, show: false }))}>CLOSE THIS PANEL.</Button>
+            }
          </Container>
       </div>
    )
