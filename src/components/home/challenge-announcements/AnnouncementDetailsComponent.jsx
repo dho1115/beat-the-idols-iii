894a24cc3ee3e49fd7847e2474235245dfcfe73b
admin_dho <davidhorph@hotmail.com>
2025-09-20 17:49:34 -0500

    Moved videosEliebleForChallenge.eligibeVideos
    Moved the logic to display/add eligible videos to Challenge to AddVideosToChallenge.jsx.

--- src/components/home/challenge-announcements/AnnouncementDetailsComponent.jsx
+++ src/components/home/challenge-announcements/AnnouncementDetailsComponent.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import { Button, Container } from 'reactstrap';
 
 //Components.
+import AddVideosToChallenge from './AddVideosToChallenge';
 import UploadVideo from '../../templates/video/upload/UploadVideo';
 import VideoWrapper from '../../templates/video_wrapper/VideoWrapper';
 import YouTubeVideo from '../../templates/video/you_tube/YouTubeVideo';
@@ -11,31 +12,33 @@ import YouTubeVideo from '../../templates/video/you_tube/YouTubeVideo';
 import { dataContext } from '../../../App';
 
 import "../Challenges.styles.css";
-import { video } from 'motion/react-client';
 
 const AnnouncementDetailsComponent = () => {
-   const { user, id } = useParams();
-   const { challengeAnnouncements, allUsers, videos } = useContext(dataContext);
-
+   const { user, id } = useParams(); //_userid, challenge announcement id.
+   const { challengeAnnouncements, currentUser, allUsers, videos } = useContext(dataContext);
+   const { personalImages } = currentUser;
    const [announcement, setAnnouncement] = useState({})
    const [actualChallenge, setActualChallenge] = useState({})
    const [videosInChallenge, setVideosInChallenge] = useState([]);
-   const [videosEligibleForChallenge, setvideosEligibleForChallenge] = useState({ show: false, videos: [] });
-   const announcementDetails = challengeAnnouncements.find(({ announcement: { _challengeAnnouncementID } }) => _challengeAnnouncementID == id)
+   const [videosEligibleForChallenge, setvideosEligibleForChallenge] = useState({ show: false, eligibleVideos: [] });
 
+   const announcementDetails = challengeAnnouncements.find(({ announcement: { _challengeAnnouncementID } }) => _challengeAnnouncementID == id)
+   
    const announcementOwner = allUsers.find(val => val.id == user);
 
    const showEligibleVideos = () => {
-      const eligibleVideos = videos.filter(({ _userID, id: videoID }) => (_userID == user) && (!videosInChallenge.includes(videoID)))
+      const eligibleVideos = videos.filter(({ _userID, id: videoID }) => (_userID == user) && (!announcementDetails.challenge.challengeVideos.includes(videoID)))
 
-      setvideosEligibleForChallenge({ show: !videosEligibleForChallenge.show, videos: [...eligibleVideos] });
+      setvideosEligibleForChallenge({ show: !videosEligibleForChallenge.show, eligibleVideos: [...eligibleVideos] });
       return eligibleVideos;
    }
 
    useEffect(() => {
+      const videosInChallenge = videos.filter(({ id }) => announcementDetails.challenge.challengeVideos.includes(id)); //videos that joined challenge.
+
       setAnnouncement(prv => ({ ...prv, ...announcementDetails.announcement, owner: announcementOwner.username }));
       setActualChallenge(prv => ({ ...prv, ...announcementDetails.challenge }));
-      setVideosInChallenge(prv => ([...prv, ...announcementDetails.challenge.challengeVideos]));
+      setVideosInChallenge(prv => ([...prv, ...videosInChallenge]));
 
       return (() => {
          setvideosEligibleForChallenge({ show: false, videos: [] });
@@ -44,7 +47,7 @@ const AnnouncementDetailsComponent = () => {
          setAnnouncement({});
       })
    }, [])
-   
+
    return (
       <div className='m-1 p-1'>
          <div className='announcementDetailsProfile w-100'>
@@ -55,7 +58,7 @@ const AnnouncementDetailsComponent = () => {
                <h5>Description: <strong className='text-danger'>{announcement.description}</strong>.</h5>
             </div>
             <div className='announcementDetailsCoverImg'>
-               <img src={announcement.cover_img} className='img-fluid'/>
+               <img src={personalImages[0]} className='img-fluid'/>
             </div>
          </div>
          <Container>
@@ -65,7 +68,7 @@ const AnnouncementDetailsComponent = () => {
             </headline>
             <Container className="announcement-videos-container p-3">
                {
-                  videos.map(({ title, description, videoType, urlOrFile, username }, idx) => (
+                  videosInChallenge.map(({ title, description, videoType, urlOrFile, username }, idx) => (
                      <VideoWrapper
                         key={idx}
                         color={idx%2==1 ? "red" : "yellow"}
@@ -84,21 +87,41 @@ const AnnouncementDetailsComponent = () => {
                   ))
                }
             </Container>
-            <Button color='danger' size='lg' onClick={showEligibleVideos}><strong>JOIN THIS CHALLENGE!!!</strong></Button>
+            {
+               !videosEligibleForChallenge.show && <Button color='danger' size='lg' onClick={showEligibleVideos}><strong>JOIN THIS CHALLENGE!!!</strong></Button>
+            }            
          </Container>
          <Container>
-            <header><h1>VIDEOS ELIGIBLE FOR CHALLENGE:</h1></header>
             {
-               (videosEligibleForChallenge.show && !videosEligibleForChallenge.videos.length)
+               (videosEligibleForChallenge.show && !videosEligibleForChallenge.eligibleVideos.length)
                &&
                <h1>You have no eligible videos for challenge (probably because they are already in the challenge).</h1>
             }
             {
-               (videosEligibleForChallenge.show && videosEligibleForChallenge.length)
+               (videosEligibleForChallenge.show && videosEligibleForChallenge.eligibleVideos.length)
                &&
-               <div>
-                  <strong>{JSON.stringify(videosEligibleForChallenge.videos)}</strong>
-               </div>
+               <>
+                  {/* <h1>*** SELECT VIDEO(S) TO ADD TO CHALLENGE ***</h1>
+                  <div className="eligibleVideosDiv">
+                        {
+                        videosEligibleForChallenge.eligibleVideos.map((val, idx) => {
+                           const video_component = val.videoType == 'you-tube' ? <YouTubeVideo url={val.urlOrFile} title={val.title} /> : <UploadVideo file={val.urlOrFile} />
+                           return (
+                              <VideoWrapper
+                                 key={idx}
+                                 idx={idx}
+                                 video_component={video_component}
+                                 username={val.username}
+                                 title={val.title}
+                                 description={val.description}
+                                 button_text="ADD THIS VIDEO!!!"
+                                 clickLogic={() => console.log(`About to add ${JSON.stringify(val)} to challenge-announcement ${id}!!!`)}
+                              />
+                           )
+                        })
+                        }
+                  </div> */} {/* Moved to AddVideosToChallenge.jsx */}
+               </>
             }
             {
                videosEligibleForChallenge.show
