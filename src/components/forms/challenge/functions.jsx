31028d8e303a539f4853819f4819c4cbf7960625
admin_dho <davidhorph@hotmail.com>
2025-10-07 15:44:50 -0500

    Added CRUD functions.

--- src/components/forms/challenge/functions.jsx
+++ src/components/forms/challenge/functions.jsx
@@ -1,13 +1,32 @@
-export const defaultExpirationDate = (challengeDetails, challengeAnnouncement, DateTime) => {
-      if (challengeDetails.challengeAnnouncementID) {
-         const deadlineString = challengeAnnouncement.announcementEndsOn;
-         const deadline = DateTime.fromISO(deadlineString)
-         const defaultChallengeExpiration = deadline.plus({ months: 5 }).toFormat('yyyy-MM-dd');
-         
-         return defaultChallengeExpiration;
-      }
+import { PostDataAPI } from "../../../functions/postapi";
 
-      const defaultChallengeExpiration = DateTime.local().plus({ months: 5 }).toFormat("yyyy-MM-dd");
+export const defaultExpirationDate = (challengeDetails, challengeAnnouncement, DateTime) => {
+   if (challengeDetails.challengeAnnouncementID) {
+      const deadlineString = challengeAnnouncement.announcementEndsOn;
+      const deadline = DateTime.fromISO(deadlineString)
+      const defaultChallengeExpiration = deadline.plus({ months: 5 }).toFormat('yyyy-MM-dd');
       
       return defaultChallengeExpiration;
    }
+
+   const defaultChallengeExpiration = DateTime.local().plus({ months: 5 }).toFormat("yyyy-MM-dd");
+   
+   return defaultChallengeExpiration;
+}
+
+//CRUD FUNCTIONS.
+
+export const AddChallengeToDB = (link=null, data=null, setState=null) => {
+   const dataTypes = JSON.stringify({ link: typeof (link), data: typeof (data), setState: typeof (setState) });
+   try {
+      if (typeof(link) != 'string' || typeof(data) != 'object' || typeof(setState) != 'function') throw new Error(`Wrong data type for one or more of the arguments. link and data must be a string and setState must be a function. You have: ${dataTypes}.`)
+
+      return PostDataAPI(link, data)
+         .then(result => setState(prv => ([...prv, result.jsonData])))
+         .catch(error => console.error({ error, errorCode: error.code, errorMessage: error.message }));
+   }
+   catch (err) {
+      console.error({ err, errCode: err.code, errMessage: err.message })
+      return err;
+   }
+}
