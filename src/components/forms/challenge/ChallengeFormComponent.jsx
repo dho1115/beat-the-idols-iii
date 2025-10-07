ff1d8c3306c937dd35b158a446090424d7c17d2a
admin_dho <davidhorph@hotmail.com>
2025-10-07 13:58:22 -0500

    commented out setChallengeAnnouncement.
    Commented out (and will remove) setChallengeAnnouncement inside handleSubmit b/c setChallengeAnnouncements (plural) will not update after setChallengeAnnouncement b/c both are in the same function body.

--- src/components/forms/challenge/ChallengeFormComponent.jsx
+++ src/components/forms/challenge/ChallengeFormComponent.jsx
@@ -28,21 +28,18 @@ const ChallengeFormComponent = () => {
       e.preventDefault();
       const posted = DateTime.local().toFormat('yyyy-MM-dd');
 
-      // const defaultExpiration = defaultExpirationDate(challengeDetails, challengeAnnouncement, DateTime);
 
       const form_content = challengeDetails.howChallengeEnds == 'votes' ?
          { ...challengeDetails, posted, challengeEndsOn: defaultExpirationDate(challengeDetails, challengeAnnouncement, DateTime) }
          :
          { ...challengeDetails, posted };
-      
-      // setChallengeDetails(prv => ({ ...prv, ...form_content }));
 
       if (challengeAnnouncement._challengeAnnouncementID) {
          const form_challenge_announcement = { posted, _announcementOwnerID: currentUser.id, challengeVideoIDs: [...challengeDetails.challengeVideoIDs], cover_img: challengeDetails.challengeCoverImage }
 
-         setChallengeAnnouncement(prv => ({ ...prv, ...form_challenge_announcement })); //set challengeAnnouncement only if user selected it.
+         // setChallengeAnnouncement(prv => ({ ...prv, ...form_challenge_announcement })); //set challengeAnnouncement only if user selected it.
 
-         setChallengeAnnouncements(prv => ([...prv, { announcement: { ...challengeAnnouncement }, challenge: { ...challengeDetails } }])); //from dataContext in App.jsx.
+         setChallengeAnnouncements(prv => ([...prv, { announcement: { ...challengeAnnouncement, ...form_challenge_announcement }, challenge: { ...challengeDetails } }])); //from dataContext in App.jsx.
          
          navigate(`/currentUser/${currentUser.id}/view/challenges/announcements`)
       } else {
