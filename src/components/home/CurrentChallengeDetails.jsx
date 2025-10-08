54951dac36e481c8722c8a16e235639e4576ec8b
admin_dho <davidhorph@hotmail.com>
2025-10-08 12:06:20 -0500

    Update CurrentChallengeDetails.jsx
    Created logic to make sure the user cannot go into the url bar and randomly type in a _challengeID.
    
    BEFORE: If the user did this, the app breaks.
    
    AFTER: Using this piece of code:
     const validateParams = currentChallenges.find(({ id }) => id == _challengeID)
    
    Along with an if/then logic, the user will be taken to the homepage using <Navigate />.

--- src/components/home/CurrentChallengeDetails.jsx
+++ src/components/home/CurrentChallengeDetails.jsx
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react'
-import { useLocation, useParams } from 'react-router-dom'
+import React, { useContext, useEffect } from 'react'
+import { Navigate, useNavigate, useParams } from 'react-router-dom'
 import { Button, Container } from 'reactstrap';
 
 import { dataContext } from '../../App';
@@ -8,33 +8,39 @@ import "./Challenges.styles.css";
 
 
 const CurrentChallengeDetails = () => {
+   const navigate = useNavigate();
    const { _challengeID } = useParams();
-
    const { currentChallenges, currentUser } = useContext(dataContext);
 
-   const { challengeCoverImage, challengeEndsOn, _challengeOwnerID, challengeVideoIDs, description, title, winningVotes } = currentChallenges.find(val => val._challengeID == _challengeID);
+   const validateParams = currentChallenges.find(({ id }) => id == _challengeID)
 
-   return (
-      <div>
-         <div className='challenge-details-header my-3'>
-            <div className='challenge-details-description-div'>
-               <h3>Challenge ID: <span className='text-danger'>{_challengeID}</span></h3>
-               <h3>EXPIRES: {challengeEndsOn}.</h3>
-               <h3>TITLE: <span className='text-danger'>{title}</span>.</h3>
-               <h3><span className='text-danger'>{description}</span>.</h3>
-               {
-                  winningVotes && <h3>First video to reach <span className='text-danger'>{winningVotes}</span> votes wins!!!.</h3>
-               }
+   if (validateParams) {
+      const { challengeCoverImage, challengeEndsOn, _challengeOwnerID, challengeVideoIDs, description, title, winningVotes } = currentChallenges.find(val => val._challengeID == _challengeID);
+      return (
+         <div>
+            <div className='challenge-details-header my-3'>
+               <div className='challenge-details-description-div'>
+                  <h3>Challenge ID: <span className='text-danger'>{_challengeID}</span></h3>
+                  <h3>EXPIRES: {challengeEndsOn}.</h3>
+                  <h3>TITLE: <span className='text-danger'>{title}</span>.</h3>
+                  <h3><span className='text-danger'>{description}</span>.</h3>
+                  {
+                     winningVotes && <h3>First video to reach <span className='text-danger'>{winningVotes}</span> votes wins!!!.</h3>
+                  }
+               </div>
+               <div className='challenge-details-img-div'>
+                  <img src={challengeCoverImage} alt='challenge cover img' className='challenge-details-img' />
+               </div>
             </div>
-            <div className='challenge-details-img-div'>
-               <img src={challengeCoverImage} alt='challenge cover img' className='challenge-details-img' />
-            </div>
+            <Container>
+               <strong>{challengeVideoIDs}</strong>
+            </Container>
          </div>
-         <Container>
-            <strong>{challengeVideoIDs}</strong>
-         </Container>
-      </div>
-   )
+      )
+   } else {
+      return <Navigate to="/" replace={false} />
+   }
+   
 }
 
 export default CurrentChallengeDetails
