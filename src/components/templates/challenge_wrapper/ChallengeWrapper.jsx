671fab9bc67e4d214f204753f0da695a8b41c821
admin_dho <davidhorph@hotmail.com>
2025-10-12 14:58:03 -0500

    Update ChallengeWrapper.jsx
    css changes.

--- src/components/templates/challenge_wrapper/ChallengeWrapper.jsx
+++ src/components/templates/challenge_wrapper/ChallengeWrapper.jsx
@@ -16,13 +16,13 @@ const ChallengeWrapper = ({ coverImg, title, button_text, clickLogic, ...args })
    const { username, email } = allUsers.find(({ id }) => id == _ownerID);
 
    return (
-      <Card className='m-1' style={{ position: 'relative', border: idx % 2 == 1 ? '3.5px solid lightseagreen' : '3.5px solid red', height: '100%', width: '100%', borderRadius: '15.3px', overflow: 'hidden', objectFit: 'cover' }}>
+      <Card style={{ position: 'relative', border: idx % 2 == 1 ? '3.5px solid lightseagreen' : '3.5px solid red', height: '100%', width: '100%', borderRadius: '15.3px', overflow: 'hidden', objectFit: 'cover' }}>
          <Suspense fallback={<SuspenseFallback />}>
             <img src={coverImg} alt='cover pic for challenge wrapper' className='cover-img' />
 
             <CardTitle className='p-1' style={{ position: 'relative', width: '100%', zIndex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}><h5 className='deadline-title'>DEADLINE: <span className='text-danger'>{expires}</span>.</h5></CardTitle>
             
-            <CardTitle className='p-1' style={{ position: 'relative', width: '100%', zIndex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <CardTitle className='p-1' style={{ position: 'relative', width: '100%', zIndex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50%' }}>
                <h3 className='title'> {title.length > 25 ? title.substring(0, 25) + "..." : title} </h3>
             </CardTitle>    
             {
