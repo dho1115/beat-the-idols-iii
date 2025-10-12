f3ef403efac9fe7f528fa0bddf8135b1f7e3085f
admin_dho <davidhorph@hotmail.com>
2025-10-12 14:57:37 -0500

    Added vote logic.
    Added vote logic and tested to see if alert( ) shows if the votes == winningVotes or if todaysDate (inside the onHandleVote function == challengeEndsOn).

--- src/components/templates/video_wrapper/VideoWrapper.jsx
+++ src/components/templates/video_wrapper/VideoWrapper.jsx
@@ -6,7 +6,7 @@ import { Card, CardBody, CardFooter, CardText, CardTitle, Button } from 'reactst
 import "./VideoWrapper.styles.css";
 
 const VideoWrapper = ({ video_component, title, description, button_text, clickLogic, ...args }) => {
-   const { color, record, username, idx } = args;
+   const { color, record, username, idx, votes } = args;
 
    return (
       <Card className='m-1' style={{ border: idx % 2 == 1 ? '3.5px solid lightseagreen' : '3.5px solid red', height: '100%', borderRadius: '15.3px' }}>
@@ -22,12 +22,8 @@ const VideoWrapper = ({ video_component, title, description, button_text, clickL
             <CardBody className='px-1'>
                <h5>Posted By: {username}</h5>
                {record ? <h5>wins: <span className='text-danger'>{record.wins}</span> losses: <span className='text-danger'>{record.losses}</span>, ties: <span className='text-danger'>{record.ties}</span>, pct: <span className='text-danger'>{record.winPct}</span></h5> : <p></p>}
-               {
-                  description &&
-                  <CardText className='px-1' style={{paddingBottom: '5px', height: '100%'}}>
-                     {description}
-                  </CardText>
-               }
+               { description && <CardText className='px-1' style={{paddingBottom: '5px', height: '100%'}}>{description}</CardText> }
+               { (votes || votes === 0) && <CardText style={{paddingBottom: '5px'}}><h3>VOTES: <span className='text-danger'>{votes}</span>.</h3></CardText> }
             </CardBody>            
             {
                (button_text && clickLogic)
