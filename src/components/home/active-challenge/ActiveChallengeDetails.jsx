96a09b7c0720312ef40d253b38c740e6bc05f0cf
admin_dho <davidhorph@hotmail.com>
2025-10-12 20:19:22 -0500

    Added DateTime( ) and DateTime.diff( ) logic.
    1. Created the following useState to calculate days remaining (to 3 decimal places): const [daysRemainingForChallenge, setDaysRemainingForChallenge] = useState(DateTime.fromISO(challengeEndsOn).diff(DateTime.now(), ['days']));
    
    2. Inside onHandleVote and useEffect, I created the logic to see if the challenge has ended using my custom function, hasChallengeEnded.

--- src/components/home/active-challenge/ActiveChallengeDetails.jsx
+++ src/components/home/active-challenge/ActiveChallengeDetails.jsx
@@ -9,7 +9,7 @@ import VideoWrapper from '../../templates/video_wrapper/VideoWrapper';
 import YouTubeVideo from '../../templates/video/you_tube/YouTubeVideo';
 
 //functions.
-import { hasChallengeEnded } from './functions';
+import { challengeHasEnded } from './functions';
 
 import { dataContext } from '../../../App';
 
@@ -23,14 +23,15 @@ const ActiveChallengeDetails = () => {
 
    const [videosInChallengeState, setVideosInChallengeState] = useState(videosInChallenge)
 
+   const [daysRemainingForChallenge, setDaysRemainingForChallenge] = useState(DateTime.fromISO(challengeEndsOn).diff(DateTime.now(), ['days']));
+
    const challengeOwnerObject = allUsers.find(val => val.id == _challengeOwnerID);
 
    const onHandleVote = (val) => {
-      const todaysDate = DateTime.now().toFormat('yyyy-MM-dd');
       const { id } = val;
-
-      if (hasChallengeEnded(id, todaysDate, challengeEndsOn, val.votes, winningVotes)) {
-         alert("CHALLENGE HAS ENDED!!!")
+      const { votes } = val.challengeAccessories;
+      if (challengeHasEnded(id, daysRemainingForChallenge.days.toFixed(3), votes, winningVotes)) {
+         alert(`CHALLENGE HAS ENDED!!! RESULTS ARE: ${JSON.stringify(videosInChallengeState)}.`)
          return;
       }
 
@@ -43,17 +44,23 @@ const ActiveChallengeDetails = () => {
       console.log(videosInChallengeState);
    }
 
+   useEffect(() => {
+      if (daysRemainingForChallenge.days.toFixed(3) <= 0) {
+         alert(`THIS CHALLENGE HAS ENDED!!! THE RESULTS ARE: ${JSON.stringify(videosInChallengeState)}.`);
+      }
+   }, [])
+
    return (
       <div>
          <div className='challenge-details-header my-3'>
             <div className='challenge-details-description-div p-3'>
                <h3>Challenge ID: <span className='text-danger'>{_challengeID}</span></h3>
                <h3>OWNER: {challengeOwnerObject.username} (id# {_challengeOwnerID})</h3>
-               <h3>EXPIRES: {challengeEndsOn}.</h3>
+               <h3>EXPIRES: {challengeEndsOn} (<span className='text-danger'>{daysRemainingForChallenge.days.toFixed(3)} days left</span>).</h3>
                <h3>TITLE: <span className='text-danger'>{title}</span>.</h3>
                <h3>DESCRIPTION:<span className='text-danger'>{description}</span>.</h3>
                {
-                  winningVotes && <h3>First video to reach <span className='text-danger'>{winningVotes}</span> votes wins!!!.</h3>
+                  winningVotes ? <h3>First video to reach <span className='text-danger'>{winningVotes}</span> votes wins!!!.</h3>: ""
                }
             </div>
             <div className='challenge-details-img-div'>
@@ -66,13 +73,14 @@ const ActiveChallengeDetails = () => {
                   <VideoWrapper
                      key={idx}
                      idx={idx}   
-                     video_component={val.videoType=='you-tube' ? <YouTubeVideo title={val.title} url={val.url} /> : <UploadVideo file={val.urlOrFile} />}
+                     video_component={val.videoType=='you-tube' ? <YouTubeVideo title={val.title} url={val.urlOrFile} /> : <UploadVideo file={val.urlOrFile} />}
                      title={val.title}
                      description={null}
                      username={val.username}
                      votes={val.challengeAccessories.votes}
-                     button_text="VOTE FOR THIS VIDEO!!!"
+                     button_text={challengeHasEnded() ? "THIS CHALLENGE HAS ENDED!!!" : "VOTE FOR THIS VIDEO!!!"}
                      clickLogic={() => onHandleVote(val)}
+                     disabled={challengeHasEnded()}
                   />
                ))
             }
