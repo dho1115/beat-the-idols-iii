f69a511cb2b28930be0139dc847cdebed9b97d3e
admin_dho <davidhorph@hotmail.com>
2025-10-12 20:20:45 -0500

    votes logic, disabled={disabled) feature.
    Created logic for votes and disabled feature.

--- src/components/templates/video_wrapper/VideoWrapper.jsx
+++ src/components/templates/video_wrapper/VideoWrapper.jsx
@@ -6,7 +6,7 @@ import { Card, CardBody, CardFooter, CardText, CardTitle, Button } from 'reactst
 import "./VideoWrapper.styles.css";
 
 const VideoWrapper = ({ video_component, title, description, button_text, clickLogic, ...args }) => {
-   const { color, record, username, idx, votes } = args;
+   const { color, record, username, idx, votes, disabled } = args;
 
    return (
       <Card className='m-1' style={{ border: idx % 2 == 1 ? '3.5px solid lightseagreen' : '3.5px solid red', height: '100%', borderRadius: '15.3px' }}>
@@ -23,13 +23,13 @@ const VideoWrapper = ({ video_component, title, description, button_text, clickL
                <h5>Posted By: {username}</h5>
                {record ? <h5>wins: <span className='text-danger'>{record.wins}</span> losses: <span className='text-danger'>{record.losses}</span>, ties: <span className='text-danger'>{record.ties}</span>, pct: <span className='text-danger'>{record.winPct}</span></h5> : <p></p>}
                { description && <CardText className='px-1' style={{paddingBottom: '5px', height: '100%'}}>{description}</CardText> }
-               { (votes || votes === 0) && <CardText style={{paddingBottom: '5px'}}><h3>VOTES: <span className='text-danger'>{votes}</span>.</h3></CardText> }
+               { (votes || votes === 0) && <CardText style={{paddingBottom: '5px'}}>VOTES: <span className='text-danger'>{votes}</span></CardText> }
             </CardBody>            
             {
                (button_text && clickLogic)
                &&
                <CardFooter className='w-100' style={{backgroundColor: 'black'}}>
-                  <Button type="button" className= {`btn btn-${color ? color : 'danger'} btn-block w-100`} onClick={clickLogic || null}>{button_text}</Button>
+                  <Button type="button" className= {`btn btn-${color ? color : 'danger'} btn-block w-100`} disabled={disabled} onClick={clickLogic || null}>{button_text}</Button>
                </CardFooter>
             }
          </Suspense>
