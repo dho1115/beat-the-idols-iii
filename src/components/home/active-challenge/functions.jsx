ca359b9ba67d302555ee079a91176464c3a361cb
admin_dho <davidhorph@hotmail.com>
2025-10-13 06:42:28 -0500

    New Function: addVoteToVideoLogic
    addVoteToVideoLogic will be imported to ActiveChallengeDetails.jsx and replace updateVideosInChallengeState. It basically does the same thing. Just refactoring my code.

--- src/components/home/active-challenge/functions.jsx
+++ src/components/home/active-challenge/functions.jsx
@@ -1,5 +1,14 @@
+export const addVoteToVideoLogic = (videosInChallegeState, targetID) => videosInChallegeState.map((targetVideo) => {
+   if (targetVideo.id == targetID) targetVideo.challengeAccessories.votes += 1;
+   return targetVideo;
+})
+
 export const challengeHasEnded = (videoID, dateDifference, votes, WinningVotes) => {   
    if (Number(dateDifference <= 0)) return true;
    else if (WinningVotes && (votes == WinningVotes)) return true;
    return false;
+}
+
+export const endChallengeLogic = (arg) => {
+   alert(`CHALLENGE HAS ENDED!!! RESULTS ARE: ${JSON.stringify(arg)}.`)
 }
