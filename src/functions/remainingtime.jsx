3886299575967391451c86212919a8e76bb01c0b
admin_dho <davidhorph@hotmail.com>
2025-10-18 16:10:24 -0500

    Update remainingtime.jsx

--- src/functions/remainingtime.jsx
+++ src/functions/remainingtime.jsx
@@ -1,3 +1,5 @@
-export const findExpiredChallenges = (activeChallenges, DateTime_Object, timeRemainingFunc) => activeChallenges.filter(({ challengeEndsOn }) => timeRemainingFunc(DateTime_Object, challengeEndsOn)) <= 0;
+export const findExpiredChallenges = (activeChallenges, DateTime_Object, timeRemainingFunc) => activeChallenges.filter(({ challengeEndsOn }) => timeRemainingFunc(DateTime_Object, challengeEndsOn) <= 0);
 
-export const timeRemaining = (DateTime_Object, challengeEndsOn, ...args) => DateTime_Object.fromISO(challengeEndsOn).diff(DateTime_Object.now(), ['days']);
+export const timeRemaining = (DateTime_Object, challengeEndsOn, ...args) => DateTime_Object.fromISO(challengeEndsOn).diff(DateTime_Object.now(), ['days']).days.toFixed(3);
+
+export const deleteExpiredChallenges = (expiredChallenges, deletePromise) => expiredChallenges.map(({ _challengeID }) => deletePromise(`http://localhost:3003/activeChallenges/${_challengeID}`));
