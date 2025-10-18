e042cdd9121dc1478eba34912237eac70587f95c
admin_dho <davidhorph@hotmail.com>
2025-10-18 14:19:00 -0500

    refactoring.

--- src/functions/remainingtime.jsx
+++ src/functions/remainingtime.jsx
@@ -1,5 +1,3 @@
-export const findExpiredChallenges = (activeChallenges, promise) => {
-   return activeChallenges.map(({challengeEndsOn, _challengeID}) => challengeEndsOn)
-}
+export const findExpiredChallenges = (activeChallenges, DateTime_Object, timeRemainingFunc) => activeChallenges.filter(({ challengeEndsOn }) => timeRemainingFunc(DateTime_Object, challengeEndsOn)) <= 0;
 
 export const timeRemaining = (DateTime_Object, challengeEndsOn, ...args) => DateTime_Object.fromISO(challengeEndsOn).diff(DateTime_Object.now(), ['days']);
