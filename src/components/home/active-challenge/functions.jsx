32a0e4b0523dc5f7c6735be028d533c500c36b42
admin_dho <davidhorph@hotmail.com>
2025-10-18 17:08:40 -0500

    Added logic for updateVideoRecords function.
    updateVideoReocrds function updates the records for winners, losers and tied videos based on highestVote. The leaders are the ones that == highestVote.
    
    if leaders.length > 1, there is a tie.
    
    It IS  possible updateLosers can be [ ] (if all the videos are tied).

--- src/components/home/active-challenge/functions.jsx
+++ src/components/home/active-challenge/functions.jsx
@@ -11,6 +11,36 @@ export const calculateHighestVote = (videosInChallegeState) => {
    return Math.max(...ArrayOfVotes);
 }
 
+export const updateVideoRecords = (expiredChallenge, highestVote) => {
+   const { videosInChallenge } = expiredChallenge;
+   try {
+      const leaders = videosInChallenge.map(({ challengeAccessories: { votes } }) => votes == highestVote);
+
+      const losers = videosInChallenge.map(({ challengeAccessories: { votes } }) => votes < highestVote);
+
+      const updateLeaders = leaders.map(({ challengeAccessories, record, ...rest }) => {
+         if (leaders.length > 1) {
+            record.ties += 1;
+            record.winPct = record.wins / (record.wins + record.losses + record.ties);
+            return { record, ...rest };
+         } //tie logic.
+         record.wins += 1;
+         record.winPct = record.wins / (record.wins + record.losses + record.ties); //win logic
+         return { record, ...rest };
+      })
+
+      const updateLosers = losers.length > 0 ? losers.map(({ challengeAccessories, record, ...rest }) => {
+         record.losses += 1;
+         record.winPct = record.wins / (record.wins + record.losses + record.ties);
+         return { record, ...rest };
+      }) : [];
+      return { updatedVideoData: [...updateLeaders, ...updateLosers] };
+   } catch (error) {
+      console.error({ message: 'updateVideoRecordError!!!', error, errorMessage: error.message, errorCode: error.code });
+      return { updatedVideoData: [] };
+   }
+}
+
 export const challengeHasEnded = (videoID, dateDifference, votes, WinningVotes) => {   
    if (Number(dateDifference <= 0)) return true;
    else if (WinningVotes && (votes == WinningVotes)) return true;
@@ -19,7 +49,6 @@ export const challengeHasEnded = (videoID, dateDifference, votes, WinningVotes) 
 
 export const endChallengeLogic = async (arg, findLeaders, url, deleteObjectAPI=deleteObjectAPI) => {
    try {
-      alert(`CHALLENGE HAS ENDED!!! \nHERE ARE YOUR LEADERS AND LOSERS: \n ${JSON.stringify(findLeaders)}.`);
       const deleteChallenge = await deleteObjectAPI(url)
       return deleteChallenge
    } catch (error) {
