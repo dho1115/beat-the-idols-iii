54dfef11bca4745ceed75029244b9c5cdfc22b48
admin_dho <davidhorph@hotmail.com>
2025-10-18 14:07:42 -0500

    New Functions added.

--- src/components/home/active-challenge/functions.jsx
+++ src/components/home/active-challenge/functions.jsx
@@ -1,7 +1,15 @@
-export const addVoteToVideoLogic = (videosInChallegeState, targetID) => videosInChallegeState.map((targetVideo) => {
-   if (targetVideo.id == targetID) targetVideo.challengeAccessories.votes += 1;
-   return targetVideo;
+import { UpdateDataAPI } from "../../../functions/updateapi";
+import { deleteObjectAPI } from "../../../functions/deleteapi";
+
+export const addVoteToVideoLogic = (videosInChallegeState, targetID) => videosInChallegeState.map(challengeVideo => {
+   if (challengeVideo.id == targetID) challengeVideo.challengeAccessories.votes += 1;
+   return challengeVideo;
 })
+
+export const calculateHighestVote = (videosInChallegeState) => {
+   const ArrayOfVotes = videosInChallegeState.map(({ challengeAccessories: { votes } }) => votes);
+   return Math.max(...ArrayOfVotes);
+}
 
 export const challengeHasEnded = (videoID, dateDifference, votes, WinningVotes) => {   
    if (Number(dateDifference <= 0)) return true;
@@ -9,6 +17,30 @@ export const challengeHasEnded = (videoID, dateDifference, votes, WinningVotes) 
    return false;
 }
 
-export const endChallengeLogic = (arg) => {
-   alert(`CHALLENGE HAS ENDED!!! RESULTS ARE: ${JSON.stringify(arg)}.`)
+export const endChallengeLogic = async (arg, findLeaders, url, deleteObjectAPI=deleteObjectAPI) => {
+   try {
+      alert(`CHALLENGE HAS ENDED!!! \nHERE ARE YOUR LEADERS AND LOSERS: \n ${JSON.stringify(findLeaders)}.`);
+      const deleteChallenge = await deleteObjectAPI(url)
+      return deleteChallenge
+   } catch (error) {
+      console.error({ error, errorCode: error.code, errorMessage: error.message });
+   }
+}
+
+export const findLeaders = (videosInChallenge, calculateHighestVote) => {
+   const leaders = videosInChallenge.filter(({ challengeAccessories: { votes } }) => votes == calculateHighestVote);
+   const losers = videosInChallenge.filter(({ challengeAccessories: { votes } }) => votes != calculateHighestVote);
+
+   return { highestVote: calculateHighestVote, leaders, losers, isTie: leaders.length > 1 };
+}
+
+export const updateDatabaseAndState = async (CRUD_action, url, setStateWrapperFunction, data = null) => {
+   try {
+      const updateDB = await CRUD_action(url, data);
+      setStateWrapperFunction()
+      return { message: "updateDatabseAndState SUCCESS!!!", updateDB };
+   } catch (error) {
+      console.error({error})
+      return { error, errorCode: error.code, errorMessage: error.message };
+   }
 }
