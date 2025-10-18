b6452b7a146f882918d529ef4a43ee1ff8ea02d7
admin_dho <davidhorph@hotmail.com>
2025-10-18 14:07:17 -0500

    Imorted new CRUD functions and other functions.

--- src/components/home/active-challenge/ActiveChallengeDetails.jsx
+++ src/components/home/active-challenge/ActiveChallengeDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useMemo, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { Button, Container } from 'reactstrap';
 import { DateTime } from 'luxon';
@@ -9,12 +9,16 @@ import VideoWrapper from '../../templates/video_wrapper/VideoWrapper';
 import YouTubeVideo from '../../templates/video/you_tube/YouTubeVideo';
 
 //functions.
-import { addVoteToVideoLogic, challengeHasEnded, endChallengeLogic } from './functions';
+import { addVoteToVideoLogic, calculateHighestVote, challengeHasEnded, endChallengeLogic, findLeaders, updateDatabaseAndState } from './functions';
+import { timeRemaining } from '../../../functions/remainingtime';
+
+import { UpdateDataAPI } from '../../../functions/updateapi';
+import { PatchDataAPI } from '../../../functions/patchapi';
 
 import { dataContext } from '../../../App';
 
 import "../Challenges.styles.css";
-import WinningVotesOption from '../../forms/challenge/challenge_ends_option/WinningVotesOption';
+
 
 const ActiveChallengeDetails = () => {
    const { _challengeID } = useParams();
@@ -22,27 +26,44 @@ const ActiveChallengeDetails = () => {
 
    const { challengeCoverImage, challengeEndsOn, _challengeOwnerID, videosInChallenge, description, title, winningVotes } = currentChallenges.find(val => val._challengeID == _challengeID);
 
-   const [videosInChallengeState, setVideosInChallengeState] = useState(videosInChallenge)
+   const [highestVoteState, setHighestVoteState] = useState(0);
 
-   const [daysRemainingForChallenge, setDaysRemainingForChallenge] = useState(DateTime.fromISO(challengeEndsOn).diff(DateTime.now(), ['days']));
+   const [videosInChallengeState, setVideosInChallengeState] = useState(videosInChallenge);
+
+   // const [daysRemainingForChallenge, setDaysRemainingForChallenge] = useState(DateTime.fromISO(challengeEndsOn).diff(DateTime.now(), ['days']));
+   const [daysRemainingForChallenge, setDaysRemainingForChallenge] = useState(timeRemaining(DateTime, challengeEndsOn));
 
    const challengeOwnerObject = allUsers.find(val => val.id == _challengeOwnerID);
 
    const onHandleVote = ({ id, challengeAccessories: { votes } }) => {
-      const challengeEnded = challengeHasEnded(id, daysRemainingForChallenge.days.toFixed(3), votes, winningVotes)
-      if (challengeEnded) {
-         endChallengeLogic(videosInChallengeState); //all the logic to end challenge.
-         return;
-      }
+      const challengeEnded = challengeHasEnded(id, daysRemainingForChallenge.days.toFixed(3), votes, winningVotes) //check if challenge has ended.
 
       const addVoteToSelectedVideo = addVoteToVideoLogic(videosInChallengeState, id);
 
-      setVideosInChallengeState(addVoteToSelectedVideo);
+      PatchDataAPI(`http://localhost:3003/activeChallenges/${_challengeID}`, { videosInChallenge: addVoteToSelectedVideo })
+         .then(response => {
+            setVideosInChallengeState(addVoteToSelectedVideo);
+            return response.json()
+         })
+         .then(result => {
+            const highestVote = calculateHighestVote(videosInChallengeState);
+            (highestVote != highestVoteState) && setHighestVoteState(highestVote);
+            return { highestVote };
+         })
+         .then(({ highestVote }) => {
+            if (challengeEnded) {
+               endChallengeLogic(videosInChallengeState, findLeaders(videosInChallengeState, highestVote), `http://localhost:3003/activeChallenges/${_challengeID}`);
+               return { challengeEnded };
+            }
+            const leaders = findLeaders(videosInChallengeState, highestVote); //current vote leaders.
+            return leaders;
+         })
+         .catch(err => console.error({ message: "PatchDataAPI error!!!", err, errCode: err.code, errMessage: err.message }));
    }
 
    useEffect(() => {
       if (daysRemainingForChallenge.days.toFixed(3) <= 0) {
-         endChallengeLogic(videosInChallengeState)
+         endChallengeLogic(videosInChallengeState, findLeaders(videosInChallengeState, highestVoteState), `http://localhost:3003/activeChallenges/${_challengeID}`)
       }
    }, [])
 
