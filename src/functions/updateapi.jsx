f4c14f17bfeda529d604f824aeeeadb2386229ca
admin_dho <davidhorph@hotmail.com>
2025-10-19 13:28:28 -0500

    UpdateDataInDBThenSetState.

--- src/functions/updateapi.jsx
+++ src/functions/updateapi.jsx
@@ -4,4 +4,16 @@ export const UpdateDataAPI = (url, updatedData) => fetch(url, {
       'Content-Type': 'application/json'
    },
    body: JSON.stringify(updatedData)
-}).then(result => console.log({message: "SUCCESS!!!", result})).catch(error => console.error({errorMessage: error.message}))
+})
+   .then(result => console.log({ message: "SUCCESS!!!", result }))
+   .catch(error => console.error({ errorMessage: error.message }))
+
+export const UpdateDataInDBThenSetState = async (UpdateDataAPI, url, updatedData, setStateWrapper) => {
+   try {
+      const updateDB = await UpdateDataAPI(url, updatedData);
+      setStateWrapper(updatedData)
+      return updatedData;
+   } catch (error) {
+      console.error({ message: "UpdateDataInDBThenSetState ERROR!!!", error, errorMessage: error.message, errorCode: error.code });
+   }
+}
