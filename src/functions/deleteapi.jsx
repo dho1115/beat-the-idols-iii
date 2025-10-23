7581b20f0d9e2b5521358ad6f4103f75d45d85cb
admin_dho <davidhorph@hotmail.com>
2025-10-23 15:40:33 -0500

    Update deleteapi.jsx

--- src/functions/deleteapi.jsx
+++ src/functions/deleteapi.jsx
@@ -2,8 +2,11 @@ export const deleteObjectAPI = url => fetch(url, {
    method: 'DELETE'
 })
    .then(result => {
-   if (result.ok) console.log({ message: `${url} has been deleted!!!`, result });
+      if (result.ok) console.log({ message: `${url} has been deleted!!!`, result })
+      
+      else console.error("delete is NOT ok:", result);
 
+      debugger;
       return { result, url };
    })
    .catch(error => console.error({ message: `Error deleting ${url}...`, error, errorCode: error.code, errorMessage: error.message }));
