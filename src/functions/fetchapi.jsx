c0b18b4d1f3cb3fe77a39ece86e0837e499fac6e
admin_dho <davidhorph@hotmail.com>
2025-11-03 11:15:15 -0600

    handleExpiredActiveChallenges.
    Problem: Inside fetchDataThenSetState, one of the arguments is fetchData which already returns the data as .json( ).
    
    What I did was:
    
    data = response.json( );
    
    Which is where the error was: response is ALREADY a json( ). I should not just do response.json( ) because in fetchDataAPI, it is already returned as json( ) data.
    
    I should have just setState( data );
    
    Since fetchDataThenSetState is also inside handeExpiredActiveChallenges inside AppJsxFunctions, then that is why handleExpiredActiveChallenges threw an error.

--- src/functions/fetchapi.jsx
+++ src/functions/fetchapi.jsx
@@ -3,6 +3,7 @@ export const fetchDataAPI = async url => {
       const rawData = await fetch(url);
       const jsonData = await rawData.json();
 
+
       return jsonData;
    } catch (error) {
       console.error({ from: 'fetchDataAPI (fetchapi.jsx)', error, errorCode: error.code, errorMessage: error.message, status: error.status });
@@ -13,8 +14,10 @@ export const fetchDataAPI = async url => {
 
 export const fetchDataThenSetState = async (fetchDataAPI, url, setStateWrapper) => {
    try {
-      const response = await fetchDataAPI(url);
-      const data = response.json();
+      const data = await fetchDataAPI(url);
+      
+      if (typeof (data) != 'object') throw new Error(`ERROR INSIDE fetchDataThenSetState (fetchapi.jsx): data should be a function. Instead, it is of type ${typeof (data)}.`)
+      
       setStateWrapper(data);
       return data;
    } catch (error) {
