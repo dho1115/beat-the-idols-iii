40db69ebafbc97afd8a3d39da9e4d8732ac99455
admin_dho <davidhorph@hotmail.com>
2025-11-14 11:22:25 -0600

    Added logic to add video to state only (for now).
    I tried to destructure record prop from showEligibleVideos, but the record prop kept giving me the value for videoType ('you-tube'), so what I did was, I changed the destructured args to just val inside showEligibleVideos.
    
    Also, added logic inside handleAddVideoToChallenge function to add the video to the challengeAnnouncementState:
    
       const handleAddVideoToChallenge = args => {
          setVideoSubmissionStatus(true);
          const { description, posted, ...challengeArgs } = args;
          const challengeVideoObject = { ...challengeArgs, challengeAccessories: { votes: 0, finalStatus: 'pending' } };
    
          const updatedChallengeAnnouncements = challengeAnnouncements.map(announcement => {
             if (announcement.id == _challengeAnnouncementID) {
                announcement.announcement.videosInChallenge.push(challengeVideoObject);
                announcement.challenge.videosInChallenge.push(challengeVideoObject)
             }
    
             return announcement;
          })
    
          setChallengeAnnouncements(updatedChallengeAnnouncements);
          setVideoSubmissionStatus(false)
    
          console.log("===== CHALLENGE ANNOUNCEMENT UPDATED!!! =====");
          console.log(`challengeAnnouncment id ${_challengeAnnouncementID} has been updated with ${JSON.stringify(challengeVideoObject)}. The state, updatedChallengeAnnouncements is now: `, updatedChallengeAnnouncements);
          console.log("=============================================");
       }
    
    Note how inside handleAddVideoToChallenge, I destructured out the description and posted property because I did not need those properties inside the video property for challengeAnnouncement and activeChallenges.
    
    This is the logic to set the challengeAnnouncment state:
    
          const updatedChallengeAnnouncements = challengeAnnouncements.map(announcement => {
             if (announcement.id == _challengeAnnouncementID) {
                announcement.announcement.videosInChallenge.push(challengeVideoObject);
                announcement.challenge.videosInChallenge.push(challengeVideoObject)
             }
    
             return announcement;
          })

--- src/components/home/challenge-announcements/AddVideosToChallenge.jsx
+++ src/components/home/challenge-announcements/AddVideosToChallenge.jsx
@@ -1,32 +1,64 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { dataContext } from '../../../App';
 
 //components
 import YouTubeVideo from '../../templates/video/you_tube/YouTubeVideo';
 import UploadVideo from '../../templates/video/upload/UploadVideo';
 import VideoWrapper from '../../templates/video_wrapper/VideoWrapper';
 
+//Contexts.
+import { dataContext } from '../../../App';
+
 import "../Challenges.styles.css";
 
 const AddVideosToChallenge = ({ showEligibleVideos }) => {
-   const { id: _challengeAnnouncementID } = useParams;
+   const [videoSubmissionStatus, setVideoSubmissionStatus] = useState(false);
 
-   const handleAddVideoToChallenge = (id, _userID, title, username) => {
-      console.log(`Added ${title} by ${username} (_videoID #${id}) into the challenge!!!`)
+   const { challengeAnnouncements, setChallengeAnnouncements } = useContext(dataContext);
+   const { id: _challengeAnnouncementID } = useParams();
+
+   const handleAddVideoToChallenge = args => {
+      setVideoSubmissionStatus(true);
+      const { description, posted, ...challengeArgs } = args;
+      const challengeVideoObject = { ...challengeArgs, challengeAccessories: { votes: 0, finalStatus: 'pending' } };
+
+      const updatedChallengeAnnouncements = challengeAnnouncements.map(announcement => {
+         if (announcement.id == _challengeAnnouncementID) {
+            announcement.announcement.videosInChallenge.push(challengeVideoObject);
+            announcement.challenge.videosInChallenge.push(challengeVideoObject)
+         }
+
+         return announcement;
+      })
+
+      setChallengeAnnouncements(updatedChallengeAnnouncements);
+      setVideoSubmissionStatus(false)
+
+      console.log("===== CHALLENGE ANNOUNCEMENT UPDATED!!! =====");
+      console.log(`challengeAnnouncment id ${_challengeAnnouncementID} has been updated with ${JSON.stringify(challengeVideoObject)}. The state, updatedChallengeAnnouncements is now: `, updatedChallengeAnnouncements);
+      console.log("=============================================");
    }
+
+   useEffect(() => {
+      console.log("===== CHALLENGE ANNOUNCEMENT UPDATED!!! =====");
+      console.log(`challengeAnnouncment id ${_challengeAnnouncementID} has been updated: `, challengeAnnouncements);
+      console.log("=============================================");
+   }, [videoSubmissionStatus])
 
    return (
       <div className='eligible-videos-container p-3 my-3'>
          {
-            showEligibleVideos().map(({ id, _userID, description, username, title, videoType, urlOrFile }, idx) => (
+            showEligibleVideos().map((val, idx) => (
                <VideoWrapper
                   key={idx}
-                  video_component={videoType == 'you-tube' ? <YouTubeVideo url={urlOrFile} /> : <UploadVideo file={urlOrFile} />}
-                  title={title}
-                  username={username}
-                  button_text="SELECT THIS VIDEO!!!" clickLogic={() => handleAddVideoToChallenge(id, _userID, title, username)}
-               />))
+                  urlOrFile={val.urlOrFile}
+                  video_component={val.videoType == 'you-tube' ? <YouTubeVideo url={val.urlOrFile} /> : <UploadVideo file={val.urlOrFile} />}
+                  record={val.record}
+                  title={val.title}
+                  username={val.username}
+                  button_text="ADD TO CHALLENGE!!!" clickLogic={() => handleAddVideoToChallenge(val)}
+               />
+            ))
          }
       </div>
    )
