0d1bc6eb7755c84175c6a40791ba0b9f5c8f4727
admin_dho <davidhorph@hotmail.com>
2025-12-18 17:40:47 -0600

    FIREBASE CONFIGURATION.
    // Import the functions you need from the SDKs you need
    import { initializeApp } from "firebase/app";
    // TODO: Add SDKs for Firebase products that you want to use
    // https://firebase.google.com/docs/web/setup#available-libraries
    
    // Your web app's Firebase configuration
    const firebaseConfig = {
      apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
      authDomain: import.meta.env.VITE_AUTH_DOMAIN,
      projectId: "beat-the-idols",
      storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
      messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
      appId: "1:626239044244:web:5512d57bde6d92e4b1d281"
    };
    
    // Initialize Firebase
    export const app = initializeApp(firebaseConfig);

---
+++ src/functions/firebase/configuration.jsx
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: "beat-the-idols",
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: "1:626239044244:web:5512d57bde6d92e4b1d281"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
